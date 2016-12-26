d608d784384410006e6dbae4158c354f9c46e2bb
Misterkister <misterk1217@hotmail.com>
2016-12-25 20:47:34 -0600

    Update ScreenNetRoom underlay.lua

--- Themes/Til Death/BGAnimations/ScreenNetRoom underlay.lua
+++ Themes/Til Death/BGAnimations/ScreenNetRoom underlay.lua
@@ -5,7 +5,11 @@ t[#t+1] = Def.Quad{
 };
 
 t[#t+1] = Def.Quad{
-	InitCommand=cmd(xy,SCREEN_WIDTH-capWideScale(get43size(350),350),0;halign,0;valign,0;zoomto,4,SCREEN_HEIGHT;diffuse,color("#FFFFFF"));
+	InitCommand=cmd(draworder,-300;xy,frameX,frameY-100;zoomto,SCREEN_WIDTH,160;halign,0;diffuse,getMainColor('highlight');diffusealpha,0.15;diffusebottomedge,color("0,0,0,0"))
+}
+
+t[#t+1] = Def.Quad{
+	InitCommand=cmd(xy,SCREEN_WIDTH-capWideScale(get43size(350),350),0;halign,0;valign,0;zoomto,4,SCREEN_HEIGHT;diffuse,getMainColor('highlight'));
 };
 
 return t
