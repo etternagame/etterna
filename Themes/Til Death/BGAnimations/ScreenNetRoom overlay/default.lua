b8978a01a4cac7d9b16b199713cc5c96f653f7cb
Nickito12 <abramlujan@gmail.com>
2016-12-31 14:09:09 -0300

    Theme controlled userlist

--- Themes/Til Death/BGAnimations/ScreenNetRoom overlay/default.lua
+++ Themes/Til Death/BGAnimations/ScreenNetRoom overlay/default.lua
@@ -19,7 +19,7 @@ return false;
 end
 
 local t = Def.ActorFrame{
-	OnCommand=function(self) SCREENMAN:GetTopScreen():AddInputCallback(input) end;
+	OnCommand=function(self) SCREENMAN:GetTopScreen():AddInputCallback(input) SCREENMAN:GetTopScreen():UsersVisible(false) end;
 }
 
 t[#t+1] = Def.Actor{
@@ -38,5 +38,46 @@ t[#t+1] = LoadFont("Common Large")..{
 t[#t+1] = LoadActor("../_cursor");
 t[#t+1] = LoadActor("currenttime");
 t[#t+1] = LoadActor("../_halppls");
+
+
+t[#t+1] = LoadFont("Common Normal") .. {
+	InitCommand=cmd(xy,SCREEN_WIDTH/3,SCREEN_TOP+15;zoom,0.35;diffuse,getMainColor('positive');maxwidth,SCREEN_WIDTH),
+	BeginCommand=cmd(queuecommand,"Set"),
+	SetCommand=function(self)
+			local str = ""
+			local top = SCREENMAN:GetTopScreen()
+			if top:GetUserQty() > 5 then
+				for i=1,5 do
+					str = str .. "      " .. (top:GetUser(i))
+				end
+			
+			else
+				for i=1,top:GetUserQty() do
+					str = str .. "      " .. (top:GetUser(i))
+				end
+			end
+			self:settext(str)
+	end,
+	PlayerJoinedMessageCommand=cmd(queuecommand,"Set"),
+	PlayerUnjoinedMessageCommand=cmd(queuecommand,"Set"),
+	UsersUpdateMessageCommand=cmd(queuecommand,"Set"),
+}
+t[#t+1] = LoadFont("Common Normal") .. {
+	InitCommand=cmd(xy,SCREEN_WIDTH/3,SCREEN_TOP+25;zoom,0.35;diffuse,getMainColor('positive');maxwidth,SCREEN_WIDTH),
+	BeginCommand=cmd(queuecommand,"Set"),
+	SetCommand=function(self)
+			local str = ""
+			local top = SCREENMAN:GetTopScreen()
+			if top:GetUserQty() > 5 then
+				for i=6,top:GetUserQty() do
+					str = str .. "      " .. (top:GetUser(i))
+				end
+			end
+			self:settext(str)
+	end,
+	PlayerJoinedMessageCommand=cmd(queuecommand,"Set"),
+	PlayerUnjoinedMessageCommand=cmd(queuecommand,"Set"),
+	UsersUpdateMessageCommand=cmd(queuecommand,"Set"),
+}
 
 return t
