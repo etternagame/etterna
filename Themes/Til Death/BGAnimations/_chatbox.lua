d124e149cf4ec7e6387808240443af0e62f5b309
Nickito12 <abramlujan@gmail.com>
2016-12-31 16:06:05 -0300

    Hide/show chatbox depending on tab. Fix width

--- Themes/Til Death/BGAnimations/_chatbox.lua
+++ Themes/Til Death/BGAnimations/_chatbox.lua
@@ -4,8 +4,8 @@ local border = 5
 
 local inputX = THEME:GetMetric("ScreenNetSelectBase","ChatInputX")
 local inputY = THEME:GetMetric("ScreenNetSelectBase","ChatInputY")
-local inputWidth = THEME:GetMetric("ScreenNetSelectBase","ChatTextInputWidth")*0.5
-local inputHeight = 40
+local inputWidth = THEME:GetMetric("ScreenNetSelectBase","ChatTextInputWidth")*0.4
+local inputHeight = 25
 
 
 local outputX = THEME:GetMetric("ScreenNetSelectBase","ChatOutputX")
@@ -13,13 +13,44 @@ local outputY = THEME:GetMetric("ScreenNetSelectBase","ChatOutputY")
 local outputWidth = THEME:GetMetric("ScreenNetSelectBase","ChatTextOutputWidth")*0.3153
 local outputHeight = THEME:GetMetric("ScreenNetSelectBase","ChatOutputLines")*9.25
 
+if IsUsingWideScreen() == true then
+
+local border = 5
+
+local inputX = THEME:GetMetric("ScreenNetSelectBase","ChatInputX")
+local inputY = THEME:GetMetric("ScreenNetSelectBase","ChatInputY")
+local inputWidth = THEME:GetMetric("ScreenNetSelectBase","ChatTextInputWidth")*0.4
+local inputHeight = 25
+
+
+local outputX = THEME:GetMetric("ScreenNetSelectBase","ChatOutputX")
+local outputY = THEME:GetMetric("ScreenNetSelectBase","ChatOutputY")
+local outputWidth = THEME:GetMetric("ScreenNetSelectBase","ChatTextOutputWidth")*0.3153
+local outputHeight = THEME:GetMetric("ScreenNetSelectBase","ChatOutputLines")*9.25
+
+end
 
 t[#t+1] = Def.Quad{
-	InitCommand=cmd(xy,inputX-border,inputY-border;zoomto,inputWidth+border*2,inputHeight+border*2;halign,0;valign,0;diffuse,color("#00000099"););	
-}
-
+	InitCommand=cmd(xy,inputX-border,inputY-border;zoomto,outputWidth+border*2,inputHeight+border*2;halign,0;valign,0;diffuse,color("#00000099");),
+	TabChangedMessageCommand=function(self)
+		local top= SCREENMAN:GetTopScreen()
+		if getTabIndex() == 0 then
+			self:visible(true)
+		else 
+			self:visible(false)
+		end
+	end,
+	}
 t[#t+1] = Def.Quad{
-	InitCommand=cmd(xy,outputX-border,outputY-border;zoomto,outputWidth+border*2,outputHeight+border*2;halign,0;valign,0;diffuse,color("#00000099"););	
+	InitCommand=cmd(xy,outputX-border,outputY-border;zoomto,outputWidth+border*2,outputHeight+border*2;halign,0;valign,0;diffuse,color("#00000099");),
+	TabChangedMessageCommand=function(self)
+		local top= SCREENMAN:GetTopScreen()
+		if getTabIndex() == 0 then
+			self:visible(true)
+		else 
+			self:visible(false)
+		end
+	end,
 }
 
 return t
