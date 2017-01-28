dcf722c9dae22330796a3f28cad9e3c4e8a5a05d
Nickito12 <abramlujan@gmail.com>
2017-01-28 19:52:25 -0300

    Add files via upload

--- Themes/Til Death/BGAnimations/_chatbox.lua
+++ Themes/Til Death/BGAnimations/_chatbox.lua
@@ -1,4 +1,6 @@
-local t = Def.ActorFrame{}
+
+local top
+local whee
 
 local border = 5
 
@@ -29,6 +31,58 @@ local outputWidth = THEME:GetMetric("ScreenNetSelectBase","ChatTextOutputWidth")
 local outputHeight = THEME:GetMetric("ScreenNetSelectBase","ChatOutputLines")*9.25
 
 end
+
+local function isOverChatbox()
+	if INPUTFILTER:GetMouseX() > outputX-border and INPUTFILTER:GetMouseX() < outputX + outputWidth+border*2 and INPUTFILTER:GetMouseY() > outputY-border and INPUTFILTER:GetMouseY() < outputY + outputHeight+border*2 then
+		return true
+	end
+	return false
+end
+
+
+local function scrollInput(event)
+	if event.DeviceInput.button == "DeviceButton_tab" then
+		if event.type == "InputEventType_FirstPress" then
+			local pressingtab = true
+		elseif event.type == "InputEventType_Release" then
+			local pressingtab = false
+		end
+	elseif event.DeviceInput.button == "DeviceButton_mousewheel up" and event.type == "InputEventType_FirstPress" then
+		if isOverChatbox() then
+			top:ScrollChatUp()
+		else
+			moving = true
+			if pressingtab == true then
+				whee:Move(-1)	
+			else
+				whee:Move(-1)	
+			end
+		end
+	elseif event.DeviceInput.button == "DeviceButton_mousewheel down" and event.type == "InputEventType_FirstPress" then
+		if isOverChatbox() then
+			top:ScrollChatDown()
+		else
+			moving = true
+			if pressingtab == true then
+				whee:Move(1)	
+			else
+				whee:Move(1)	
+			end
+		end
+	elseif moving == true then
+		whee:Move(0)
+		moving = false
+	end
+end
+
+
+local t = Def.ActorFrame{
+	BeginCommand=function(self)
+		top = SCREENMAN:GetTopScreen()
+		whee = top:GetMusicWheel()
+		top:AddInputCallback(scrollInput)
+	end,
+}
 
 t[#t+1] = Def.Quad{
 	InitCommand=cmd(xy,inputX-border,inputY-border;zoomto,outputWidth+border*2,inputHeight+border*2;halign,0;valign,0;diffuse,color("#00000099");),
