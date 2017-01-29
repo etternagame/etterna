e24fdede98789d4cc23d6e1552f0205bb2f5ee45
Nickito12 <abramlujan@gmail.com>
2017-01-28 22:37:46 -0300

    Update _mousewheelscroll.lua

--- Themes/Til Death/BGAnimations/_mousewheelscroll.lua
+++ Themes/Til Death/BGAnimations/_mousewheelscroll.lua
@@ -6,21 +6,21 @@ local pressingtab = false
 local function scrollInput(event)
 	if event.DeviceInput.button == "DeviceButton_tab" then
 		if event.type == "InputEventType_FirstPress" then
-			local pressingtab = true
+			pressingtab = true
 		elseif event.type == "InputEventType_Release" then
-			local pressingtab = false
+			pressingtab = false
 		end
 	elseif event.DeviceInput.button == "DeviceButton_mousewheel up" and event.type == "InputEventType_FirstPress" then
 		moving = true
 		if pressingtab == true then
-			whee:Move(-1)	
+			whee:Move(-2)	
 		else
 			whee:Move(-1)	
 		end
 	elseif event.DeviceInput.button == "DeviceButton_mousewheel down" and event.type == "InputEventType_FirstPress" then
 		moving = true
 		if pressingtab == true then
-			whee:Move(1)	
+			whee:Move(2)	
 		else
 			whee:Move(1)	
 		end
