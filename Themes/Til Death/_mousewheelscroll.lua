511d2db45e708cc76eaa92c1feb7c24e9afd7818
Nickito12 <abramlujan@gmail.com>
2017-01-28 21:36:20 -0300

    Add files via upload

---
+++ Themes/Til Death/_mousewheelscroll.lua
@@ -0,0 +1,40 @@
+
+local moving = false
+local whee
+local pressingtab = false
+
+local function scrollInput(event)
+	if event.DeviceInput.button == "DeviceButton_tab" then
+		if event.type == "InputEventType_FirstPress" then
+			local pressingtab = true
+		elseif event.type == "InputEventType_Release" then
+			local pressingtab = false
+		end
+	elseif event.DeviceInput.button == "DeviceButton_mousewheel up" and event.type == "InputEventType_FirstPress" then
+		moving = true
+		if pressingtab == true then
+			whee:Move(-3)	
+		else
+			whee:Move(-1)	
+		end
+	elseif event.DeviceInput.button == "DeviceButton_mousewheel down" and event.type == "InputEventType_FirstPress" then
+		moving = true
+		if pressingtab == true then
+			whee:Move(3)	
+		else
+			whee:Move(1)	
+		end
+	elseif moving == true then
+		whee:Move(0)
+		moving = false
+	end
+end
+
+local t = Def.ActorFrame{
+	BeginCommand=function(self)
+		whee = SCREENMAN:GetTopScreen():GetMusicWheel()
+		SCREENMAN:GetTopScreen():AddInputCallback(scrollInput)
+		self:visible(false)
+	end,
+}
+return t
