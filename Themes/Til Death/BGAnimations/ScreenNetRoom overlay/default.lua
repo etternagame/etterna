927a38c47fff68c6608e9200e586f9cad618c132
Nickito12 <abramlujan@gmail.com>
2017-01-29 12:54:20 -0300

    Update default.lua

--- Themes/Til Death/BGAnimations/ScreenNetRoom overlay/default.lua
+++ Themes/Til Death/BGAnimations/ScreenNetRoom overlay/default.lua
@@ -36,6 +36,7 @@ t[#t+1] = LoadFont("Common Large")..{
 	InitCommand=cmd(xy,5,32;halign,0;valign,1;zoom,0.55;diffuse,getMainColor('highlight');settext,"Lobby";);
 }
 t[#t+1] = LoadActor("../_cursor");
+t[#t+1] = LoadActor("../_mouseselect")
 t[#t+1] = LoadActor("currenttime");
 t[#t+1] = LoadActor("../_halppls");
 t[#t+1] = LoadActor("../_userlist");
