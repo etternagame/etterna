94b6b11133ca4aaf38774db4d84f4c349eb87646
Nickito12 <abramlujan@gmail.com>
2017-01-29 12:54:26 -0300

    Update default.lua

--- Themes/Til Death/BGAnimations/ScreenNetSelectBase overlay/default.lua
+++ Themes/Til Death/BGAnimations/ScreenNetSelectBase overlay/default.lua
@@ -39,6 +39,7 @@ t[#t+1] = LoadActor("currentsort")
 t[#t+1] = LoadActor("currenttime")
 t[#t+1] = LoadFont("Common Large")..{InitCommand=cmd(xy,5,32;halign,0;valign,1;zoom,0.55;diffuse,getMainColor("positive");settext,"Select Music:")}
 t[#t+1] = LoadActor("../_cursor")
+t[#t+1] = LoadActor("../_mouseselect")
 t[#t+1] = LoadActor("../_halppls")
 --t[#t+1] = LoadActor("wifesearchbar")
 t[#t+1] = LoadActor("../_userlist")
