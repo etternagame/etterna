3ec7644883a096837f950c06300e773835e279db
MinaciousGrace <minaciousgrace@gmail.com>
2017-06-01 23:33:10 -0400

    rebrand to etterna in installer files

--- src/ProductInfo.h
+++ src/ProductInfo.h
@@ -5,18 +5,13 @@
 
 /**
  * @brief A friendly string to refer to the product in crash dialogs, etc.
- *
- * As an example, use "StepMania" here, not "StepMania4".
  */
-#define PRODUCT_FAMILY_BARE StepMania - Etterna
+#define PRODUCT_FAMILY_BARE Etterna
 
 /**
  * @brief A unique name for each application that you might want installed side-by-side with other applications.
- *
- * As an example, use "StepMania4" here, not "StepMania".
- * It would cause a conflict with older versions such as StepMania 3.X.
  */
-#define PRODUCT_ID_BARE Stepmania - Etterna
+#define PRODUCT_ID_BARE Etterna
 
 // These cannot be #undef'd so make them unlikely to conflict with anything
 #define PRODUCT_STRINGIFY(x) #x
@@ -25,10 +20,10 @@
 #define PRODUCT_FAMILY		PRODUCT_XSTRINGIFY(PRODUCT_FAMILY_BARE)
 #define PRODUCT_ID			PRODUCT_XSTRINGIFY(PRODUCT_ID_BARE)
 
-#define VIDEO_TROUBLESHOOTING_URL "http://old.stepmania.com/stepmaniawiki.php?title=Video_Driver_Troubleshooting"
+#define VIDEO_TROUBLESHOOTING_URL "http://google.com"
 /** @brief The URL to report bugs on the program. */
-#define REPORT_BUG_URL "https://github.com/xwidghet/stepmania/issues"
-#define SM_DOWNLOAD_URL "https://github.com/xwidghet/stepmania"
+#define REPORT_BUG_URL "https://github.com/etternagame/etterna/issues"
+#define SM_DOWNLOAD_URL "https://github.com/etternagame/etterna"
 
 #define CAN_INSTALL_PACKAGES true
 
