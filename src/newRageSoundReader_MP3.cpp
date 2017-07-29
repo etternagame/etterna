0c3e24b992035c7c4414a4e986e3452b81aead31
Nickito12 <abramlujan@gmail.com>
2017-07-29 01:30:02 -0300

    First approximation to MP3 reader using FFMPEG

--- src/newRageSoundReader_MP3.cpp
+++ src/newRageSoundReader_MP3.cpp
@@ -1,25 +1,220 @@
 #include "newRageSoundReader_MP3.h"
 #include <io.h>
+#include "RageUtil.h"
 
+
+
+//Copied from RageSoundReader_WAV.cpp
+namespace
+{
+	/* pBuf contains iSamples 8-bit samples; convert to 16-bit.  pBuf must
+	* have enough storage to hold the resulting data. */
+	void Convert8bitToFloat(void *pBuf, int iSamples)
+	{
+		/* Convert in reverse, so we can do it in-place. */
+		const uint8_t *pIn = (uint8_t *)pBuf;
+		float *pOut = (float *)pBuf;
+		for (int i = iSamples - 1; i >= 0; --i)
+		{
+			int iSample = pIn[i];
+			iSample -= 128; /* 0..255 -> -128..127 */
+			pOut[i] = iSample / 128.0f;
+		}
+	}
+
+	/* Flip 16-bit samples if necessary.  On little-endian systems, this will
+	* optimize out. */
+	void ConvertLittleEndian16BitToFloat(void *pBuf, int iSamples)
+	{
+		/* Convert in reverse, so we can do it in-place. */
+		const int16_t *pIn = (int16_t *)pBuf;
+		float *pOut = (float *)pBuf;
+		for (int i = iSamples - 1; i >= 0; --i)
+		{
+			int16_t iSample = Swap16LE(pIn[i]);
+			pOut[i] = iSample / 32768.0f;
+		}
+	}
+
+	void ConvertLittleEndian24BitToFloat(void *pBuf, int iSamples)
+	{
+		/* Convert in reverse, so we can do it in-place. */
+		const unsigned char *pIn = (unsigned char *)pBuf;
+		float *pOut = (float *)pBuf;
+		pIn += iSamples * 3;
+		for (int i = iSamples - 1; i >= 0; --i)
+		{
+			pIn -= 3;
+
+			int32_t iSample =
+				(int(pIn[0]) << 0) |
+				(int(pIn[1]) << 8) |
+				(int(pIn[2]) << 16);
+
+			/* Sign-extend 24-bit to 32-bit: */
+			if (iSample & 0x800000)
+				iSample |= 0xFF000000;
+
+			pOut[i] = iSample / 8388608.0f;
+		}
+	}
+
+	void ConvertLittleEndian32BitToFloat(void *pBuf, int iSamples)
+	{
+		/* Convert in reverse, so we can do it in-place. */
+		const int32_t *pIn = (int32_t *)pBuf;
+		float *pOut = (float *)pBuf;
+		for (int i = iSamples - 1; i >= 0; --i)
+		{
+			int32_t iSample = Swap32LE(pIn[i]);
+			pOut[i] = iSample / 2147483648.0f;
+		}
+	}
+};
 
 newRageSoundReader_MP3::newRageSoundReader_MP3()
 {
+	codec=NULL;
+	codecCtx = NULL;
+	decodedFrame=NULL;
+	IOCtx = NULL;
+	formatCtx = NULL;
+	length = 0;
+	numChannels = 0;
+	dataSize = 0;
+	numSamples = 0;
+	sampleRate = 1;
 }
 
 
 newRageSoundReader_MP3::~newRageSoundReader_MP3()
 {
+	//Free everything if it isn't already
+	if (codecCtx)
+		avcodec::avcodec_close(codecCtx);
+	if (formatCtx)
+		avcodec::avformat_close_input(&formatCtx);  // AVFormatContext is released by avformat_close_input
+	if (IOCtx)
+		avcodec::av_free(IOCtx);             // AVIOContext is released by av_free
+	if (codec)
+		avcodec::av_free(codec);
+	if (decodedFrame)
+		avcodec::av_frame_free(&decodedFrame);
 }
 
+void RegisterProtocols()
+{
+	static bool Done = false;
+	if (Done)
+		return;
+	Done = true;
+
+	avcodec::avcodec_register_all();
+	avcodec::av_register_all();
+}
+int ReadFunc(void* ptr, uint8_t* buf, int buf_size)
+{
+	RageFileBasic *pFile = reinterpret_cast<RageFileBasic *>(ptr);
+	return pFile->Read(buf, buf_size);
+}
+// whence: SEEK_SET, SEEK_CUR, SEEK_END (like fseek) and AVSEEK_SIZE
+int64_t SeekFunc(void* ptr, int64_t pos, int whence)
+{
+	RageFileBasic *pFile = reinterpret_cast<RageFileBasic *>(ptr);
+	if (whence == AVSEEK_SIZE)
+		return -1;
+	return pFile->Seek(static_cast<int>(pos), whence);
+}
 RageSoundReader_FileReader::OpenResult newRageSoundReader_MP3::Open(RageFileBasic *pFile)
 {
+	RegisterProtocols();
+	m_pFile = pFile;
+
+	//Free everything if it isn't already
+	if (formatCtx)
+		avcodec::avformat_close_input(&formatCtx);  // AVFormatContext is released by avformat_close_input
+	if (IOCtx)
+		avcodec::av_free(IOCtx);             // AVIOContext is released by av_free
+	if (codecCtx)
+		avcodec::avcodec_close(codecCtx);
+	if (codec)
+		avcodec::av_free(codec);
+	if (decodedFrame)
+		avcodec::av_frame_free(&decodedFrame);
+
+	IOCtx = avcodec::avio_alloc_context(buffer, 20480,  // internal Buffer and its size
+		0,                  // bWriteable (1=true,0=false) 
+		pFile,          // user data ; will be passed to our callback functions
+		ReadFunc,
+		0,                  // Write callback function 
+		SeekFunc);
+
+	formatCtx = avcodec::avformat_alloc_context();
+	formatCtx->pb = IOCtx;
+
+
+	// Determining the input format:
+	int readBytes = 0;
+	readBytes += m_pFile->Read(buffer, 20480);
+	m_pFile->Seek(0, SEEK_SET);
+	avcodec::AVProbeData probeData;
+	probeData.buf = buffer;
+	probeData.buf_size = readBytes;
+	probeData.filename = "";
+
+	// Determine the input-format:
+	formatCtx->iformat = av_probe_input_format(&probeData, 1);
+	formatCtx->flags = AVFMT_FLAG_CUSTOM_IO;
+	if (avcodec::avformat_open_input(&formatCtx, "", 0, 0) != 0) {
+		SetError("Error opening file");
+		return OPEN_UNKNOWN_FILE_FORMAT;
+	}
+
+	// Retrieve stream information
+	if (avcodec::avformat_find_stream_info(formatCtx, NULL)<0) {
+		SetError("Couldn't find stream information");
+		return OPEN_UNKNOWN_FILE_FORMAT;
+	}
+
+	// Find the first audio stream
+	audioStream = -1;
+	int i = 0;
+	int nbStreams = formatCtx->nb_streams;
+	for (i = 0; i<nbStreams; i++) {
+		if (formatCtx->streams[i]->codec->codec_type == avcodec::AVMEDIA_TYPE_AUDIO) {
+			audioStream = i;
+			break;
+		}
+	}
+	if (audioStream == -1) {
+		SetError("Didn't find a audio stream");
+		return OPEN_UNKNOWN_FILE_FORMAT;
+	}
+	// Get a pointer to the codec context for the audio stream
+	codecCtx = formatCtx->streams[audioStream]->codec;
+
+	// Find the decoder for the audio stream
+	codec = avcodec::avcodec_find_decoder(codecCtx->codec_id);
+	if (codec == NULL) {
+		SetError("Codec not found\n");
+		return OPEN_UNKNOWN_FILE_FORMAT;
+	}
+
+	if (avcodec::avcodec_open2(codecCtx, codec, NULL) < 0) {
+		SetError("Error opening decoder");
+		return OPEN_UNKNOWN_FILE_FORMAT;
+	}
+	curFrame = 0;
+	curChannel = 0;
+	curSample = 0;
+	numChannels = codecCtx->channels;
+	bitrate = codecCtx->bit_rate;
+	sampleRate = codecCtx->sample_rate; 
+	timeBase = (double)codecCtx->time_base.num * AV_TIME_BASE / (double)codecCtx->time_base.den;
+	length = formatCtx->duration * timeBase;
 	return OPEN_OK;
 }
 
-int newRageSoundReader_MP3::GetLength() const
-{
-	return 0;
-}
 
 newRageSoundReader_MP3 *newRageSoundReader_MP3::Copy() const 
 {
@@ -30,12 +225,136 @@ newRageSoundReader_MP3 *newRageSoundReader_MP3::Copy() const
 	return ret;
 }
 
+//0=> EOF. -1=> Error . >=0 => Properly SetPosition
 int newRageSoundReader_MP3::SetPosition(int iFrame)
 {
-	return 0;
+	if (decodedFrame)
+		avcodec::av_frame_free(&decodedFrame);
+	return avcodec::av_seek_frame(formatCtx, audioStream, (int64_t)iFrame*timeBase, AVSEEK_FLAG_ANY);
 }
 
+//I think this is supposed to read samples, not frames
+//Each sample being a float to be stored in the buffer
+//So we need to read and decode a frame(Each frame has 1 or more channels and each has samples) if needed
+//Then read samples from the frame until the frame finishes or we read enough samples
+//Then either return the samples read or read and decode another frame
 int newRageSoundReader_MP3::Read(float *pBuf, int iFrames)
 {
-	return 0;
+	uint8_t* buf = (uint8_t*)(pBuf);//Increases by 1 byte
+	int samplesRead = 0;
+	if (iFrames <= 0)
+		return iFrames;
+	if (decodedFrame != NULL) {
+		samplesRead += WriteSamplesForAllChannels(buf + samplesRead*numChannels*dataSize, iFrames - samplesRead);
+	}
+	while (samplesRead < iFrames) {
+		if (!decodedFrame)
+			switch (ReadAFrame()) {
+			case -1:
+				return ERROR;
+				break;
+			case -2:
+				SetError("EOF");
+				return END_OF_FILE;
+				break;
+			};
+		samplesRead += WriteSamplesForAllChannels(buf + samplesRead*numChannels*dataSize, iFrames-samplesRead);
+	}
+	//Translate the raw data into something SM or whatever it is that uses it can understand
+	switch (dataSize)
+	{
+	case 1:
+		Convert8bitToFloat(buf, samplesRead*numChannels);
+		break;
+	case 2:
+		ConvertLittleEndian16BitToFloat(buf, samplesRead*numChannels);
+		break;
+	case 3:
+		ConvertLittleEndian24BitToFloat(buf, samplesRead*numChannels);
+		break;
+	case 4:
+		ConvertLittleEndian32BitToFloat(buf, samplesRead*numChannels);
+		/* otherwise 3; already a float */
+		break;
+	}
+	return samplesRead;
+}
+
+//Write samplesToRead samples for each channel.
+//Also, the data is stored as follows: [sample1ch1,sample1ch2,sample1ch2,..]
+int newRageSoundReader_MP3::WriteSamplesForAllChannels(void *pBuf, int samplesToRead)
+{
+	uint8_t *buf = (uint8_t*)(pBuf);
+
+	int samplesWritten = 0;
+	if ((numSamples - curSample) <= samplesToRead) {
+		for (; curSample < numSamples; curSample++) {
+			for (curChannel=0; curChannel < numChannels; curChannel++)
+				memcpy(buf + (samplesWritten*numChannels + curChannel)*dataSize, decodedFrame->data[curChannel] + dataSize*curSample, (unsigned int)dataSize);
+			samplesWritten++;
+		}
+		//Free the frame since we've read it all
+		avcodec::av_frame_free(&decodedFrame);
+		curSample = 0;
+	}
+	else {
+		for (; curSample < samplesToRead; curSample++) {
+			for (curChannel=0; curChannel < numChannels; curChannel++)
+				memcpy(buf + (samplesWritten*numChannels + curChannel)*dataSize, decodedFrame->data[curChannel] + dataSize*curSample, (unsigned int)dataSize);
+			samplesWritten++;
+		}
+	}
+	curFrame += samplesWritten;
+	return samplesWritten;
+
+}
+
+int newRageSoundReader_MP3::GetNextSourceFrame() const {
+	return curFrame;
+}
+//Return: -1 => Error already set. -2 => EOF. >=0 => bytesRead
+int newRageSoundReader_MP3::ReadAFrame()
+{
+	avcodec::AVPacket avpkt;
+	avcodec::av_init_packet(&avpkt);
+	avpkt.data = buffer;
+	if (decodedFrame)
+		avcodec::av_frame_free(&decodedFrame);
+	if (!(decodedFrame = avcodec::av_frame_alloc())) {
+		SetError("Error allocating memory for frame");
+		return -1;
+	}
+	while (avcodec::av_read_frame(formatCtx, &avpkt) >= 0) {
+	//while (avpkt.size > 0) {
+		avpkt.dts =
+			avpkt.pts = AV_NOPTS_VALUE;
+		if (avpkt.stream_index == audioStream) {
+			int size = avpkt.size;
+			while (avpkt.size > 0) {
+				int gotFrame = 0;
+				int len = avcodec_decode_audio4(codecCtx, decodedFrame, &gotFrame, &avpkt);
+				if (len == -1) {
+					SetError("Error while decoding\n");
+					return -1;
+				}
+				//avpkt.size -= len;
+				//avpkt.data += len;
+				if (gotFrame) {
+					curChannel = 0;
+					curSample = 0;
+					numSamples = decodedFrame->nb_samples;
+					dataSize = avcodec::av_get_bytes_per_sample(codecCtx->sample_fmt);
+					numChannels = codecCtx->channels;
+					//m_pFile->Seek(m_pFile->Tell() - avpkt.size);
+					//return bytesRead-avpkt.size;
+					int read = avpkt.size;
+					av_free_packet(&avpkt);
+					return read;
+				}
+			}
+			break;
+		}
+	}
+	av_free_packet(&avpkt);
+	return -2;
 }
--- src/newRageSoundReader_MP3.h
+++ src/newRageSoundReader_MP3.h
@@ -6,24 +6,59 @@
 #include "RageSoundReader_FileReader.h"
 #include "RageFile.h"
 
+namespace avcodec
+{
+	extern "C"
+	{
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/pixdesc.h>
+#include <libavcodec/avcodec.h>
+	}
+};
+
+
+void RegisterProtocols();
+
 class newRageSoundReader_MP3 :
 	public RageSoundReader_FileReader
 {
 public:
 	OpenResult Open(RageFileBasic *pFile); 
 	void Close();
-	int GetLength() const;
+	int GetLength() const { return length; }
 	int SetPosition(int iFrame);
+	int aux;
 	int Read(float *pBuf, int iSample);
-	int GetSampleRate() const { return 0; };
-	unsigned GetNumChannels() const { return 0; };
-	int GetNextSourceFrame() const { return 0; };
+	int GetSampleRate() const { return sampleRate; }
+	unsigned GetNumChannels() const { return numChannels; }
+	int GetNextSourceFrame() const ;
 	newRageSoundReader_MP3();
 	~newRageSoundReader_MP3();
 	newRageSoundReader_MP3(const newRageSoundReader_MP3 &); /* not defined; don't use */
 	newRageSoundReader_MP3 *Copy() const;
 private:
-	int SampleRate;
+	int sampleRate;
+	int length;
+	uint8_t buffer[20480 + 64];
+	int audioStream;
+	int numChannels;
+	avcodec::AVIOContext* IOCtx;
+	avcodec::AVFormatContext* formatCtx;
+	int numSamples; 
+	int WriteSamplesForAllChannels(void *pBuf, int samplesToRead);
+	int bitrate;
+	int dataSize;
+	int WriteFrame(char *pBuf);
+	int curFrame;
+	int curSample; 
+	double timeBase;
+	int curChannel;
+	avcodec::AVCodec *codec; 
+	avcodec::AVCodecContext *codecCtx;
+	avcodec::AVFrame *decodedFrame;
+	int WriteSamples(void *pBuf, int samples);
+	int ReadAFrame();
 };
 
 #endif
