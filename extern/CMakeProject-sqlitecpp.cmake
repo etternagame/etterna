7ac45cfec8429ecb71ff08a32d69172875eb20c0
Nickito12 <abramlujan@gmail.com>
2017-09-14 13:10:51 -0300

    Link sqlitecpp

---
+++ extern/CMakeProject-sqlitecpp.cmake
@@ -0,0 +1,5 @@
+
+add_subdirectory(SQLiteCpp)
+set_property(TARGET "SQLiteCpp" PROPERTY FOLDER "External Libraries")
+set_property(TARGET "SQLiteCpp_cpplint" PROPERTY FOLDER "External Libraries")
+set_property(TARGET "sqlite3" PROPERTY FOLDER "External Libraries")
