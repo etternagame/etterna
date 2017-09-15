c7addfc4e62ba2b4897eccbf812db4e1cf39ec12
MinaciousGrace <minaciousgrace@gmail.com>
2017-09-15 01:34:09 -0400

    don't write a noccpbkey unless it's different from the pbkey

--- src/ScoreManager.cpp
+++ src/ScoreManager.cpp
@@ -390,7 +390,7 @@ XNode* ScoresAtRate::CreateNode(const int& rate) const {
 	string rs = ssprintf("%.3f", static_cast<float>(rate) / 10000.f);
 	// should be safe as this is only called if there is at least 1 score (which would be the pb)
 	o->AppendAttr("PBKey", PBptr->GetScoreKey());
-	if(noccPBptr != nullptr)
+	if(noccPBptr != nullptr && PBptr->GetScoreKey() != noccPBptr->GetScoreKey())	// don't write unless it's different from the pbkey -mina
 		o->AppendAttr("noccPBKey", noccPBptr->GetScoreKey());
 	o->AppendAttr("BestGrade", GradeToString(bestGrade));
 	o->AppendAttr("Rate", rs);
