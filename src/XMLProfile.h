1b390c368c33ede7d812a26d600c5707acf9573b
Nickito12 <abramlujan@gmail.com>
2017-09-17 20:37:09 -0300

    Move xml profile loading/saving to XMLProfile

---
+++ src/XMLProfile.h
@@ -0,0 +1,64 @@
+#ifndef Profile_XML
+#define Profile_XML
+
+#include "global.h"
+#include "GameConstantsAndTypes.h"
+#include "HighScore.h"
+#include "XmlFile.h"
+#include "Profile.h"
+#include "XmlFileUtil.h"
+
+
+class XNode;
+
+class XMLProfile {
+public:
+	static void MoveBackupToDir(const RString &sFromDir, const RString &sToDir);
+	// For converting to etterna from stats.xml
+	void LoadStatsXmlForConversion();
+	ProfileLoadResult LoadStatsXmlFromNode(const XNode* pNode, bool bIgnoreEditable = true);
+	// Etterna profile
+	ProfileLoadResult LoadEttFromDir(RString dir);
+	ProfileLoadResult LoadEttXmlFromNode(const XNode* pNode);
+
+	void LoadEttGeneralDataFromNode(const XNode* pNode);
+	void LoadEttScoresFromNode(const XNode* pNode);
+	void LoadFavoritesFromNode(const XNode *pNode);
+	void LoadPermaMirrorFromNode(const XNode *pNode);
+	void LoadScoreGoalsFromNode(const XNode *pNode);
+	void LoadPlaylistsFromNode(const XNode *pNode);
+	void LoadGeneralDataFromNode(const XNode* pNode);
+	void LoadSongScoresFromNode(const XNode* pNode);
+	void LoadCategoryScoresFromNode(const XNode* pNode);
+	void LoadScreenshotDataFromNode(const XNode* pNode);
+	ProfileLoadResult LoadStatsFromDir(RString dir, bool require_signature);
+
+	bool SaveStatsXmlToDir(RString sDir, bool bSignData);
+	bool SaveEttXmlToDir(RString sDir) const;
+	bool SaveStatsXmlToDir(RString sDir, bool bSignData) const;
+
+	XNode* SaveEttGeneralDataCreateNode() const;
+	XNode* SaveEttScoresCreateNode() const;
+	XNode* SaveEttXmlCreateNode() const;
+
+	Profile* profile;
+
+	XNode* SaveFavoritesCreateNode() const;
+	XNode* SavePermaMirrorCreateNode() const;
+	XNode* SaveScoreGoalsCreateNode() const;
+	XNode* SavePlaylistsCreateNode() const;
+
+	XNode* SaveStatsXmlCreateNode() const;
+
+	XNode* SaveGeneralDataCreateNode() const;
+	XNode* SaveSongScoresCreateNode() const;
+
+	XNode* SaveCategoryScoresCreateNode() const;
+	XNode* SaveScreenshotDataCreateNode() const;
+
+	XNode* SaveCoinDataCreateNode() const;
+	RString profiledir;
+};
+
+
+#endif
