5ca657e23ea15300f0117888a32ee7f0fbdb780d
Unknown <minaciousgrace@gmail.com>
2017-11-20 06:18:06 -0500

    update version tags/splashs

--- src/GameState.h
+++ src/GameState.h
@@ -214,7 +214,7 @@ public:
 	bool		m_bLoadingNextSong;
 	int		GetLoadingCourseSongIndex() const;
 
-	RString GetEtternaVersion() { return "0.57.1"; }
+	RString GetEtternaVersion() { return "0.58.0"; }
 	bool isplaylistcourse = false;
 	bool IsPlaylistCourse() { return isplaylistcourse; }
 	bool CountNotesSeparately();
