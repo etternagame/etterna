7b253b1bbd991706af03317c0e06bc4faa721ebc
Nicolas <abramlujan@gmail.com>
2018-02-06 02:53:14 -0300

    Remove PB-only xml

--- src/ScoreManager.cpp
+++ src/ScoreManager.cpp
@@ -486,12 +486,9 @@ XNode* ScoresAtRate::CreateNode(const int& rate) const {
 
 	// prune out sufficiently low scores
 	FOREACHUM_CONST(string, HighScore, scores, i) {
-		if (i->second.GetScoreKey() == PBptr->GetScoreKey()) {
-			if (i->second.GetWifeScore() > SCOREMAN->minpercent) {
-				o->AppendChild(i->second.CreateEttNode());
-				saved++;
-			}
-			break;
+		if (i->second.GetWifeScore() > SCOREMAN->minpercent) {
+			o->AppendChild(i->second.CreateEttNode());
+			saved++;
 		}
 	}
 
