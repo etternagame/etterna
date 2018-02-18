0bff457a1ddb29cf32c4b5fc3b329848cfd87d05
Unknown <foxfire667@gmail.com>
2018-02-18 16:07:34 -0500

    [Theme] Screen Player Options
    File has been added so that the background can be displayed on the song select options screen.

---
+++ Themes/Til Death/BGAnimations/ScreenPlayerOptions underlay.lua
@@ -0,0 +1,11 @@
+t = Def.ActorFrame {};
+
+local frameX = THEME:GetMetric("ScreenTitleMenu","ScrollerX")-10
+local frameY = THEME:GetMetric("ScreenTitleMenu","ScrollerY")
+
+t[#t+1] = LoadActor(THEME:GetPathG("","_OptionsScreen")) .. {
+	InitCommand=function(self)
+		self:xy(capWideScale(get43size(SCREEN_WIDTH-10),SCREEN_WIDTH-425),frameY-70):zoom(0.8):diffusealpha(1):baserotationx(0)
+	end	
+}
+return t
