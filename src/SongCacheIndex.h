13b610841b86a680be1eadb1e8ab33aa5c911c55
Nicolas <abramlujan@gmail.com>
2018-04-02 03:57:05 -0300

    Pass by reference for efficiency

--- src/SongCacheIndex.h
+++ src/SongCacheIndex.h
@@ -44,7 +44,7 @@ public:
 	unsigned GetCacheHash( const RString &path ) const;
 	bool delay_save_cache;
 	
-	int64_t InsertStepsTimingData(TimingData timing);
+	int64_t InsertStepsTimingData(TimingData &timing);
 	int64_t InsertSteps(const Steps* pSteps, int64_t songID);
 	bool LoadSongFromCache(Song* song, string dir);
 	bool CacheSong(Song& song, string dir);
