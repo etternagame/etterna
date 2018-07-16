938f296689ecc7bbd3e86af9499349618fb98c48
Unknown <minaciousgrace@gmail.com>
2018-07-16 00:03:17 -0400

    overhaul offsetplot lua files
    - screenscoretaboffsetplot loads offsetplot.lua now instead of being self contained (aka redundant) and the cases for loading from scores vs pss and display changes are handled explicitly
    
    - this includes custom scoring window code so the scoretab offsets can now make use of that (probably i haven't tested it)
    
    - various internally redundant code was removed
    
    - killed init commands since most of the params are no longer static; everything just updates to judgedisplay message commands
    
    - mp evaluation stuff wasn't touched and is probably very very broken, screenonlinescoretab is also very redundant with this and should be replaced but i'll let nick handle that case

--- Themes/Til Death/BGAnimations/offsetplot.lua
+++ Themes/Til Death/BGAnimations/offsetplot.lua
@@ -1,5 +1,4 @@
--- Note this file is different from the scoretaboffsetplot because it takes values from pss and not a highscore object
--- this diffrentiation should probably be handled in a single file to reduce code redundancy -mina
+-- updated to handle both immediate evaluation when pulling data from pss (doesnt require invoking calls to loadreplay data) and scoretab plot construction (does) -mina
 
 local judges = { "marv", "perf", "great", "good", "boo", "miss" }
 local tst = { 1.50,1.33,1.16,1.00,0.84,0.66,0.50,0.33,0.20 }
@@ -16,15 +15,16 @@ local plotWidth, plotHeight = 400,120
 local plotX, plotY = SCREEN_WIDTH - 9 - plotWidth/2, SCREEN_HEIGHT - 56 - plotHeight/2
 local dotDims, plotMargin = 2, 4
 local maxOffset = math.max(180, 180*tso)
+local bgalpha = 0.8
 
-local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1)
-local dvt = pss:GetOffsetVector()
-local nrt = pss:GetNoteRowVector()
-local ctt = pss:GetTrackVector()		-- column information for each offset
-local ntt = pss:GetTapNoteTypeVector()	-- notetype information (we use this to handle mine hits differently- currently that means not displaying them)
-local td = GAMESTATE:GetCurrentSteps(PLAYER_1):GetTimingData()
+-- initialize tables we need for replay data here, we don't know where we'll be loading from yet
+local dvt = {}
+local nrt = {}
+local ctt = {}
+local ntt = {}
 local wuab = {}
 local finalSecond = GAMESTATE:GetCurrentSong(PLAYER_1):GetLastSecond()
+local td = GAMESTATE:GetCurrentSteps(PLAYER_1):GetTimingData()
 
 local function fitX(x)	-- Scale time values to fit within plot width.
 	return x/finalSecond*plotWidth - plotWidth/2
@@ -35,9 +35,37 @@ local function fitY(y)	-- Scale offset values to fit within plot height
 end
 
 local o = Def.ActorFrame{
-	InitCommand=function(self)
+	OnCommand=function(self)
 		self:xy(plotX,plotY)
-		MESSAGEMAN:Broadcast("JudgeDisplayChanged")		-- prim really handled all this much more elegantly
+		
+		-- being explicit about the logic since atm these are the only 2 cases we handle
+		if SCREENMAN:GetTopScreen():GetName() == "ScreenEvaluationNormal" then		-- default case, all data is in pss and no disk load is required
+			local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1)
+			dvt = pss:GetOffsetVector()
+			nrt = pss:GetNoteRowVector()
+			ctt = pss:GetTrackVector()					-- column information for each offset
+			ntt = pss:GetTapNoteTypeVector()			-- notetype information (we use this to handle mine hits differently- currently that means not displaying them)
+			pss:UnloadReplayData()						-- force unload replaydata in memory after loading it (not sure if i should allow this but i don't trust deconstructors) -mina
+		end
+		
+		if SCREENMAN:GetTopScreen():GetName() == "ScreenScoreTabOffsetPlot" then	-- loaded from scoretab not eval so we need to read from disk and adjust plot display
+			plotWidth, plotHeight = SCREEN_WIDTH,SCREEN_WIDTH*0.3
+			self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y)
+			bgalpha = 1
+			
+			-- the internals here are really inefficient this should be handled better (internally) -mina
+			local score = getScoreForPlot()
+			dvt = score:GetOffsetVector()
+			nrt = score:GetNoteRowVector()
+			ctt = score:GetTrackVector()
+			ntt = score:GetTapNoteTypeVector()
+		end
+		
+		-- Convert noterows to timestamps and plot dots (this is important it determines plot x values!!!)
+		for i=1,#nrt do
+			wuab[i] = td:GetElapsedTimeFromNoteRow(nrt[i])
+		end
+		MESSAGEMAN:Broadcast("JudgeDisplayChanged")		-- prim really handled all this much more elegantly		
 	end,
 	CodeMessageCommand=function(self,params)
 		if enabledCustomWindows then
@@ -62,7 +90,7 @@ local o = Def.ActorFrame{
 		maxOffset = (enabledCustomWindows and judge ~= 0) and customWindow.judgeWindows.boo or math.max(180, 180*tso)
 		MESSAGEMAN:Broadcast("JudgeDisplayChanged")
 	end,
-	UpdateNetEvalStatsMessageCommand = function(self)
+	UpdateNetEvalStatsMessageCommand = function(self)		-- i haven't updated or tested neteval during last round of work -mina
 		local s = SCREENMAN:GetTopScreen():GetHighScore()
 		if s then
 			score = s
@@ -77,7 +105,7 @@ local o = Def.ActorFrame{
 }
 -- Center Bar
 o[#o+1] = Def.Quad{
-	InitCommand=function(self)
+	JudgeDisplayChangedMessageCommand=function(self)
 		self:zoomto(plotWidth+plotMargin,1):diffuse(byJudgment("TapNoteScore_W1"))
 	end
 }
@@ -85,32 +113,24 @@ local fantabars = {22.5, 45, 90, 135}
 local bantafars = {"TapNoteScore_W2", "TapNoteScore_W3", "TapNoteScore_W4", "TapNoteScore_W5"}
 for i=1, #fantabars do 
 	o[#o+1] = Def.Quad{
-		InitCommand=function(self)
-			self:y( fitY(tso*fantabars[i])):zoomto(plotWidth+plotMargin,1):diffuse(byJudgment(bantafars[i]))
-		end,
 		JudgeDisplayChangedMessageCommand=function(self)
+			self:zoomto(plotWidth+plotMargin,1):diffuse(byJudgment(bantafars[i]))
 			local fit = (enabledCustomWindows and judge ~= 0) and customWindow.judgeWindows[judges[i]] or tso*fantabars[i]
 			self:y( fitY(fit))
-		end,
+		end
 	}
 	o[#o+1] = Def.Quad{
-		InitCommand=function(self)
-			self:y( fitY(-tso*fantabars[i])):zoomto(plotWidth+plotMargin,1):diffuse(byJudgment(bantafars[i]))
-		end,
 		JudgeDisplayChangedMessageCommand=function(self)
+			self:zoomto(plotWidth+plotMargin,1):diffuse(byJudgment(bantafars[i]))
 			local fit = (enabledCustomWindows and judge ~= 0) and customWindow.judgeWindows[judges[i]] or tso*fantabars[i]
 			self:y( fitY(-fit))
-		end,
+		end
 	}
 end
 -- Background
-o[#o+1] = Def.Quad{InitCommand=function(self)
-	self:zoomto(plotWidth+plotMargin,plotHeight+plotMargin):diffuse(color("0.05,0.05,0.05,0.05")):diffusealpha(0.8)
+o[#o+1] = Def.Quad{JudgeDisplayChangedMessageCommand=function(self)
+	self:zoomto(plotWidth+plotMargin,plotHeight+plotMargin):diffuse(color("0.05,0.05,0.05,0.05")):diffusealpha(bgalpha)
 end}
--- Convert noterows to timestamps and plot dots
-for i=1,#nrt do
-	wuab[i] = td:GetElapsedTimeFromNoteRow(nrt[i])
-end
 
 local dotWidth = dotDims / 2
 o[#o+1] = Def.ActorMultiVertex{
@@ -120,11 +140,14 @@ o[#o+1] = Def.ActorMultiVertex{
 			local x = fitX(wuab[i])
 			local y = fitY(dvt[i])
 			local fit = (enabledCustomWindows and judge ~= 0) and customWindow.judgeWindows.boo + 3 or math.max(183, 183*tso)
-			local color = (enabledCustomWindows and judge ~= 0) and customOffsetToJudgeColor(dvt[i], customWindow.judgeWindows) or offsetToJudgeColor(dvt[i]/1000, tst[judge])
+			local color = (enabledCustomWindows and judge ~= 0) and customOffsetToJudgeColor(dvt[i], customWindow.judgeWindows) or offsetToJudgeColor(dvt[i], tst[judge])
 			if math.abs(y) > plotHeight/2 then
 				y = fitY(fit)
 			end
-			if ntt[i] ~= "TapNoteType_Mine" then						-- screen out mines for now, we can maybe use a separate graphic or display later -mina
+			
+			-- screen out mines for now, we can maybe use a separate graphic or display later -mina
+			-- i would have thought this would vomit errors for files without taptype data but i guess not?
+			if ntt[i] ~= "TapNoteType_Mine" then						
 				verts[#verts+1] = {{x-dotWidth,y+dotWidth,0}, color}
 				verts[#verts+1] = {{x+dotWidth,y+dotWidth,0}, color}
 				verts[#verts+1] = {{x+dotWidth,y-dotWidth,0}, color}
@@ -138,18 +161,14 @@ o[#o+1] = Def.ActorMultiVertex{
 
 -- Early/Late markers
 o[#o+1] = LoadFont("Common Normal")..{
-	InitCommand=function(self)
-		self:xy(-plotWidth/2,-plotHeight/2+2):settextf("Late (+%ims)", maxOffset):zoom(0.35):halign(0):valign(0)
-	end,
 	JudgeDisplayChangedMessageCommand=function(self)
+		self:xy(-plotWidth/2,-plotHeight/2+2):settextf("Late (+%ims)", maxOffset):zoom(0.35):halign(0):valign(0)
 		self:settextf("Late (+%ims)", maxOffset)
 	end
 }
 o[#o+1] = LoadFont("Common Normal")..{
-	InitCommand=function(self)
-		self:xy(-plotWidth/2,plotHeight/2-2):settextf("Early (-%ims)", maxOffset):zoom(0.35):halign(0):valign(1)
-	end,
 	JudgeDisplayChangedMessageCommand=function(self)
+		self:xy(-plotWidth/2,plotHeight/2-2):settextf("Early (-%ims)", maxOffset):zoom(0.35):halign(0):valign(1)
 		self:settextf("Early (-%ims)", maxOffset)
 	end
 }
