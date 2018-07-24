71ac7ffc33edf396bd0798d9bc2bc7de42f3d9d4
Nicolas <abramlujan@gmail.com>
2018-07-24 19:51:46 -0300

    Make the stepsdisplaylist rows clickable

--- Themes/Til Death/Graphics/StepsDisplayListRow frame.lua
+++ Themes/Til Death/Graphics/StepsDisplayListRow frame.lua
@@ -1,4 +1,4 @@
-t = Def.ActorFrame{}
+t = Def.ActorFrame{ }
 
 
 t[#t+1] = Def.Quad{
@@ -11,6 +11,19 @@ t[#t+1] = Def.Quad{
 	InitCommand=function(self)
 		self:x(22):zoomto(66,22):diffuse(color("#ffffff")):diffusealpha(0.5)
 	end;
+	MouseLeftClickMessageCommand=function(self)
+		if isOver(self) then
+			local s = GAMESTATE:GetCurrentSong()
+			if s then
+				local index = math.floor((getTrueY(self)-70)/24)+1
+				local allSteps = SongUtil.GetPlayableSteps(s)
+				local steps = allSteps[index]
+				if steps then
+					GAMESTATE:SetCurrentSteps(PLAYER_1, steps)
+				end
+			end
+		end
+	end,
 };
 
 
