7284467532a83ad3d85fa18617b552cf994ce43a
Unknown <minaciousgrace@gmail.com>
2018-07-24 10:24:40 -0400

    default to bundles only if there are no loaded songs

--- Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/downloads.lua
+++ Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/downloads.lua
@@ -9,7 +9,7 @@ local t = Def.ActorFrame{
 		self:bouncebegin(0.2):xy(0,0):diffusealpha(1)
 	end,
 	TabChangedMessageCommand=function(self)
-		if getTabIndex() == 8 and SONGMAN:GetNumSongs() < 300 then
+		if getTabIndex() == 8 and SONGMAN:GetNumSongs() == 0 then
 			SCREENMAN:SetNewScreen("ScreenBundleSelect")
 		elseif getTabIndex() == 8 then
 			SCREENMAN:SetNewScreen("ScreenPackDownloader")
