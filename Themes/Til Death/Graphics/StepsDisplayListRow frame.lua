5bb8ab533480da5e8b552ab6e4acd4b36eabcd1d
Unknown <minaciousgrace@gmail.com>
2018-07-29 13:28:13 -0400

    fix theme recursion error when switching to min steps diff

--- Themes/Til Death/Graphics/StepsDisplayListRow frame.lua
+++ Themes/Til Death/Graphics/StepsDisplayListRow frame.lua
@@ -1,16 +1,16 @@
-t = Def.ActorFrame{ }
+t = Def.ActorFrame{}
 
 
 t[#t+1] = Def.Quad{
 	InitCommand=function(self)
 		self:zoomto(22,22):diffuse(color("#ffffff")):diffusealpha(0.7)
 	end;
-};
+}
 
 t[#t+1] = Def.Quad{
 	InitCommand=function(self)
 		self:x(22):zoomto(66,22):diffuse(color("#ffffff")):diffusealpha(0.5)
-	end;
+	end,
 	MouseLeftClickMessageCommand=function(self)
 		if isOver(self) then
 			local s = GAMESTATE:GetCurrentSong()
@@ -18,13 +18,13 @@ t[#t+1] = Def.Quad{
 				local index = math.floor((getTrueY(self)-70)/24)+1
 				local allSteps = SongUtil.GetPlayableSteps(s)
 				local steps = allSteps[index]
-				if steps then
+				if steps and steps ~= GAMESTATE:GetCurrentSteps(PLAYER_1) then
 					GAMESTATE:SetCurrentSteps(PLAYER_1, steps)
 				end
 			end
 		end
 	end,
-};
+}
 
 
 
