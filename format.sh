ef30777cd491d0c2ac256ac708241573ee2f1b23
Nicolas <abramlujan@gmail.com>
2018-09-17 03:02:14 -0300

    Ignore extern in format.sh

--- format.sh
+++ format.sh
@@ -1,8 +1,10 @@
-find "src" \( -name '*.h' -or -name '*.cpp' \) -print0 | xargs -0 "clang-format" -i -style=file
+find "src" \( -name '*.h' -or -name '*.cpp' \) -print0 | 
+	awk '/src/' | awk '!/extern/' | 
+	grep '^src' | grep -E '*\.(cpp|h)' | 
+	xargs -0 "clang-format" -i -style=file
 
 filelist=`git ls-files --full-name | awk '/src/' | awk '!/extern/' | grep '^src' | grep -E '*\.(cpp|h)'`
 
 for f in $filelist; do
-echo "${f}"
 	clang-format -i -style=file "${f}"
 done
