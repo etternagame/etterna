fbf1d3d8f2317147d808977ff47e8db31409b2b3
Unknown <minaciousgrace@gmail.com>
2018-10-31 21:48:32 -0400

    fix reverse offset for chart preview

--- Themes/Til Death/BGAnimations/_chartpreview.lua
+++ Themes/Til Death/BGAnimations/_chartpreview.lua
@@ -1,7 +1,7 @@
 -- all the preview stuff should be var'd and used consistently -mina
 local noteField = false
 local usingreverse = GAMESTATE:GetPlayerState(PLAYER_1):GetCurrentPlayerOptions():UsingReverse()
-local prevrevY = 225
+local prevrevY = 300
 local prevZoom = 0.65
 local musicratio = 1
 
