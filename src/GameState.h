fd8e5dda5f30119b9a9a814f32ebf07d460de112
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2018-11-06 10:36:25 -0500

    iterate version tags to 0.62

--- src/GameState.h
+++ src/GameState.h
@@ -1,4 +1,4 @@
-﻿#ifndef GAMESTATE_H
+#ifndef GAMESTATE_H
 #define GAMESTATE_H
 
 #include "Difficulty.h"
@@ -224,7 +224,7 @@ class GameState
 	bool m_bLoadingNextSong;
 	int GetLoadingCourseSongIndex() const;
 
-	RString GetEtternaVersion() { return "0.61.0"; }
+	RString GetEtternaVersion() { return "0.62.0"; }
 	bool isplaylistcourse = false;
 	bool IsPlaylistCourse() { return isplaylistcourse; }
 	bool CountNotesSeparately();
