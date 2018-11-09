92e9868337f7d12d38b7b6060ed897cee0118ccb
poco0317 <poco0317@users.noreply.github.com>
2018-11-09 14:03:03 -0600

    Fix permamirror breaking replays

--- Themes/Til Death/BGAnimations/ScreenGameplay underlay/default.lua
+++ Themes/Til Death/BGAnimations/ScreenGameplay underlay/default.lua
@@ -3,7 +3,8 @@ local modslevel = topscreen == "ScreenEditOptions" and "ModsLevel_Stage" or "Mod
 local playeroptions = GAMESTATE:GetPlayerState(PLAYER_1):GetPlayerOptions(modslevel)
 playeroptions:Mini(2 - playerConfig:get_data(pn_to_profile_slot(PLAYER_1)).ReceptorSize / 50)
 local profile = PROFILEMAN:GetProfile(PLAYER_1)
-if profile:IsCurrentChartPermamirror() then -- turn on mirror if song is flagged as perma mirror
+local replaystate = GAMESTATE:GetPlayerState(PLAYER_1):GetPlayerController() == "PlayerController_Replay"
+if profile:IsCurrentChartPermamirror() and not replaystate then -- turn on mirror if song is flagged as perma mirror
 	playeroptions:Mirror(true)
 end
 
