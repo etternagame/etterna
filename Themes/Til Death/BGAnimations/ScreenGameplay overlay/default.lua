6c1790e1b0995f8fabcac8ac590de99c3930da6e
poco0317 <poco0317@users.noreply.github.com>
2018-11-13 21:02:31 -0600

    Mouse visibility in customize gameplay & replays (fullscreen)

--- Themes/Til Death/BGAnimations/ScreenGameplay overlay/default.lua
+++ Themes/Til Death/BGAnimations/ScreenGameplay overlay/default.lua
@@ -1,12 +1,17 @@
 -- Everything relating to the gameplay screen is gradually moved to WifeJudgmentSpotting.lua
+local inReplay = GAMESTATE:GetPlayerState(PLAYER_1):GetPlayerController() == "PlayerController_Replay"
+local inCustomize = playerConfig:get_data(pn_to_profile_slot(PLAYER_1)).CustomizeGameplay
 local t = Def.ActorFrame {}
 t[#t + 1] = LoadActor("WifeJudgmentSpotting")
 t[#t + 1] = LoadActor("titlesplash")
 t[#t + 1] = LoadActor("leaderboard")
-if GAMESTATE:GetPlayerState(PLAYER_1):GetPlayerController() == "PlayerController_Replay" then
+if inReplay then
 	t[#t + 1] = LoadActor("replayscrolling")
 end
-if playerConfig:get_data(pn_to_profile_slot(PLAYER_1)).CustomizeGameplay then
+if inCustomize then
 	t[#t + 1] = LoadActor("messagebox")
+end
+if inReplay or inCustomize then
+	t[#t + 1] = LoadActor("../_cursor")
 end
 return t
