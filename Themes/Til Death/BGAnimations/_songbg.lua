bf195c03191c73ed17f84795031194c1991c208e
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2018-11-24 16:22:36 -0500

    cleanup old stuff in songbg

--- Themes/Til Death/BGAnimations/_songbg.lua
+++ Themes/Til Death/BGAnimations/_songbg.lua
@@ -1,7 +1,3 @@
-local magnitude = 0.03
-local maxDistX = SCREEN_WIDTH * magnitude
-local maxDistY = SCREEN_HEIGHT * magnitude
-
 local enabled = themeConfig:get_data().global.SongBGEnabled
 local brightness = 0.3
 
@@ -13,21 +9,12 @@ if enabled then
 			CurrentSongChangedMessageCommand = function(self)
 				self:finishtweening():smooth(0.5):diffusealpha(0):sleep(0.2):queuecommand("ModifySongBackground")
 			end,
-			BeginCommand = function(self)
-				self:scaletocover(0, 0, SCREEN_WIDTH + maxDistX / 4, SCREEN_BOTTOM + maxDistY / 4):diffusealpha(0.3)
-			end,
 			ModifySongBackgroundCommand = function(self)
 				self:finishtweening()
-				collectgarbage()
 				if GAMESTATE:GetCurrentSong() and GAMESTATE:GetCurrentSong():GetBackgroundPath() then
 					self:finishtweening()
 					self:visible(true)
 					self:LoadBackground(GAMESTATE:GetCurrentSong():GetBackgroundPath())
-					if moveBG then
-						self:scaletocover(0 - maxDistY / 8, 0 - maxDistY / 8, SCREEN_WIDTH + maxDistX / 8, SCREEN_BOTTOM + maxDistY / 8)
-					else
-						self:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_BOTTOM)
-					end
 					self:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_BOTTOM)
 					self:sleep(0.25)
 					self:smooth(0.5)
