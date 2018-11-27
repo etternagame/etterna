676bc31cad801245b0c7c6092d165adc68dc6aad
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2018-11-27 01:26:40 -0500

    auto iterate to 0.64.0

--- src/GameState.h
+++ src/GameState.h
@@ -224,7 +224,7 @@ class GameState
 	bool m_bLoadingNextSong;
 	int GetLoadingCourseSongIndex() const;
 
-	RString GetEtternaVersion() { return "0.63.0"; }
+	RString GetEtternaVersion() { return "0.64.0"; }
 	bool isplaylistcourse = false;
 	bool IsPlaylistCourse() { return isplaylistcourse; }
 	bool CountNotesSeparately();
