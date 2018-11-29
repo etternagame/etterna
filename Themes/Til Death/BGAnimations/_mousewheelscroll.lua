ea54daf2554c00afbc5312d3c8327e8cac54d870
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2018-11-29 02:12:35 -0500

    refactor mousescroll a bit and fix multi use when songs are selected

--- Themes/Til Death/BGAnimations/_mousewheelscroll.lua
+++ Themes/Til Death/BGAnimations/_mousewheelscroll.lua
@@ -4,37 +4,31 @@ local pressingtab = false
 local top
 
 local function scrollInput(event)
-	if top:GetSelectionState() == 2 then
-		return
+	if top:GetName() == "ScreenSelectmusic" then
+		if top:GetSelectionState() == 2 then
+			return
+		end	
 	elseif event.DeviceInput.button == "DeviceButton_tab" then
 		if event.type == "InputEventType_FirstPress" then
 			pressingtab = true
 		elseif event.type == "InputEventType_Release" then
 			pressingtab = false
 		end
-	elseif
-		event.DeviceInput.button == "DeviceButton_mousewheel up" and event.type == "InputEventType_FirstPress" and
-			top:GetSelectionState() ~= 2
-	 then
-		moving = true
-		if pressingtab == true then
-			whee:Move(-2)
-		elseif whee:IsSettled() then
-			whee:Move(-1)
-		else
-			whee:Move(-1)
-		end
-	elseif
-		event.DeviceInput.button == "DeviceButton_mousewheel down" and event.type == "InputEventType_FirstPress" and
-			top:GetSelectionState() ~= 2
-	 then
-		moving = true
-		if pressingtab == true then
-			whee:Move(2)
-		elseif whee:IsSettled() then
-			whee:Move(1)
-		else
-			whee:Move(1)
+	elseif event.type == "InputEventType_FirstPress" then
+		if event.DeviceInput.button == "DeviceButton_mousewheel up" then
+			moving = true
+			if pressingtab == true and not whee:IsSettled() then
+				whee:Move(2)
+			else
+				whee:Move(1)
+			end
+		elseif event.DeviceInput.button == "DeviceButton_mousewheel down" then
+			moving = true
+			if pressingtab == true and not whee:IsSettled() then
+				whee:Move(2)
+			else
+				whee:Move(1)
+			end
 		end
 	elseif moving == true then
 		whee:Move(0)
