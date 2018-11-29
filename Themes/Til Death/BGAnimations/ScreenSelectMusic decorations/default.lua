5e6c27d6cbf4c42e3335668a187a8f349da01cf5
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2018-11-29 08:51:57 -0500

    load tabs first at music select for excellent mega good reasons

--- Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/default.lua
+++ Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/default.lua
@@ -1,9 +1,9 @@
 local t = Def.ActorFrame {}
 
+t[#t + 1] = LoadActor("tabs")
 t[#t + 1] = LoadActor("wifetwirl")
 t[#t + 1] = LoadActor("msd")
 t[#t + 1] = LoadActor("songsearch")
-t[#t + 1] = LoadActor("tabs")
 t[#t + 1] = LoadActor("songinfo")
 t[#t + 1] = LoadActor("score")
 t[#t + 1] = LoadActor("profile")
