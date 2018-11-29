9a39605cdf4b37bf9a866be80ee7e19f2bc8b73b
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2018-11-29 09:15:45 -0500

    turns out we needed this one

--- Themes/Til Death/BGAnimations/ScreenNetSelectMusic decorations.lua
+++ Themes/Til Death/BGAnimations/ScreenNetSelectMusic decorations.lua
@@ -1,2 +1,9 @@
 local t = LoadActor("ScreenSelectMusic decorations/default")
+	t[#t + 1] =
+		Def.ActorFrame {
+		BeginCommand = function(self)
+			SCREENMAN:GetTopScreen():AddInputCallback(MPinput)
+		end
+	}
+	
 return t
