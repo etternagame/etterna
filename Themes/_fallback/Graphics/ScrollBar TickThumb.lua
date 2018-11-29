6c5818d17b9b1f1d925a2a930e45e3a601f60cdb
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2018-11-29 08:51:09 -0500

    theme errors

--- Themes/_fallback/Graphics/ScrollBar TickThumb.lua
+++ Themes/_fallback/Graphics/ScrollBar TickThumb.lua
@@ -4,16 +4,20 @@ t[#t + 1] =
 	Def.ActorFrame {
 	Def.Quad {
 		Name = "DootyMcBooty",
-		OnCommand = function(self)
+		BeginCommand = function(self)
 			self:zoomto(32, 32):valign(0.634522134234)
-			whee = SCREENMAN:GetTopScreen():GetMusicWheel()
+			if SCREENMAN:GetTopScreen():GetName() == "ScreenSelectMusic" or SCREENMAN:GetTopScreen():GetName() == "ScreenNetSelectMusic" then
+				whee = SCREENMAN:GetTopScreen():GetMusicWheel()
+			end
 		end,
 		LeftClickMessageCommand = function(self)
-			if SCREEN_WIDTH - INPUTFILTER:GetMouseX() < 32 then
-				local idx = whee:GetCurrentIndex()
-				local num = whee:GetNumItems()
-				local dum = (INPUTFILTER:GetMouseY() - 45) / (SCREEN_HEIGHT - 103)
-				whee:Move(notShit.round(num * dum) - idx)
+			if SCREENMAN:GetTopScreen():GetName() == "ScreenSelectMusic" or SCREENMAN:GetTopScreen():GetName() == "ScreenNetSelectMusic" then
+				if SCREEN_WIDTH - INPUTFILTER:GetMouseX() < 32 then
+					local idx = whee:GetCurrentIndex()
+					local num = whee:GetNumItems()
+					local dum = (INPUTFILTER:GetMouseY() - 45) / (SCREEN_HEIGHT - 103)
+					whee:Move(notShit.round(num * dum) - idx)
+				end
 			end
 		end
 	}
