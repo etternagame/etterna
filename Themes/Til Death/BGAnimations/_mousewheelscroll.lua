12807aec9abb08a81ee15b1f506963e0524e17b2
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2018-11-29 22:41:51 -0500

    remove another redundant selectionstate check in mouseselect

--- Themes/Til Death/BGAnimations/_mousewheelscroll.lua
+++ Themes/Til Death/BGAnimations/_mousewheelscroll.lua
@@ -35,13 +35,9 @@ local function scrollInput(event)
 		moving = false
 	end
 
-	if top:GetSelectionState() == 2 then
-		return
-	end
-
 	local mouseX = INPUTFILTER:GetMouseX()
 	local mouseY = INPUTFILTER:GetMouseY()
-
+	
 	if mouseX > capWideScale(370, 500) and mouseX < SCREEN_WIDTH - 32 then
 		if event.DeviceInput.button == "DeviceButton_left mouse button" and event.type == "InputEventType_FirstPress" then
 			local n = 0
