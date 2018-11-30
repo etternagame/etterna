95bc1a6a54b2ae7a1c480d7dde732e201d9dea14
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2018-11-30 05:33:20 -0500

    context comment for earlier change

--- src/ScoreManager.cpp
+++ src/ScoreManager.cpp
@@ -226,6 +226,7 @@ ScoresForChart::SetTopScores()
 			eligiblescores.emplace_back(hs);
 	}
 
+	// if there aren't 2 noccpbs in top scores we might as well use old cc scores -mina
 	if (eligiblescores.size() < 2) {
 		FOREACHM(int, ScoresAtRate, ScoresByRate, i)
 		{
