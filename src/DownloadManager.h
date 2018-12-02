0b8386ea41f5ebae0833f992f634552062b1d695
Nicolas <abramlujan@gmail.com>
2018-12-02 07:09:15 -0300

    Update DownloadManager.h

--- src/DownloadManager.h
+++ src/DownloadManager.h
@@ -164,7 +164,7 @@ class OnlineScore
 class DownloadManager
 {
   public:
-	static const LuaReference EMPTY_REFERENCE = LuaReference();
+	static const LuaReference EMPTY_REFERENCE;
 	DownloadManager();
 	~DownloadManager();
 	map<string, Download*> downloads; // Active downloads
