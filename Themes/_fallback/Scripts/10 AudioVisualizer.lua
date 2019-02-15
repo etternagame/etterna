9be3fa574e6d53f3aab5393511be3185ee6e4a67
poco0317 <poco0317@users.noreply.github.com>
2019-02-14 21:44:57 -0600

    Make visualizer smooth and slightly more on sync
    by delaying the animation ...

--- Themes/_fallback/Scripts/10 AudioVisualizer.lua
+++ Themes/_fallback/Scripts/10 AudioVisualizer.lua
@@ -101,7 +101,7 @@ function audioVisualizer:new(params)
         CurrentSongChangedMessageCommand = function(self)
             self:RunCommandsOnChildren(
                 function(self)
-                    self:zoomtoheight(0)
+                    self:finishtweening():smooth(0.3):zoomtoheight(0)
                 end
             )
         end
@@ -123,12 +123,12 @@ function audioVisualizer:new(params)
         local maxHeight = (params.maxHeight or 120) - minHeight
         if params.onBarUpdate then
             frame.updater = params.barUpdater or function(actor, value)
-                    actor:zoomtoheight(minHeight + value * maxHeight)
+                    actor:hurrytweening(0.15):smooth(0.22):zoomtoheight(minHeight + value * maxHeight)
                     params.onBarUpdate(actor, value)
                 end
         else
             frame.updater = params.barUpdater or function(actor, value)
-                    actor:zoomtoheight(minHeight + value * maxHeight)
+                    actor:hurrytweening(0.15):smooth(0.22):zoomtoheight(minHeight + value * maxHeight)
                 end
         end
     end
