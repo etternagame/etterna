e8859239a194425d3290803a526c7e971374fc49
Hidden <jameskr97@gmail.com>
2019-03-08 03:42:56 -0500

    Repaired PNG Writing

--- src/RageSurface_Save_PNG.cpp
+++ src/RageSurface_Save_PNG.cpp
@@ -5,6 +5,13 @@
 #include "RageSurfaceUtils.h"
 #include "RageSurface_Save_PNG.h"
 #include "RageUtil.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+// Note: This funnction is only called on RageDisplay.cpp:978 and the sError result is not used in the response.
 bool RageSurfaceUtils::SavePNG(RageSurface* pImg, RageFile& f, RString& sError) {
-	return true;
+    // Functions from "stb_image_write.h" return 0 on success
+    f.Close(); // The RageFile reference is already opened. Should be closed for following function to succeed.
+    return 0 != stbi_write_png(f.GetRealPath(), pImg->w, pImg->h, 4, pImg->pixels, pImg->w * 4);
 }
