b9b627d95b98216f1cd129cbc6963016c33daf6a
Hidden <jameskr97@gmail.com>
2019-03-09 21:56:02 -0500

    Added converted check and comments.

--- src/RageSurface_Save_PNG.cpp
+++ src/RageSurface_Save_PNG.cpp
@@ -15,11 +15,17 @@ bool RageSurfaceUtils::SavePNG(RageSurface* pImg, RageFile& f, RString& sError) 
     f.Close(); // The RageFile reference is already opened. Should be closed for following function to succeed.
 
     RageSurface* res;
-    RageSurfaceUtils::ConvertSurface(pImg, res, pImg->w, pImg->h, 32,
+    bool converted = RageSurfaceUtils::ConvertSurface(pImg, res, pImg->w, pImg->h, 32,
 					Swap32BE(0xFF000000),
 					Swap32BE(0x00FF0000),
 					Swap32BE(0x0000FF00),
 					Swap32BE(0x000000FF));
 
-    return 0 != stbi_write_png(f.GetRealPath(), pImg->w, pImg->h, 4, res->pixels, pImg->w * 4);
+    if (!converted) res = pImg;
+
+    // stride_in_bytes is image width in bytes
+    bool success = 0 != stbi_write_png(f.GetRealPath(), res->w, res->h, 4, res->pixels, res->w * 4);
+    if (converted) delete res; // If we converted then we created a new surface which we need to delete
+
+    return success;
 }
