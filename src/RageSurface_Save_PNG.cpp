b6407a07f2c4d2ff4050d05d6b3990c635084dfb
Hidden <jameskr97@gmail.com>
2019-03-09 20:21:40 -0500

    PNG writer color fix.
    Windows saved with mismatched RGB

--- src/RageSurface_Save_PNG.cpp
+++ src/RageSurface_Save_PNG.cpp
@@ -1,4 +1,4 @@
-﻿#include "global.h"
+#include "global.h"
 #include "RageFile.h"
 #include "RageLog.h"
 #include "RageSurface.h"
@@ -9,9 +9,17 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-// Note: This funnction is only called on RageDisplay.cpp:978 and the sError result is not used in the response.
+// Note: This function is only called on RageDisplay.cpp:978 and the sError result is not used in the response.
 bool RageSurfaceUtils::SavePNG(RageSurface* pImg, RageFile& f, RString& sError) {
-    // Functions from "stb_image_write.h" return 0 on success
+    // Functions from "stb_image_write.h" return 0 on failure
     f.Close(); // The RageFile reference is already opened. Should be closed for following function to succeed.
-    return 0 != stbi_write_png(f.GetRealPath(), pImg->w, pImg->h, 4, pImg->pixels, pImg->w * 4);
+
+    RageSurface* res;
+    RageSurfaceUtils::ConvertSurface(pImg, res, pImg->w, pImg->h, 32,
+					Swap32BE(0xFF000000),
+					Swap32BE(0x00FF0000),
+					Swap32BE(0x0000FF00),
+					Swap32BE(0x000000FF));
+
+    return 0 != stbi_write_png(f.GetRealPath(), pImg->w, pImg->h, 4, res->pixels, pImg->w * 4);
 }
