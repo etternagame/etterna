3f9adf09915f9cfa28f2a5bc51d0708b18043e53
Hidden <jameskr97@gmail.com>
2019-03-12 04:26:54 -0400

    Revert: Remove verstub.in.cpp

---
+++ src/Etterna/verstub.in.cpp
@@ -0,0 +1,10 @@
+// clang-format off
+extern int const sm_version_major = 1;//@SM_VERSION_MAJOR@;
+extern int const sm_version_minor = 1;//@SM_VERSION_MINOR@;
+extern int const sm_version_patch = 1;//@SM_VERSION_PATCH@;
+extern char const* const sm_version_git_hash = "@SM_VERSION_GIT_HASH@";
+extern char const* const sm_version_traditional = "@SM_VERSION_TRADITIONAL@";
+extern char const* const version_date = "@SM_TIMESTAMP_DATE@";
+extern char const* const version_time = "@SM_TIMESTAMP_TIME@";
+extern char const* const product_version = "@SM_VERSION_GIT@";
+// clang-format on
