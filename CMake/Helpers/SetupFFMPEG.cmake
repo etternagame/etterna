c22b4e68624b9dcd24cf6ff133a68997e1ba34b3
James Roberts <jameskr97@gmail.com>
2019-03-21 08:56:02 -0400

    macOS Building with FFMPEG

---
+++ CMake/Helpers/SetupFFMPEG.cmake
@@ -0,0 +1,75 @@
+include(ExternalProject)
+
+set(FFMPEG_ROOT "${PROJECT_BINARY_DIR}/ffmpeg-2.1.3-src")
+set(FFMPEG_BUILD "${FFMPEG_ROOT}/build")
+
+list(APPEND FFMPEG_CONFIGURE
+  "${FFMPEG_ROOT}/configure"
+  "--disable-programs"
+  "--disable-doc"
+  "--disable-debug"
+  "--disable-avdevice"
+  "--disable-swresample"
+  "--disable-postproc"
+  "--disable-avfilter"
+  "--disable-shared"
+  "--enable-static"
+)
+
+if(CMAKE_POSITION_INDEPENDENT_CODE)
+    list(APPEND FFMPEG_CONFIGURE "--enable-pic")
+endif()
+
+if (UNIX)
+  # TODO: Figure out how to build with hardware video acceleration.
+  # These options cause link issues when not disabled.
+  list(APPEND FFMPEG_CONFIGURE "--disable-vaapi" "--disable-vdpau")
+endif()
+
+if(APPLE)
+  list(APPEND FFMPEG_CONFIGURE
+    "--arch=x86_64"
+    "--cc=clang -m64"
+    "--enable-sse")
+endif()
+
+list(APPEND FFMPEG_CONFIGURE "--enable-gpl")
+
+if (WITH_CRYSTALHD_DISABLED)
+  list(APPEND FFMPEG_CONFIGURE "--disable-crystalhd")
+endif()
+
+list(APPEND FFMPEG_CONFIGURE "--extra-cflags=-w")
+
+# list(APPEND SM_FFMPEG_MAKE $(MAKE))
+# if (WITH_FFMPEG_JOBS GREATER 0)
+#   list(APPEND SM_FFMPEG_MAKE "-j${WITH_FFMPEG_JOBS}")
+# endif()
+
+ExternalProject_Add(ffmpeg_dl
+    GIT_REPOSITORY "https://github.com/stepmania/ffmpeg.git"
+    GIT_PROGRESS TRUE
+    GIT_SHALLOW TRUE
+    GIT_TAG "n2.1.3"
+    
+    SOURCE_DIR ${FFMPEG_ROOT}
+    CONFIGURE_COMMAND "${FFMPEG_CONFIGURE}"
+    BUILD_COMMAND make -j10
+    UPDATE_COMMAND ""
+    INSTALL_COMMAND ""
+    TEST_COMMAND "")
+
+list(APPEND SMDATA_LINK_LIB
+"${FFMPEG_BUILD}/libavformat/libavformat.a"
+"${FFMPEG_BUILD}/libavcodec/libavcodec.a"
+"${FFMPEG_BUILD}/libswscale/libswscale.a"
+"${FFMPEG_BUILD}/libavutil/libavutil.a")
+
+add_library(ffmpeg STATIC "${PROJECT_SOURCE_DIR}/extern/ffmpeg/null.cpp")
+add_dependencies(ffmpeg ffmpeg_dl)
+target_link_libraries(ffmpeg ${SMDATA_LINK_LIB})
+
+target_include_directories(ffmpeg PUBLIC ${FFMPEG_ROOT})
+target_include_directories(ffmpeg PUBLIC ${PROJECT_BINARY_DIR}/ffmpeg-build)
+
+
