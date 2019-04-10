0049576e2cfd5d2cbf1742922e3e974e376ebdd0
James <jameskr97@gmail.com>
2019-04-09 21:09:35 -0400

    Removed unused variables from version stub

--- src/Etterna/verstub.in.cpp
+++ src/Etterna/verstub.in.cpp
@@ -1,9 +1,5 @@
 // clang-format off
-extern int const sm_version_major = 0;//@SM_VERSION_MAJOR@;
-extern int const sm_version_minor = 0;//@SM_VERSION_MINOR@;
-extern int const sm_version_patch = 0;//@SM_VERSION_PATCH@;
 extern char const* const sm_version_git_hash = "@SM_VERSION_GIT_HASH@";
-extern char const* const sm_version_traditional = "@SM_VERSION_TRADITIONAL@";
 extern char const* const version_date = "@SM_TIMESTAMP_DATE@";
 extern char const* const version_time = "@SM_TIMESTAMP_TIME@";
 extern char const* const product_version = "@SM_VERSION_GIT@";
