56cc0ff480ddb1ff1f1ddadb58242c19c8c8955b
James <jameskr97@gmail.com>
2019-05-03 03:39:15 -0400

    Implement ninja-build support
    Since some libraries are statically linked, we had to add the logic to tell what to statically link if we're using ninja. Anything built from source does not require this logic. Maybe this can be revisited in the future to simplify it, since it is significant duplicate code, though I believe this is preferable to having a custom `USING_NINJA` variable in the CMake script.

--- CMake/Helpers/CMakeWindows.cmake
+++ CMake/Helpers/CMakeWindows.cmake
@@ -13,7 +13,7 @@ set(ETTERNA_COMPILE_FLAGS "/MP8 /GL /arch:SSE2")
 set(ETTERNA_LINK_FLAGS "/SUBSYSTEM:WINDOWS /SAFESEH:NO /LTCG")
 
 # Build type dependant compile flags
-if (CMAKE_BUILD_TYPE STREQUAL "Release")
+if (CMAKE_BUILD_TYPE STREQUAL "Release" OR "${CMAKE_GENERATOR}" STREQUAL "Ninja")
 	set_target_properties(SQLiteCpp sqlite3 jsoncpp lua uWS discord-rpc PROPERTIES COMPILE_FLAGS "/MT") # The following libraries are set to be dynamically linked. These compile flags switch them to be statically linked.
 	set(ETTERNA_COMPILE_FLAGS "${ETTERNA_COMPILE_FLAGS} /MT")
 	set(ETTERNA_LINK_FLAGS "${ETTERNA_LINK_FLAGS} /NODEFAULTLIB:\"LIBCMT\"")
@@ -40,7 +40,7 @@ target_link_directories(Etterna PUBLIC ${DIRECTX_LIBRARY_DIR})
 target_include_directories(Etterna PRIVATE ${DIRECTX_INCLUDE_DIR})
 
 # DLL - Copy to run directory
-if("${CMAKE_GENERATOR_PLATFORM}" STREQUAL "x64") # If 64bit
+if("${CMAKE_GENERATOR_PLATFORM}" STREQUAL "x64" OR "${CMAKE_GENERATOR}" STREQUAL "Ninja") # If 64bit
 	set(ARCH 64bit)
 else()
 	set(ARCH 32bit)
