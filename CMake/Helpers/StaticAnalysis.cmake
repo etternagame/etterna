94d2a17604131805ca895070ffc50fc8cceff63c
James <jameskr97@gmail.com>
2019-05-07 16:37:42 -0400

    Conditionally add cppcheck target, and do not run with all targets
    cppcheck is intended to be executable manually, and should only be available if cppcheck has been installed on the system.

--- CMake/Helpers/StaticAnalysis.cmake
+++ CMake/Helpers/StaticAnalysis.cmake
@@ -1,14 +1,18 @@
 # cppcheck
 find_program(CPPCHECK_EXE "cppcheck")
-get_target_property(CPPCHECK_SOURCES Etterna SOURCES)
-add_custom_target(cppcheck ALL
-        COMMAND ${CPPCHECK_EXE}
-        --output-file=${PROJECT_BINARY_DIR}/cppcheck.txt
-        -I${PROJECT_SOURCE_DIR}/src
-        -I${PROJECT_BINARY_DIR}/generated
-        --enable=all #warning,performance,portability,information,missingInclude
-        --std=c++14
-        --template=[{severity}][{id}]\ {message}\ {callstack}\ \(On\ {file}:{line}\)
-        --verbose
-        ${CPPCHECK_SOURCES}
-        VERBATIM)
+
+if(${CPPCHECK_EXE})
+	get_target_property(CPPCHECK_SOURCES Etterna SOURCES)
+	add_custom_target(cppcheck
+		COMMENT "Running cppcheck"
+		VERBATIM
+		COMMAND ${CPPCHECK_EXE}
+		--output-file=${PROJECT_BINARY_DIR}/cppcheck.txt
+		-I${PROJECT_SOURCE_DIR}/src
+		-I${PROJECT_BINARY_DIR}/generated
+		--enable=all
+		--std=c++14
+		--template=[{severity}][{id}]\ {message}\ {callstack}\ \(On\ {file}:{line}\)
+		--verbose
+		${CPPCHECK_SOURCES})
+endif()
