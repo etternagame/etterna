b4b0a16988aac2279d438a2fcf79dcb58f2fce0f
James <jameskr97@gmail.com>
2019-05-08 00:41:39 -0400

    Update cppcheck parameters

--- CMake/Helpers/StaticAnalysis.cmake
+++ CMake/Helpers/StaticAnalysis.cmake
@@ -1,18 +1,15 @@
 # cppcheck
 find_program(CPPCHECK_EXE "cppcheck")
 
-if(${CPPCHECK_EXE})
-	get_target_property(CPPCHECK_SOURCES Etterna SOURCES)
-	add_custom_target(cppcheck
-		COMMENT "Running cppcheck"
-		VERBATIM
-		COMMAND ${CPPCHECK_EXE}
-		--output-file=${PROJECT_BINARY_DIR}/cppcheck.txt
-		-I${PROJECT_SOURCE_DIR}/src
-		-I${PROJECT_BINARY_DIR}/generated
-		--enable=all
-		--std=c++14
-		--template=[{severity}][{id}]\ {message}\ {callstack}\ \(On\ {file}:{line}\)
-		--verbose
-		${CPPCHECK_SOURCES})
-endif()
+get_target_property(CPPCHECK_SOURCES Etterna SOURCES)
+add_custom_target(cppcheck
+	COMMENT "Running cppcheck"
+	VERBATIM
+	COMMAND ${CPPCHECK_EXE}
+	--output-file=${PROJECT_BINARY_DIR}/cppcheck.txt
+	--language=c++
+	--enable=all
+	--std=c++14
+	--template=[{severity}][{id}][{file}:{line}]\ {message}\ {callstack}
+	--verbose
+	${CPPCHECK_SOURCES})
