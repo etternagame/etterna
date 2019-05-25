45cd0a02672423e4a515dc956c54a6deac6f3e4a
Barinade <poco0317@users.noreply.github.com>
2019-05-25 02:53:41 -0500

    Fix bad coloring for Lua Widget Buttons in Multi

--- Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/wifeTwirl.lua
+++ Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/wifeTwirl.lua
@@ -590,6 +590,15 @@ function toggleButton(textEnabled, textDisabled, msg, x, enabledF)
 		highlight = {color = getMainColor("highlight")},
 		x = 10 - 100 + capWideScale(get43size(384), 384) + x,
 		y = 61 + capWideScale(get43size(120), 120),
+		font = {
+			scale = 0.3,
+			name = "Common Large",
+			color = color("#FFFFFF"),
+			padding = {
+				x = 10,
+				y = 10
+			}
+		},
 		onInit = function(self)
 			button.turnedOn = false
 			button.updateToggleButton = function()
