0e1dc26d4835b5fe06c5f303efae2a0d3cd1bc70
jolomann <joloswaggins@gmail.com>
2019-12-11 21:01:44 +0100

    Add exit tip to ScreenTestInput
    Brings the fallback lua file for the screen into til death and adds text saying how to exit the screen

---
+++ Themes/Til Death/BGAnimations/ScreenTestInput underlay.lua
@@ -0,0 +1,28 @@
+return Def.ActorFrame {
+	--Def.ControllerStateDisplay {
+	--	InitCommand=function(self)
+	--	self:LoadGameController()
+	--end,
+	--};
+	Def.DeviceList {
+		Font = "Common Normal",
+		InitCommand = function(self)
+			self:x(SCREEN_LEFT + 20):y(SCREEN_TOP + 80):zoom(0.8):halign(0)
+		end
+	},
+	Def.InputList {
+		Font = "Common Normal",
+		InitCommand = function(self)
+			self:x(SCREEN_CENTER_X - 250):y(SCREEN_CENTER_Y):zoom(1):halign(0):vertspacing(8)
+		end
+	},
+	LoadFont("Common Large") ..
+	{
+		InitCommand=function(self)
+			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y+150):zoom(0.5):halign(0.5)
+		end,
+		OnCommand=function(self)
+			self:settext("Hold 'Enter' to exit")
+		end
+	}
+}
