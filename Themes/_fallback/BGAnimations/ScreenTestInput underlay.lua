37c6213e1b0bc5c355b656585d0b799988b46039
jolomann <joloswaggins@gmail.com>
2019-12-11 21:40:55 +0100

    Add exit tip to ScreenTestInput
    Places "Hold 'Enter' to exit" near the bottom center of the screen, along with a transparent box behind the text to prevent overlapping

--- Themes/_fallback/BGAnimations/ScreenTestInput underlay.lua
+++ Themes/_fallback/BGAnimations/ScreenTestInput underlay.lua
@@ -15,5 +15,19 @@ return Def.ActorFrame {
 		InitCommand = function(self)
 			self:x(SCREEN_CENTER_X - 250):y(SCREEN_CENTER_Y):zoom(1):halign(0):vertspacing(8)
 		end
+	},
+	Def.Quad {
+		InitCommand = function(self)
+			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y+150):halign(0.5):valign(0.5):zoomto(240,40):diffuse(color("#000000")):diffusealpha(0.5)
+		end
+	},
+	LoadFont("Common Large") ..
+	{
+		InitCommand = function(self)
+			self:x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y+150):zoom(0.5):halign(0.5)
+		end,
+		OnCommand = function(self)
+			self:settext("Hold 'Enter' to exit")
+		end
 	}
 }
