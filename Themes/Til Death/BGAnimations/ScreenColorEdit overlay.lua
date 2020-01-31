14dbc77b89f1e4d4f23c690ddc1a88d1a5a62a19
Barinade <poco0317@users.noreply.github.com>
2020-01-31 14:49:56 -0600

    ColorEdit mouse support & graphics

--- Themes/Til Death/BGAnimations/ScreenColorEdit overlay.lua
+++ Themes/Til Death/BGAnimations/ScreenColorEdit overlay.lua
@@ -10,7 +10,18 @@ local translated_info = {
 	Description = THEME:GetString("ScreenColorEdit", "Description")
 }
 
+local function inputeater(event)
+	if event.type == "InputEventType_FirstPress" then
+		if event.DeviceInput.button == "DeviceButton_left mouse button" then
+			MESSAGEMAN:Broadcast("MouseLeftClick")
+		end
+	end
+end
+
 local t = Def.ActorFrame {
+	OnCommand = function(self)
+		SCREENMAN:GetTopScreen():AddInputCallback(inputeater)
+	end,
 	CodeMessageCommand = function(self, params)
 		if params.Name == "ColorCancel" then
 			SCREENMAN:GetTopScreen():Cancel()
@@ -18,15 +29,34 @@ local t = Def.ActorFrame {
 		if params.Name == "ColorStart" then
 			SCREENMAN:GetTopScreen():Cancel()
 		end
-	end
+	end,
+	Def.Quad {
+		Name = "MainBG",
+		InitCommand = function(self)
+			self:xy(0, 0):halign(0):valign(0):zoomto(SCREEN_WIDTH, SCREEN_HEIGHT):diffuse(color("#000000EE"))
+		end
+	}
 }
 
-
-t[#t + 1] = Def.Quad {
-	Name = "MainBG",
+t[#t+1] = Def.ActorFrame {
+	Name = "ColorPick",
 	InitCommand = function(self)
-		self:xy(0, 0):halign(0):valign(0):zoomto(SCREEN_WIDTH, SCREEN_HEIGHT):diffuse(color("#000000EE"))
-	end
+		self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y)
+	end,
+
+	Def.Sprite {
+		Name = "HSVImage",
+		Texture = THEME:GetPathG("", "color_hsv"),
+		InitCommand = function(self)
+		end
+	},
+	Def.Sprite {
+		Name = "SaturationOverlay",
+		Texture = THEME:GetPathG("", "color_sat_overlay"),
+		InitCommand = function(self)
+			self:diffusealpha(0.5)
+		end,
+	}
 }
 
 t[#t + 1] = LoadActor("_frame")
@@ -37,5 +67,7 @@ t[#t + 1] = LoadFont("Common Large") .. {
 		self:xy(5, 32):halign(0):valign(1):zoom(0.55):diffuse(getMainColor("highlight")):settext(translated_info["Title"])
 	end
 }
+
+t[#t+1] = LoadActor("_cursor")
 
 return t
