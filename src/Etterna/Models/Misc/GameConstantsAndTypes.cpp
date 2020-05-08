2d2bf42ef0548370a15081b285b1c1c008bb2398
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-05-08 02:59:02 -0400

    actually add the new stuff stuff

--- src/Etterna/Models/Misc/GameConstantsAndTypes.cpp
+++ src/Etterna/Models/Misc/GameConstantsAndTypes.cpp
@@ -226,7 +226,7 @@ StringToSkillset(const RString& s)
 }
 
 static const char* CalcPatternModNames[] = { "OHJump", "Anchor", "Roll", "HS",
-											 "Jump",   "CJ",	 "StreamMod", "OHTrill", "Chaos" , "FlamJam", "WideRangeRoll"};
+											 "Jump",   "CJ",	 "StreamMod", "OHTrill", "Chaos" , "FlamJam", "WideRangeRoll", "WideRangeJumptrill", "CJOHJump", "CJQuad"};
 XToString(CalcPatternMod);
 LuaXType(CalcPatternMod);
 
