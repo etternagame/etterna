6402860b1323cdae63602f3cba0a978528f41707
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-05-27 18:38:09 -0400

    ban grindwarrior also imagine being so scatter brained

--- src/Etterna/Globals/MinaCalc.cpp
+++ src/Etterna/Globals/MinaCalc.cpp
@@ -59,7 +59,7 @@ static const int zto3[4] = { 0, 1, 2, 3 };
 
 const char note_map[16][5]{ "0000", "0001", "0010", "0011", "0100", "0101",
 							"0110", "0111", "1000", "1001", "1010", "1011",
-							"1100", "1101", "0000", "0000" };
+							"1100", "1101", "1110", "1111" };
 
 // Relies on endiannes (significantly inaccurate)
 inline float
