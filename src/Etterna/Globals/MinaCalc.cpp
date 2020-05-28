60430afc6b7d7b0e77f6d997460ac78791423e52
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-05-28 12:25:56 -0400

    ban sawmill

--- src/Etterna/Globals/MinaCalc.cpp
+++ src/Etterna/Globals/MinaCalc.cpp
@@ -58,6 +58,15 @@ static const char note_map[16][5]{ "0000", "0001", "0010", "0011",
 								   "0100", "0101", "0110", "0111",
 								   "1000", "1001", "1010", "1011",
 								   "1100", "1101", "1110", "1111" };
+enum tap_size
+{
+	single,
+	jump,
+	hand,
+	quad
+};
+static const float s_init = -5.f;
+static const float ms_init = 5000.f;
 
 #pragma region stuffs
 // Relies on endiannes (significantly inaccurate)
