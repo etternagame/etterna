4c94b1d81c6ac4e3a450bdf43bb4929d3e42b399
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-06-03 21:32:38 -0400

    ban gripwarrior

--- src/Etterna/Globals/MinaCalc.cpp
+++ src/Etterna/Globals/MinaCalc.cpp
@@ -95,9 +95,8 @@ static const float stam_prop =
 // since chorded patterns have lower enps than streams, streams default to 1
 // and chordstreams start lower
 // stam is a special case and may use normalizers again
-static const float basescalers[NUM_Skillset] = {
-	0.f, 0.97f, 0.8f, 0.83f, 0.94f, 0.7675f, 0.9f, 1.075f
-};
+static const float basescalers[NUM_Skillset] = { 0.f,   0.97f,   0.8f, 0.83f,
+												 0.94f, 0.7675f, 0.9f, 1.075f };
 bool debug_lmao = false;
 
 #pragma region stuffs
