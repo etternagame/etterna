5dbe972dc841b8c35363f50294c0c5398bed1d41
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-06-06 19:52:56 -0400

    add a generic sequencer that contains anchor sequencing as its own file

---
+++ src/Etterna/Globals/MinaCalc/Dependent/HD_GenericSequencing.h
@@ -0,0 +1,171 @@
+#pragma once
+#include "HD_MetaSequencing.h"
+#include "Etterna/Globals/MinaCalcHelpers.h"
+
+#include <array>
+#include <initializer_list>
+
+/* Contains generic sequencers passed to metahandinfo to be advanced in the row
+ * loop */
+
+// ok this has expanded into more than i bargained for an should be
+// appropriately renamed and commented when i figure out what it is
+
+// bpm flux float precision etc
+static const float anchor_buffer_ms = 10.F;
+
+// individual anchors, 2 objects per hand on 4k
+struct Anchor_Sequencing
+{
+	col_type _ct = col_init;
+
+	float _now_ms = 0.F;
+
+	// if we exceed this + buffer, break the anchor sequence
+	float _max_ms = ms_init;
+
+	// row_time of last note on this col
+	float _last = s_init;
+
+	int _len = 0;
+
+	inline void full_reset()
+	{
+		// we don't need to reset col_type
+		_now_ms = 0.F;
+		_max_ms = ms_init;
+		_last = s_init;
+		_len = 0;
+	}
+
+	inline auto col_check(const col_type ct) -> bool
+	{
+		return ct == _ct || ct == col_ohjump;
+	}
+
+	inline void operator()(const col_type ct, const float& now)
+	{
+		if (col_check(ct)) {
+			_now_ms = ms_from(now, _last);
+
+			// break the anchor if the next note is too much slower than the
+			// lowest one in the sequence
+			if (_now_ms > _max_ms + anchor_buffer_ms) {
+				_len = 1;
+				_max_ms = ms_init;
+			} else {
+				// increase anchor length and set new cutoff point
+				++_len;
+				_max_ms = _now_ms;
+			}
+		}
+		_last = now;
+	}
+};
+
+struct AnchorSequencer
+{
+	std::array<Anchor_Sequencing, num_cols_per_hand> anch;
+	std::array<int, num_cols_per_hand> max_seen = { 0, 0 };
+
+	// track windows of highest anchor per col seen during an interval
+	std::array<CalcWindow<int>, num_cols_per_hand> _mw_max;
+
+	AnchorSequencer()
+	{
+		anch[col_left]._ct = col_left;
+		anch[col_right]._ct = col_right;
+		full_reset();
+	}
+
+	inline void full_reset()
+	{
+		max_seen.fill(0);
+
+		for (auto& c : ct_loop_no_jumps) {
+			anch[c].full_reset();
+			_mw_max[c].zero();
+		}
+	}
+
+	// new plan, this will be updated from mhi, which will then use it to set
+	// its tcms values
+	inline void operator()(const col_type ct, const float& row_time)
+	{
+		// update the one
+		if (ct == col_left || ct == col_right) {
+			anch[ct](ct, row_time);
+
+			// set max seen for this col for this interval
+			max_seen[ct] =
+			  anch[ct]._len > max_seen[ct] ? anch[ct]._len : max_seen[ct];
+		} else if (ct == col_ohjump) {
+
+			// update both
+			for (auto& c : ct_loop_no_jumps) {
+				anch[c](ct, row_time);
+
+				// set max seen
+				max_seen[c] =
+				  anch[c]._len > max_seen[c] ? anch[c]._len : max_seen[c];
+			}
+		}
+	}
+
+	// returns max anchor length seen for the requested window
+	[[nodiscard]] inline auto get_max_for_window_and_col(
+	  const col_type& ct,
+	  const int& window) const -> int
+	{
+		assert(ct < num_cols_per_hand);
+		return _mw_max[ct].get_max_for_window(window);
+	}
+
+	inline void handle_interval_end()
+	{
+		for (auto& c : ct_loop_no_jumps) {
+			_mw_max[c](max_seen[c]);
+			max_seen[c] = 0;
+		}
+	}
+};
+
+// more stuff could/should be moved here? the only major issue with moving _all_
+// sequencers here is loading/setting their params
+struct SequencerGeneral
+{
+	// moving window of ms_any values, practically speaking, row with something
+	// in it on this hand to last row with something in it on the current hand
+	CalcWindow<float> _mw_cc_ms_any;
+
+	// moving window of tc_ms values, not used but we will probably want
+	// eventually (maybe? maybe move it? idk???)
+	std::array<CalcWindow<float>, num_cols_per_hand> _mw_tc_ms;
+
+	/* basic sequencers */
+	AnchorSequencer _as;
+
+	// stuff
+	inline void advance_sequencing(const col_type ct, const float& row_time, const float& ms_now) {
+
+		// update sequencers
+		_as(ct, row_time);
+
+		// set tcs ms for time now, for each column
+		for (auto& c : ct_loop_no_jumps) {
+			_mw_tc_ms[c](_as.anch[c]._now_ms);
+		}
+
+		// i guess we do this here instead??
+		_mw_cc_ms_any(ms_now);
+	}
+
+	inline void full_reset()
+	{
+		for (auto& c : ct_loop_no_jumps) {
+			_mw_tc_ms[c].zero();
+		}
+
+		_as.full_reset();
+	}
+};
