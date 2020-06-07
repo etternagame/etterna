dfa033d4549fbd98474c7d7bda5cbb4e83f7d686
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-06-07 02:19:52 -0400

    add new place for hopefully workable future base diff calc

---
+++ src/Etterna/Globals/MinaCalc/SequencedBaseDiffCalc.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <array>
+#include <vector>
+
+#include "Etterna/Globals/MinaCalcHelpers.h"
+
+using std::vector;
+
+
+
+// for cj, will be sorted from teh above, but we dont care
+static inline auto
+CJBaseDifficultySequencing(const vector<float>& input) -> float
+{
+	if (input.empty()) {
+		return 1.F;
+	}
+
+	float looloo = mean(input);
+	float doodoo = ms_to_bpm(looloo);
+	float trootroo = doodoo / 15.F;
+	return trootroo * finalscaler;
+}
