e44e978ff41b97ad1d6ac3cd9bdf4ed1e202ac71
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-06-07 02:19:31 -0400

    praise ulbu

--- src/Etterna/Globals/MinaCalc/UlbuAcolytes.h
+++ src/Etterna/Globals/MinaCalc/UlbuAcolytes.h
@@ -1,7 +1,57 @@
 #pragma once
+#include <vector>
+#include <string>
+
+using std::vector;
+
+static const std::string calc_params_xml = "Save/calc params.xml";
+static const unsigned hand_col_ids[2] = { 3, 12 };
+
 enum hands
 {
 	left_hand,
 	right_hand,
 	num_hands,
 };
+
+inline void
+Smooth(vector<float>& input, float neutral)
+{
+	float f1;
+	float f2 = neutral;
+	float f3 = neutral;
+
+	for (float& i : input) {
+		f1 = f2;
+		f2 = f3;
+		f3 = i;
+		i = (f1 + f2 + f3) / 3.F;
+	}
+}
+
+inline void
+Smooth(vector<vector<float>>& input, float neutral)
+{
+	float f1;
+	float f2 = neutral;
+	for (auto& itv : input) {
+		for (float& i : itv) {
+			f1 = f2;
+			f2 = i;
+			i = (f1 + f2 * 2.F) / 3.F;
+		}
+	}
+}
+
+inline void
+DifficultyMSSmooth(vector<float>& input)
+{
+	float f1;
+	float f2 = 0.F;
+
+	for (float& i : input) {
+		f1 = f2;
+		f2 = i;
+		i = (f1 + f2) / 2.F;
+	}
+}
