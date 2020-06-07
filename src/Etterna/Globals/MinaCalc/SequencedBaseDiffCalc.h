b2b74b3ad8874f4ad4a2f157e6d4937a528864e1
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-06-07 19:42:54 -0400

    add tech base diff new thing potentially

--- src/Etterna/Globals/MinaCalc/SequencedBaseDiffCalc.h
+++ src/Etterna/Globals/MinaCalc/SequencedBaseDiffCalc.h
@@ -19,3 +19,18 @@ CJBaseDifficultySequencing(const vector<float>& input) -> float
 	float trootroo = doodoo / 15.F;
 	return trootroo * finalscaler;
 }
+
+
+
+static inline auto
+TechBaseDifficultySequencing(const vector<float>& input) -> float
+{
+	if (input.empty()) {
+		return 1.F;
+	}
+
+	float looloo = mean(input);
+	float doodoo = ms_to_bpm(looloo);
+	float trootroo = doodoo / 15.F;
+	return trootroo * finalscaler;
+}
