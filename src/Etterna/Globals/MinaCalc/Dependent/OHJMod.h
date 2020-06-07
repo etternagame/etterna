dbeab312d7b8e958ae1f5aa457efa7e35fc83641
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-06-06 21:56:26 -0400

    move ohmod

---
+++ src/Etterna/Globals/MinaCalc/Dependent/OHJMod.h
@@ -0,0 +1,207 @@
+#pragma once
+#include <string>
+#include <array>
+#include <vector>
+
+#include "Etterna/Models/NoteData/NoteDataStructures.h"
+#include "MetaIntervalHandInfo.h"
+#include "OHJSequencing.h"
+
+using std::pair;
+using std::vector;
+
+static const CalcPatternMod _pmod = OHJumpMod;
+static const vector<CalcPatternMod> _dbg = { OHJBaseProp, OHJPropComp,
+											 OHJSeqComp,  OHJMaxSeq,
+											 OHJCCTaps,   OHJHTaps };
+static const std::string name = "OHJumpMod";
+
+struct OHJumpModGuyThing
+{
+	
+
+#pragma region params
+
+	float min_mod = 0.75F;
+	float max_mod = 1.F;
+
+	float max_seq_weight = 0.65F;
+	float max_seq_pool = 1.25F;
+	float max_seq_scaler = 1.F;
+
+	float prop_pool = 1.4F;
+	float prop_scaler = 1.F;
+
+	const vector<pair<std::string, float*>> _params{
+		{ "min_mod", &min_mod },
+		{ "max_mod", &max_mod },
+
+		{ "max_seq_weight", &max_seq_weight },
+		{ "max_seq_pool", &max_seq_pool },
+		{ "max_seq_scaler", &max_seq_scaler },
+
+		{ "prop_pool", &prop_pool },
+		{ "prop_scaler", &prop_scaler },
+	};
+#pragma endregion params and param map
+
+	OHJ_Sequencer ohj;
+	int max_ohjump_seq_taps = 0;
+	int cc_taps = 0;
+
+	float floatymcfloatface = 0.F;
+	// number of jumps scaled to total taps in hand
+	float base_seq_prop = 0.F;
+	// size of sequence scaled to total taps in hand
+	float base_jump_prop = 0.F;
+
+	float max_seq_component = neutral;
+	float prop_component = neutral;
+	float pmod = neutral;
+
+#pragma region generic functions
+
+	inline void full_reset()
+	{
+		ohj.zero();
+
+		max_ohjump_seq_taps = 0;
+		cc_taps = 0;
+
+		floatymcfloatface = 0.F;
+		base_seq_prop = 0.F;
+		base_jump_prop = 0.F;
+
+		max_seq_component = neutral;
+		prop_component = neutral;
+		pmod = neutral;
+	}
+
+#pragma endregion
+
+	inline void advance_sequencing(const col_type& now, const col_type& last)
+	{
+		ohj(now, last);
+	}
+
+	// build component based on max sequence relative to hand taps
+	inline void set_max_seq_comp()
+	{
+		max_seq_component = max_seq_pool - (base_seq_prop * max_seq_scaler);
+		max_seq_component = max_seq_component < 0.1F ? 0.1F : max_seq_component;
+		max_seq_component = fastsqrt(max_seq_component);
+	}
+
+	// build component based on number of jumps relative to hand taps
+	inline void set_prop_comp()
+	{
+		prop_component = prop_pool - (base_jump_prop * prop_scaler);
+		prop_component = prop_component < 0.1F ? 0.1F : prop_component;
+		prop_component = fastsqrt(prop_component);
+	}
+
+	inline void set_debug_output(vector<float> doot[], const int& i)
+	{
+			doot[OHJSeqComp][i] = max_seq_component;
+			doot[OHJPropComp][i] = prop_component;
+			doot[OHJBaseProp][i] = base_seq_prop;
+			doot[OHJMaxSeq][i] = floatymcfloatface;
+			doot[OHJCCTaps][i] = static_cast<float>(cc_taps);
+	}
+
+	inline auto operator()(const metaItvHandInfo& mitvhi) -> float
+	{
+		const auto& itvhi = mitvhi._itvhi;
+
+		cc_taps = mitvhi._base_pattern_types[base_left_right] +
+				  mitvhi._base_pattern_types[base_right_left];
+
+		assert(cc_taps >= 0);
+
+		// if cur_seq > max when we ended the interval, grab it
+		max_ohjump_seq_taps = ohj.cur_seq_taps > ohj.max_seq_taps
+								? ohj.cur_seq_taps
+								: ohj.max_seq_taps;
+
+ 		/* case optimization start */
+
+		// nothing here or there are no ohjumps
+		if (itvhi.get_taps_nowi() == 0 ||
+			itvhi.get_col_taps_nowi(col_ohjump) == 0) {
+			return neutral;
+		}
+
+		// everything in the interval is in an ohj sequence
+		if (max_ohjump_seq_taps >= itvhi.get_taps_nowi()) {
+			return min_mod;
+		}
+
+		/* prop scaling only case */
+
+		// no repeated oh jumps, prop scale only based on jumps taps in hand
+		// taps if the jump was immediately broken by a cross column single tap
+		// we can have values of 1, otherwise 2
+		if (max_ohjump_seq_taps < 3) {
+
+			// need to set now
+			base_jump_prop =
+			  itvhi.get_col_taps_nowf(col_ohjump) / itvhi.get_taps_nowf();
+			set_prop_comp();
+
+			pmod = CalcClamp(prop_component, min_mod, max_mod);
+			return pmod;
+		}
+
+		/* seq scaling only case */
+
+		// if this is true we have some combination of single notes
+		// and jumps where the single notes are all on the same
+		// column
+		if (cc_taps == 0) {
+			// we don't want to treat 2[12][12][12]2222 2222[12][12][12]2
+			// differently, so use the max sequence here exclusively
+			// shortcut mod calculations, we need the base props now
+
+			// build now
+			floatymcfloatface = static_cast<float>(max_ohjump_seq_taps);
+			base_seq_prop = floatymcfloatface / itvhi.get_taps_nowf();
+			set_max_seq_comp();
+
+			pmod = CalcClamp(max_seq_component, min_mod, max_mod);
+			return pmod;
+		}
+
+		/* case optimization end */
+
+		// for js we lean into max sequences more, since they're better
+		// indicators of inflated difficulty
+
+		// set either after case optimizations or in case optimizations, after
+		// the simple checks, for optimization
+		floatymcfloatface = static_cast<float>(max_ohjump_seq_taps);
+		base_seq_prop = floatymcfloatface / mitvhi._itvhi.get_taps_nowf();
+		set_max_seq_comp();
+		max_seq_component = CalcClamp(max_seq_component, 0.1F, max_mod);
+
+		base_jump_prop =
+		  itvhi.get_col_taps_nowf(col_ohjump) / itvhi.get_taps_nowf();
+		set_prop_comp();
+		prop_component = CalcClamp(prop_component, 0.1F, max_mod);
+
+		pmod = weighted_average(
+		  max_seq_component, prop_component, max_seq_weight, 1.F);
+		pmod = CalcClamp(pmod, min_mod, max_mod);
+
+		interval_reset();
+
+		return pmod;
+	}
+
+	inline void interval_reset()
+	{
+		// reset any interval stuff here
+		cc_taps = 0;
+		ohj.max_seq_taps = 0;
+		max_ohjump_seq_taps = 0;
+	}
+};
