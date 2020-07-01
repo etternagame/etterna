15705eeb3bd3b8d38acd0e3fa05c416978fc1816
born a rick, raised a morty, died a jerry <minaciousgrace@gmail.com>
2020-07-01 15:16:55 -0400

    move jack debug set into helpers

--- src/Etterna/Globals/MinaCalc.cpp
+++ src/Etterna/Globals/MinaCalc.cpp
@@ -321,28 +321,6 @@ jackloss(const float& x, Calc& calc, const int& hi) -> float
 	return total;
 }
 
-// we want intervals not row values here, just average them, it's only for calc
-// display and doesn't affect internal calculations
-inline void
-set_jack_diff_debug(Calc& calc, const int& hi)
-{
-	// interval loop
-	for (int itv = 0; itv < calc.numitv; ++itv) {
-		float diff_total = 0.F;
-		int counter = 0;
-
-		// rows per interval now
-		for (int row = 0; row < calc.itv_jack_diff_size.at(hi).at(itv); ++row) {
-			diff_total += calc.jack_diff.at(hi).at(itv).at(row);
-			++counter;
-		}
-
-		// technically this is kind of a waste of an array but whatever
-		calc.soap.at(hi)[JackBase].at(itv) =
-		  counter > 0 ? diff_total / static_cast<float>(counter) : 0.F;
-	}
-}
-
 // debug bool here is NOT the one in Calc, it is passed from chisel
 // using the final difficulty as the starting point and should only
 // be executed once per chisel
