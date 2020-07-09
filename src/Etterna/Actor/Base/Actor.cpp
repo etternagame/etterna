f35a3ed53d482633eba7e63f9593596be83c12aa
Barinade <poco0317@users.noreply.github.com>
2020-07-08 19:08:01 -0500

    Revert "guess we don't need these (!this) blocks?"
    This reverts commit 61d1ee8050d01eee388684ccca23845370177b63.

--- src/Etterna/Actor/Base/Actor.cpp
+++ src/Etterna/Actor/Base/Actor.cpp
@@ -330,6 +330,8 @@ Actor::IsOver(float mx, float my)
 Actor*
 Actor::GetFakeParentOrParent()
 {
+	if (!this)
+		return nullptr;
 	if (m_FakeParent != nullptr)
 		return m_FakeParent;
 	if (m_pParent != nullptr)
@@ -339,6 +341,8 @@ Actor::GetFakeParentOrParent()
 float
 Actor::GetTrueX()
 {
+	if (!this)
+		return 0.f;
 	auto* mfp = GetFakeParentOrParent();
 	if (!mfp)
 		return GetX();
@@ -351,6 +355,8 @@ Actor::GetTrueX()
 float
 Actor::GetTrueY()
 {
+	if (!this)
+		return 0.f;
 	auto* mfp = GetFakeParentOrParent();
 	if (!mfp)
 		return GetY();
@@ -363,6 +369,8 @@ Actor::GetTrueY()
 float
 Actor::GetTrueRotationZ()
 {
+	if (!this)
+		return 0.f;
 	auto* mfp = GetFakeParentOrParent();
 	if (!mfp)
 		return GetRotationZ();
@@ -372,21 +380,23 @@ Actor::GetTrueRotationZ()
 float
 Actor::GetTrueZoom()
 {
+	if (!this)
+		return 1.f;
 	auto* mfp = GetFakeParentOrParent();
 	if (!mfp)
 		return GetZoom();
 	return GetZoom() * mfp->GetTrueZoom();
 }
-
 bool
 Actor::IsVisible()
 {
+	if (!this)
+		return false;
 	auto* mfp = GetFakeParentOrParent();
 	if (!mfp)
 		return GetVisible();
 	return GetVisible() && mfp->IsVisible();
 }
-
 void
 Actor::Draw()
 {
