089ea61aaf28fd6927b1c51d55b3ef9d2a6c80da
Barinade <poco0317@users.noreply.github.com>
2020-07-17 14:01:20 -0500

    fix bare-frames transitions

---
+++ Themes/bare-frames/BGAnimations/Screen out.lua
@@ -0,0 +1,8 @@
+return Def.Quad {
+	InitCommand = function(self)
+		self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y):zoomto(SCREEN_WIDTH, SCREEN_HEIGHT)
+	end,
+	OnCommand = function(self)
+		self:diffuse(color("0,0,0,0")):sleep(0.1):linear(0.1):diffusealpha(1)
+	end
+}
