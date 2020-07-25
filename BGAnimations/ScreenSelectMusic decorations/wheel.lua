d6e8abb459d904da8dd085d1afd49379ccb7325d
Barinade <poco0317@users.noreply.github.com>
2020-07-25 12:28:14 -0500

    override fallback chat, redirect input to lua wheel, hide default wheel

--- BGAnimations/ScreenSelectMusic decorations/wheel.lua
+++ BGAnimations/ScreenSelectMusic decorations/wheel.lua
@@ -4,6 +4,9 @@ t[#t+1] = Def.ActorFrame {
     Name = "WheelContainer",
     InitCommand = function(self)
         self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y)
+        SCREENMAN:set_input_redirected(PLAYER_1, true)
+    end,
+    OnCommand = function(self)
     end,
     MusicWheel:new()
 }
