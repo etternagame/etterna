2713a4d0057290be5b6f670d7ae0ed1791ea3411
Barinade <poco0317@users.noreply.github.com>
2020-08-04 19:05:59 -0500

    update display banner, bpm, rate, song len, song info on wheel settle

--- BGAnimations/ScreenSelectMusic decorations/curSongBox.lua
+++ BGAnimations/ScreenSelectMusic decorations/curSongBox.lua
@@ -1,4 +1,15 @@
-local t = Def.ActorFrame {Name = "CurSongBoxFile"}
+local displayScore
+local t = Def.ActorFrame {
+    Name = "CurSongBoxFile",
+    WheelSettledMessageCommand = function(self, params)
+        -- update displayscore
+        -- it sets to nil properly by itself
+        displayScore = GetDisplayScore()
+
+        -- cascade visual update to everything
+        self:playcommand("Set", {song = params.song, group = params.group, hovered = params.hovered})
+    end
+}
 
 local ratios = {
     LeftGap = 1055 / 1920, -- distance from left side of screen to left side of frame
@@ -74,10 +85,27 @@ t[#t+1] = Def.ActorFrame {
     },
     Def.Sprite {
         Name = "Banner",
-        Texture = THEME:GetPathG("Common", "fallback banner"),
         InitCommand = function(self)
             self:halign(0):valign(0)
-            self:zoomto(actuals.Width, actuals.BannerHeight)
+            self:scaletoclipped(actuals.Width, actuals.BannerHeight)
+        end,
+        SetCommand = function(self, params)
+            self:finishtweening()
+            self:smooth(0.05)
+            self:diffusealpha(1)
+            if params.song then
+                local bnpath = params.song:GetBannerPath()
+                if not bnpath then
+                    bnpath = THEME:GetPathG("Common", "fallback banner")
+                end
+                self:LoadBackground(bnpath)
+            else
+                local bnpath = SONGMAN:GetSongGroupBannerPath(params.hovered)
+                if not bnpath or bnpath == "" then
+                    bnpath = THEME:GetPathG("Common", "fallback banner")
+                end
+                self:LoadBackground(bnpath)
+            end
         end
     },
     LoadFont("Common Normal") .. {
@@ -88,6 +116,15 @@ t[#t+1] = Def.ActorFrame {
             self:zoom(textsize)
             self:maxwidth((actuals.DiffFrameLeftGap - actuals.LeftTextLeftGap) / textsize - textzoomFudge)
             self:settext("Song Title - Song Author")
+        end,
+        SetCommand = function(self, params)
+            if params.song then
+                local title = params.song:GetDisplayMainTitle()
+                local artist = params.song:GetDisplayArtist()
+                self:settextf("%s - %s", title, artist)
+            else
+                self:settext("")
+            end
         end
     },
     LoadFont("Common Normal") .. {
@@ -98,6 +135,13 @@ t[#t+1] = Def.ActorFrame {
             self:zoom(textsize)
             self:maxwidth((actuals.DiffFrameLeftGap - actuals.LeftTextLeftGap) / textsize - textzoomFudge)
             self:settext("Song SubTitle (1995)")
+        end,
+        SetCommand = function(self, params)
+            if params.song then
+                self:settextf(params.song:GetDisplaySubTitle())
+            else
+                self:settext("")
+            end
         end
     },
     LoadFont("Common Normal") .. {
@@ -107,7 +151,9 @@ t[#t+1] = Def.ActorFrame {
             self:xy(actuals.LeftTextLeftGap, actuals.Height - actuals.TextLowerGap1)
             self:zoom(textsize)
             self:maxwidth((actuals.DiffFrameLeftGap - actuals.LeftTextLeftGap) / textsize - textzoomFudge)
-            self:settext("6.9x")
+        end,
+        SetCommand = function(self, params)
+            self:settext(getCurRateDisplayString())
         end
     },
     
@@ -128,6 +174,17 @@ t[#t+1] = Def.ActorFrame {
             self:zoom(textsize)
             self:maxwidth(actuals.LengthWidth / textsize - textzoomFudge)
             self:settext("55:55")
+        end,
+        SetCommand = function(self, params)
+            local steps = GAMESTATE:GetCurrentSteps()
+            if steps then
+                local len = GetPlayableTime()
+                self:settext(SecondsToMMSS(len))
+                self:diffuse(byMusicLength(len))
+            else
+                self:settext("--:--")
+                self:diffuse(color("1,1,1,1"))
+            end
         end
     },
     
@@ -148,6 +205,17 @@ t[#t+1] = Def.ActorFrame {
             self:xy(actuals.Width - actuals.BPMNumberRightGap, actuals.Height - actuals.TextLowerGap1)
             self:zoom(textsize)
             self:maxwidth(actuals.BPMWidth / textsize - textzoomFudge)
+        end,
+        SetCommand = function(self, params)
+            local steps = GAMESTATE:GetCurrentSteps()
+            -- it appears that SetFromSteps is broken...
+            -- note to self.
+            if steps then
+                self:visible(true)
+                self:SetFromSong(params.song)
+            else
+                self:visible(false)
+            end
         end
     }
 
