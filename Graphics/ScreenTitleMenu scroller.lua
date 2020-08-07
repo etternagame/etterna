0b33e4ee8611e9b123b7ed99b7140f4c2790748b
Barinade <poco0317@users.noreply.github.com>
2020-08-06 20:12:24 -0500

    name title scroller texts

--- Graphics/ScreenTitleMenu scroller.lua
+++ Graphics/ScreenTitleMenu scroller.lua
@@ -3,12 +3,16 @@ local gc = Var("GameCommand")
 local choiceTextZoom = 0.6
 
 return Def.ActorFrame {
-	LoadFont("Common Large") ..
-		{
-			OnCommand = function(self)
-				self:halign(0)
-				self:zoom(choiceTextZoom)
-				self:settext(THEME:GetString(SCREENMAN:GetTopScreen():GetName(), gc:GetText()))
-			end
-		}
+	-- the name of this frame is determined by C++
+	-- it will be the name of the choice
+	-- ie: ScrollerChoice<GameCommandName>
+
+	LoadFont("Common Large") ..	{
+		Name = "ScrollerText",
+		BeginCommand = function(self)
+			self:halign(0)
+			self:zoom(choiceTextZoom)
+			self:settext(THEME:GetString(SCREENMAN:GetTopScreen():GetName(), gc:GetText()))
+		end
+	}
 }
