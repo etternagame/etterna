6a28cf397aeb76f9166f9ce2eb478662bf3b9d9c
James <jameskr97@gmail.com>
2020-08-22 03:20:37 -0400

    Squashed 'extern/plog/' content from commit f4c22b03d5
    git-subtree-dir: extern/plog
    git-subtree-split: f4c22b03d5d3aa753cca8e716636ac4eb29b0917

---
+++ samples/CustomConverter/Main.cpp
@@ -0,0 +1,47 @@
+//
+// CustomConverter - shows how to implement a custom converter that encrypts log messages.
+//
+
+#include <plog/Log.h>
+
+namespace plog
+{
+    class MyConverter
+    {
+    public:
+        static std::string header(const util::nstring& str)
+        {
+            return convert(str); // We have no special header for a file, so just call convert.
+        }
+
+        static std::string convert(const util::nstring& str)
+        {
+            const std::string& in = UTF8Converter::convert(str); // Convert to UTF8 first as it is more compact.
+
+            std::string out;
+            out.resize(in.size());
+
+            // This is an encryption key.
+            const char kKey[] = "\x56\x5a\x43\x4d\x5f\x81\x4c\x4e\x19\x29\x2e\x13\x7c\x31\x14\x17\x5d\x63\x32\x39";
+
+            // Simple XOR encryption.
+            for (size_t i = 0; i < out.size(); ++i)
+            {
+                out[i] = in[i] ^ kKey[i % (sizeof(kKey) - 1)];
+            }
+
+            return out;
+        }
+    };
+}
+
+int main()
+{
+    static plog::RollingFileAppender<plog::TxtFormatter, plog::MyConverter> appender("CustomConverter.txt"); // Create an appender and pass our converter as a template parameter.
+    plog::init(plog::debug, &appender); // Initialize the logger with the appender.
+
+    PLOGD << "A debug message!";
+    PLOGD << "Another one debug message!";
+
+    return 0;
+}
---
+++ samples/Hello/Main.cpp
@@ -0,0 +1,18 @@
+//
+// Hello - a minimal introduction sample, shows the basic 3 steps to start using plog.
+//
+
+#include <plog/Log.h> // Step1: include the header.
+
+int main()
+{
+    plog::init(plog::debug, "Hello.txt"); // Step2: initialize the logger.
+
+    // Step3: write log messages using a special macro. There are several log macros, use the macro you liked the most.
+
+    PLOGD << "Hello log!"; // short macro
+    PLOG_DEBUG << "Hello log!"; // long macro
+    PLOG(plog::debug) << "Hello log!"; // function-style macro
+
+    return 0;
+}
