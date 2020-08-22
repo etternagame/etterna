26bbec5035c8eb2b2bd633e0da8f8b3fd06888a5
Barinade <poco0317@users.noreply.github.com>
2020-08-22 01:53:57 -0500

    flesh out eval scoreboard functionality (wip), enable offset changing

--- BGAnimations/ScreenEvaluation decorations/scoreBoard.lua
+++ BGAnimations/ScreenEvaluation decorations/scoreBoard.lua
@@ -24,6 +24,9 @@ local ratios = {
     ScoreClearInfoSpace = 67 / 1920, -- basically a portion of the width of the item, eyeballed
     ScoreMetaInfoSpace = 340 / 1920,
     ScorePlayerRateSpace = 60 / 1920,
+
+    CursorVerticalSpan = 12 / 1080, -- visible cursor glow measured, doubled
+    CursorHorizontalSpan = 12 / 1920, -- same
 }
 
 local actuals = {
@@ -45,6 +48,8 @@ local actuals = {
     ScoreClearInfoSpace = ratios.ScoreClearInfoSpace * SCREEN_WIDTH,
     ScoreMetaInfoSpace = ratios.ScoreMetaInfoSpace * SCREEN_WIDTH,
     ScorePlayerRateSpace = ratios.ScorePlayerRateSpace * SCREEN_WIDTH,
+    CursorVerticalSpan = ratios.CursorVerticalSpan * SCREEN_HEIGHT,
+    CursorHorizontalSpan = ratios.CursorHorizontalSpan * SCREEN_WIDTH,
 }
 
 -- we are expecting this file to be loaded with these params precalculated
@@ -75,8 +80,83 @@ t[#t+1] = Def.Quad {
     end
 }
 
+-- online and offline (default is offline)
+local isLocal = true
+-- current rate or not (default is current rate)
+-- this is NOT the variable that controls the online scores
+-- if isLocal is false, defer to allRates = not DLMAN:GetCurrentRateFilter()
+-- but do not replace the variable
+local allRates = false
+-- all scores or top scores (online only)
+local allScores = not DLMAN:GetTopScoresOnlyFilter()
+
+-- this will distribute a given highscore to the offsetplot and the other eval elements
+-- it will only work properly with a replay, so restrict it to replay-only scores
+local function distributeScore(callerActor, highscore)
+    if highscore == nil or not highscore:HasReplayData() then return end
+    local steps = GAMESTATE:GetCurrentSteps(PLAYER_1)
+    if steps ~= nil then
+        local timingdata = steps:GetTimingData()
+
+        local function actuallyDistributeScore(callerActor, highscore)
+            local params = {}
+            params.offsetVector = highscore:GetOffsetVector()
+            params.trackVector = highscore:GetTrackVector()
+            local noterows = highscore:GetNoteRowVector()
+            params.timingVector = {}
+            for i, row in ipairs(noterows) do
+                params.timingVector[i] = timingdata:GetElapsedTimeFromNoteRow(row)
+            end
+            params.typeVector = highscore:GetTapNoteTypeVector()
+            params.maxTime = steps:GetLastSecond()
+
+            callerActor:GetParent():GetChild("OffsetPlotFile"):playcommand("LoadOffsets", params)
+        end
+
+
+        -- this highscore is an online score so must request ReplayData
+        if highscore:GetScoreKey():find("^Online_") ~= nil then
+            DLMAN:RequestOnlineScoreReplayData(highscore,
+            function()
+                actuallyDistributeScore(callerActor, highscore)
+            end)
+        else
+            -- otherwise we can immediately do the thing
+            actuallyDistributeScore(callerActor, highscore)
+        end
+    end
+end
+
 local function scoreList()
     local page = 1
+    local maxPage = 1
+
+    local function movePage(n)
+        if maxPage <= 1 then
+            return
+        end
+
+        -- math to make pages loop both directions
+        local nn = (page + n) % (maxPage + 1)
+        if nn == 0 then
+            nn = n > 0 and 1 or maxPage
+        end
+        page = nn
+
+        MESSAGEMAN:Broadcast("MovedPage")
+    end
+
+    -- yes, we do have a country filter
+    -- we don't tell anyone
+    -- the Global country just shows everyone
+    local dlmanScoreboardCountryFilter = "Global"
+    
+    local mostRecentScore = SCOREMAN:GetMostRecentScore()
+    local selectedScorekey = mostRecentScore:GetScoreKey()
+    local scores = {}
+
+    -- to prevent bombing the server repeatedly with leaderboard requests
+    local alreadyRequestedLeaderboard = false
 
     local t = Def.ActorFrame {
         Name = "ScoreListFrame",
@@ -84,18 +164,89 @@ local function scoreList()
             self:xy(actuals.ScoreListLeftGap, actuals.ScoreListUpperGap)
         end,
         OnCommand = function(self)
+            self:playcommand("UpdateScores")
             self:playcommand("UpdateList")
         end,
-        UpdateListCommand = function(self)
-            local e = getRateTable(getScoresByKey(PLAYER_1))
-            local scorelist = e[getRate(SCOREMAN:GetMostRecentScore())] or {SCOREMAN:GetMostRecentScore()}
-            for i = 1, itemCount do
-                self:GetChild("ScoreItem_"..i):playcommand("SetScore", {score = scorelist[i]})
+        UpdateScoresCommand = function(self)
+            page = 1
+            if isLocal then
+                local scoresByRate = getRateTable(getScoresByKey(PLAYER_1))
+
+                if allRates then
+                    -- place every single score for the file into a table
+                    scores = {}
+                    for _, scoresAtRate in pairs(scoresByRate) do
+                        for __, s in pairs(scoresAtRate) do
+                            scores[#scores + 1] = s
+                        end
+                    end
+                    -- sort it by Overall SSR
+                    table.sort(scores, 
+                    function(a,b)
+                        return a:GetSkillsetSSR("Overall") > b:GetSkillsetSSR("Overall")
+                    end)
+                else
+                    -- place only the scores for this rate into a table
+                    -- it is already sorted by percent
+                    -- the first half of this returns nil sometimes
+                    -- particularly for scores that dont actually exist in your profile, like online replays
+                    -- in those cases, we put the mostRecentScore into its own fallback table
+                    -- mostRecentScore will never be nil. if it is, the game actually crashes before it gets here.
+                    scores = scoresByRate[getRate(mostRecentScore)] or {mostRecentScore}
+                end
+            else
+                local steps = GAMESTATE:GetCurrentSteps(PLAYER_1)
+                -- operate with dlman scores
+                -- ... everything here is determined by internal bools set by the toggle buttons
+                scores = DLMAN:GetChartLeaderBoard(steps:GetChartKey(), dlmanScoreboardCountryFilter)
+
+                -- this is the initial request for the leaderboard which should only end up running once
+                -- and when it finishes, it loops back through this command
+                -- on the second passthrough, the leaderboard is hopefully filled out
+                if #scores == 0 then
+                    if steps then
+                        if not alreadyRequestedLeaderboard then
+                            alreadyRequestedLeaderboard = true
+                            DLMAN:RequestChartLeaderBoardFromOnline(
+                                steps:GetChartKey(),
+                                function(leaderboard)
+                                    self:queuecommand("UpdateScores")
+                                    self:queuecommand("UpdateList")
+                                end
+                            )
+                        end
+                    end
+                end
             end
-        end
+        end,
+        UpdateListCommand = function(self)
+            maxPage = math.ceil(#scores / itemCount)
+            self:GetChild("Cursor"):diffusealpha(0)
+
+            for i = 1, itemCount do
+                local index = (page - 1) * itemCount + i
+                self:GetChild("ScoreItem_"..i):playcommand("SetScore", {scoreIndex = index})
+            end
+        end,
+        MovedPageMessageCommand = function(self)
+            self:playcommand("UpdateList")
+        end,
+        ToggleCurrentRateMessageCommand = function(self, params)
+            self:playcommand("UpdateScores")
+            self:playcommand("UpdateList")
+        end,
+        ToggleAllScoresMessageCommand = function(self, params)
+            self:playcommand("UpdateScores")
+            self:playcommand("UpdateList")
+        end,
+        ToggleLocalMessageCommand = function(self, params)
+            self:playcommand("UpdateScores")
+            self:playcommand("UpdateList")
+        end,
     }
     local function scoreItem(i)
         local score = nil
+        local scoreIndex = i
 
         return Def.ActorFrame {
             Name = "ScoreItem_"..i,
@@ -103,11 +254,15 @@ local function scoreList()
                 self:y((i-1) * actuals.ScoreItemSpacing + (i-1) * actuals.ScoreItemHeight)
             end,
             SetScoreCommand = function(self, params)
-                score = params.score
+                scoreIndex = params.scoreIndex
+                score = scores[scoreIndex]
                 if score == nil then
                     self:diffusealpha(0)
                 else
                     self:diffusealpha(1)
+                    if score:GetScoreKey() == selectedScorekey then
+                        self:GetParent():GetChild("Cursor"):playcommand("SetIndex", {index = i})
+                    end
                 end
             end,
 
@@ -117,6 +272,14 @@ local function scoreList()
                     self:halign(0):valign(0)
                     self:zoomto(actuals.ScoreItemWidth, actuals.ScoreItemHeight)
                     self:diffusealpha(0.2)
+                end,
+                MouseDownCommand = function(self)
+                    if score ~= nil and score:HasReplayData() then
+                        selectedScorekey = score:GetScoreKey()
+                        self:GetParent():GetParent():GetChild("Cursor"):playcommand("SetIndex", {index = i})
+                        -- this actor great grandparent is the ScoreBoardFrame (the file level t)
+                        distributeScore(self:GetParent():GetParent():GetParent(), score)
+                    end
                 end
             },
             LoadFont("Common Normal") .. {
@@ -229,21 +392,54 @@ local function scoreList()
                 end
             }
         }
-
-
     end
     t[#t+1] = Def.Sprite {
         Name = "Cursor",
         Texture = THEME:GetPathG("", "scoreboardGlow"),
         InitCommand = function(self)
-            self:visible(false)
-        end
+            self:halign(0):valign(0)
+            self:x(-actuals.CursorHorizontalSpan / 2)
+            self:zoomto(actuals.ScoreItemWidth + actuals.CursorHorizontalSpan, actuals.ScoreItemHeight + actuals.CursorVerticalSpan)
+            self:diffusealpha(0)
+        end,
+        SetIndexCommand = function(self, params)
+            local i = params.index
+            if scores[i] == nil then
+                self:diffusealpha(0)
+            else
+                self:diffusealpha(1)
+                self:y((i-1) * actuals.ScoreItemSpacing + (i-1) * actuals.ScoreItemHeight - actuals.CursorVerticalSpan / 2)
+            end
+        end,
     }
+    
     for i = 1, itemCount do
         t[#t+1] = scoreItem(i)
     end
+
+    t[#t+1] = Def.Quad {
+        Name = "MouseWheelRegion",
+        InitCommand = function(self)
+            self:halign(0):valign(0)
+            -- have to offset by the parent position...
+            self:xy(-actuals.ScoreListLeftGap, -actuals.ScoreListUpperGap)
+            self:diffusealpha(0)
+            self:zoomto(actuals.FrameWidth, actuals.FrameHeight)
+        end,
+        MouseScrollMessageCommand = function(self, params)
+            if isOver(self) then
+                if params.direction == "Up" then
+                    movePage(-1)
+                else
+                    movePage(1)
+                end
+            end
+        end
+    }
     return t
 end
+
+
 
 t[#t+1] = Def.ActorFrame {
     Name = "LeftButtons",
@@ -259,6 +455,16 @@ t[#t+1] = Def.ActorFrame {
             self:zoom(topButtonSize)
             self:maxwidth((actuals.VerticalDividerLeftGap - actuals.LeftButtonLeftGap) / topButtonSize - textZoomFudge)
             self:settext("Local")
+        end,
+        MouseDownCommand = function(self)
+            isLocal = true
+            MESSAGEMAN:Broadcast("ToggleLocal")
+        end,
+        MouseOverCommand = function(self)
+            self:diffusealpha(0.8)
+        end,
+        MouseOutCommand = function(self)
+            self:diffusealpha(1)
         end
     },
     UIElements.TextButton(1, 1, "Common Normal") .. {
@@ -269,6 +475,16 @@ t[#t+1] = Def.ActorFrame {
             self:zoom(topButtonSize)
             self:maxwidth((actuals.VerticalDividerLeftGap - actuals.LeftButtonLeftGap) / topButtonSize - textZoomFudge)
             self:settext("Online")
+        end,
+        MouseDownCommand = function(self)
+            isLocal = false
+            MESSAGEMAN:Broadcast("ToggleLocal")
+        end,
+        MouseOverCommand = function(self)
+            self:diffusealpha(0.8)
+        end,
+        MouseOutCommand = function(self)
+            self:diffusealpha(1)
         end
     },
 
@@ -280,6 +496,16 @@ t[#t+1] = Def.ActorFrame {
             self:zoom(bottomButtonSize)
             self:maxwidth((actuals.VerticalDividerLeftGap - actuals.LeftButtonLeftGap) / bottomButtonSize - textZoomFudge)
             self:settext("All Scores")
+        end,
+        MouseDownCommand = function(self)
+            allScores = true
+            MESSAGEMAN:Broadcast("ToggleAllScores")
+        end,
+        MouseOverCommand = function(self)
+            self:diffusealpha(0.8)
+        end,
+        MouseOutCommand = function(self)
+            self:diffusealpha(1)
         end
     },
     UIElements.TextButton(1, 1, "Common Normal") .. {
@@ -290,6 +516,16 @@ t[#t+1] = Def.ActorFrame {
             self:zoom(bottomButtonSize)
             self:maxwidth((actuals.VerticalDividerLeftGap - actuals.LeftButtonLeftGap) / bottomButtonSize - textZoomFudge)
             self:settext("Top Scores")
+        end,
+        MouseDownCommand = function(self)
+            allScores = false
+            MESSAGEMAN:Broadcast("ToggleAllScores")
+        end,
+        MouseOverCommand = function(self)
+            self:diffusealpha(0.8)
+        end,
+        MouseOutCommand = function(self)
+            self:diffusealpha(1)
         end
     },
     UIElements.TextButton(1, 1, "Common Normal") .. {
@@ -300,6 +536,16 @@ t[#t+1] = Def.ActorFrame {
             self:zoom(bottomButtonSize)
             self:maxwidth((actuals.VerticalDividerLeftGap - actuals.LeftButtonLeftGap) / bottomButtonSize - textZoomFudge)
             self:settext("Current Rate")
+        end,
+        MouseDownCommand = function(self)
+            allRates = false
+            MESSAGEMAN:Broadcast("ToggleCurrentRate")
+        end,
+        MouseOverCommand = function(self)
+            self:diffusealpha(0.8)
+        end,
+        MouseOutCommand = function(self)
+            self:diffusealpha(1)
         end
     },
     UIElements.TextButton(1, 1, "Common Normal") .. {
@@ -310,6 +556,16 @@ t[#t+1] = Def.ActorFrame {
             self:zoom(bottomButtonSize)
             self:maxwidth((actuals.VerticalDividerLeftGap - actuals.LeftButtonLeftGap) / bottomButtonSize - textZoomFudge)
             self:settext("All Rates")
+        end,
+        MouseDownCommand = function(self)
+            allRates = true
+            MESSAGEMAN:Broadcast("ToggleCurrentRate")
+        end,
+        MouseOverCommand = function(self)
+            self:diffusealpha(0.8)
+        end,
+        MouseOutCommand = function(self)
+            self:diffusealpha(1)
         end
     }
 
