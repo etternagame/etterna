66a8d52a1084c3c179223fc27e275952407bbfd5
Barinade <poco0317@users.noreply.github.com>
2020-08-22 03:41:53 -0500

    give eval lua access to sending a highscore to the pss faker function

--- src/Etterna/Screen/Others/ScreenEvaluation.cpp
+++ src/Etterna/Screen/Others/ScreenEvaluation.cpp
@@ -249,7 +249,14 @@ class LunaScreenEvaluation : public Luna<ScreenEvaluation>
 		CHECKPOINT_M("Setting PSS from ReplayData via Lua");
 		PlayerStageStats* pPSS = Luna<PlayerStageStats>::check(L, 1);
 		NoteData nd = GAMESTATE->m_pCurSteps->GetNoteData();
-		HighScore* hs = SCOREMAN->GetMostRecentScore();
+
+		// allow either a highscore or nothing, which defaults to most recent
+		HighScore* hs;
+		if (lua_isnil(L, 3))
+			hs = SCOREMAN->GetMostRecentScore();
+		else
+			hs = Luna<HighScore>::check(L, 3);
+		
 		float ts = FArg(2);
 		PlayerOptions potmp;
 		potmp.FromString(hs->GetModifiers());
