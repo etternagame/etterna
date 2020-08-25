6eb24def2014fafa999744073ddf8bb1f141465c
James <jameskr97@gmail.com>
2020-08-25 01:27:55 -0400

    Comment and update CMake file

--- CMake/Helpers/DocumentationTools.cmake
+++ CMake/Helpers/DocumentationTools.cmake
@@ -4,16 +4,19 @@ find_package(Doxygen OPTIONAL_COMPONENTS dot)
 if(NOT DOXYGEN_FOUND)
     message(WARNING "Doxygen not found. Documentation target will not be created.")
 else()
-    # set input and output files
-    set(DOXYGEN_IN ${PROJECT_SOURCE_DIR}/Docs/Doxyfile.in)
-    set(DOXYGEN_OUT ${PROJECT_BINARY_DIR}/Doxyfile)
+    include(FetchContent)
+    # m.css theme
+    FetchContent_Declare(m.css GIT_REPOSITORY https://github.com/mosra/m.css)
+    FetchContent_MakeAvailable(m.css)
+    FetchContent_GetProperties(m.css SOURCE_DIR MCSS_SOURCE)
+    set(DOXYGEN_DIR ${PROJECT_SOURCE_DIR}/Docs/Doxygen)
 
     # configure the file
     configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
 
     # note the option ALL which allows to build the docs together with the application
     add_custom_target(doxygen 
-        COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+        COMMAND ${MCSS_SOURCE}/documentation/doxygen.py ${PROJECT_BINARY_DIR}/conf.py
         WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
         COMMENT "Generating API documentation with Doxygen"
         VERBATIM)
