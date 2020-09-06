fdf7bba47a8b80cbf73092c1c9a1994a6b4fb101
Barinade <poco0317@users.noreply.github.com>
2020-09-05 19:27:38 -0500

    add sprite button to scripts

--- Scripts/97 UIElements.lua
+++ Scripts/97 UIElements.lua
@@ -454,6 +454,32 @@ function UIElements.QuadButton(z, depth)
 	return t
 end
 
+-- clickable image
+function UIElements.SpriteButton(z, depth, tex)
+
+	local t = Def.Sprite {
+		Texture = tex,
+		InitCommand = function(self) 
+			self:z(z)
+		end,
+		OnCommand = function(self)
+			local screen = SCREENMAN:GetTopScreen()
+			if screen ~= nil then
+				BUTTON:AddButton(self, screen:GetName(), depth)
+			end
+		end,
+		MouseOverCommand = function(self) end,
+		MouseOutCommand = function(self) end,
+		MouseUpCommand = function(self, params) end,
+		MouseDownCommand = function(self, params) end,
+		MouseClickCommand = function(self, params) end,
+		MouseReleaseCommand = function(self, params) end,
+		MouseDragCommand = function(self, params) end,
+		MouseHoldCommand = function(self, params) end,
+	}
+	return t
+end
+
 ---
 --- the point of this is to have a square hover area for a text button
 --- it is possible to just hover the text but that introduces some odd issues
