cac3c8caf638c438bf30ab81fad3c733c64dee11
James <jameskr97@gmail.com>
2020-10-30 00:38:18 -0400

    Show error dialog when windows reaches sm_crash

--- src/Etterna/Globals/global.cpp
+++ src/Etterna/Globals/global.cpp
@@ -9,6 +9,7 @@
 #ifdef _WIN32
 #define _WIN32_WINDOWS 0x0410 // include Win98 stuff
 #include "windows.h"
+#include <fmt/format.h>
 #elif defined(__APPLE__)
 #include "archutils/Darwin/Crash.h"
 #include <stdlib.h>
@@ -16,9 +17,20 @@ using CrashHandler::DebugBreak;
 using CrashHandler::IsDebuggerPresent;
 #endif
 
-void NORETURN
-sm_crash(const char* reason)
-{
+#ifdef _WIN32
+void showCrashDialog(const char* message){
+    std::string error_message =
+            "Etterna has crashed.\n\n"
+            "A crash report was created in the \"CrashData\" folder.\n\n"
+            "Please send that file to the developers, and they can find out what happened!\n\n"
+            "Crash Reason: {}";
+
+    MessageBox(nullptr, fmt::format(error_message, message).c_str(), "Crash Message", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+}
+
+#endif
+
+void NORETURN sm_crash(const char* reason) {
 #if defined(_WIN32) || defined(__APPLE__) || defined(_XDBG)
 	/* If we're being debugged, throw a debug break so it'll suspend the
 	 * process. */
@@ -33,20 +45,10 @@ sm_crash(const char* reason)
 #endif
 
 #ifdef _WIN32
-	CrashHandler::ForceCrash(reason);
+    showCrashDialog(reason);
 #endif
 	Locator::getLogger()->fatal(reason);
     Core::Crash::generateMinidump();
 
-#ifdef _WIN32
-	/* Do something after the above, so the call/return isn't optimized to a
-	 * jmp; that way, this function will appear in backtrace stack traces. */
-#if defined(_MSC_VER)
-	//__nop();
-#elif defined(__GNUC__) // MinGW or similar
-	asm("nop");
-#endif
-#else
-	_exit(1);
-#endif
+    _exit(1);
 }
