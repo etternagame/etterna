88264e1572e1784841c2e2350ce0490b8f61caec
Barinade <poco0317@users.noreply.github.com>
2020-11-06 17:08:09 -0600

    make getLargestWidthOfChild actually recursive

--- Scripts/98 util.lua
+++ Scripts/98 util.lua
@@ -2,20 +2,21 @@
 
 
 -- return the width of the widest child
--- ASSUMING all immediate children are not ActorFrames
--- this can be made into a recursive function but some behavior indicates that may not be a good idea
+-- works recursively: considers all children in the tree
 function getLargestChildWidth(actorFrame)
     local largest = 0
     if actorFrame == nil then
         return largest
     end
 
-    for name, child in pairs(actorFrame:GetChildren()) do
-        local w = child:GetZoomedWidth()
-        if w > largest then
-            largest = w
+    actorFrame:RunCommandsRecursively(
+        function(self)
+            local w = self:GetZoomedWidth()
+            if w > largest then
+                largest = w
+            end
         end
-    end
+    )
 
     return largest
 end
