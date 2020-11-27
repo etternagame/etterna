ac855054978d8f23fe52418c6204164b730e9402
James <jameskr97@gmail.com>
2020-11-27 04:05:06 -0500

    Remove space: Single VideoCardDefaults for any platform

--- src/Etterna/Globals/StepMania.cpp
+++ src/Etterna/Globals/StepMania.cpp
@@ -462,7 +462,7 @@ CheckVideoDefaultSettings()
 	// detected - mina
 	static Preference<bool> TKGP("ResetVideoSettingsWithNewGPU", true);
 
-	VideoCardDefaults defaults{"", "d3d, opengl", 800, 600, 32, 32, 32, 1024, false};
+	VideoCardDefaults defaults{"", "d3d,opengl", 800, 600, 32, 32, 32, 1024, false};
     Locator::getLogger()->trace("Card matches '{}'.",!defaults.sDriverRegex.empty() ? defaults.sDriverRegex.c_str() : "(unknown card)");
 
 
