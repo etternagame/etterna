63c75d3ab8778148e04868531ffcdb936ead19af
Barinade <poco0317@users.noreply.github.com>
2020-12-05 18:22:26 -0600

    prevent skipping general tab tweens randomly
    sometimes this happens and sometimes this doesnt, but making this code change causes it to never happen which is better

--- BGAnimations/ScreenSelectMusic decorations/default.lua
+++ BGAnimations/ScreenSelectMusic decorations/default.lua
@@ -1,16 +1,22 @@
 local t = Def.ActorFrame {}
 
+local rightFrameVisible = true
+
 t[#t+1] = LoadActor("wheel")
 
 t[#t+1] = Def.ActorFrame {
     Name = "RightFrame",
     GeneralTabSetMessageCommand = function(self)
-        CONTEXTMAN:SetFocusedContextSet(SCREENMAN:GetTopScreen():GetName(), "Main1")
-        self:finishtweening()
-        self:smooth(0.1)
-        self:x(0)
+        if not rightFrameVisible then
+            CONTEXTMAN:SetFocusedContextSet(SCREENMAN:GetTopScreen():GetName(), "Main1")
+            self:finishtweening()
+            self:smooth(0.1)
+            self:x(0)
+            rightFrameVisible = true
+        end
     end,
     PlayerInfoFrameTabSetMessageCommand = function(self)
+        rightFrameVisible = false
         self:finishtweening()
         self:smooth(0.1)
         self:x(SCREEN_WIDTH)
