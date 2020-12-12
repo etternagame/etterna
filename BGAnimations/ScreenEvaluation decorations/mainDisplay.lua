3f5196f04f7dc213c58f1176dde920888e6a4ff7
Barinade <poco0317@users.noreply.github.com>
2020-12-11 19:31:34 -0600

    relocate and slightly modularize the judgment bar display for reusage

--- BGAnimations/ScreenEvaluation decorations/mainDisplay.lua
+++ BGAnimations/ScreenEvaluation decorations/mainDisplay.lua
@@ -241,16 +241,6 @@ local tapJudgments = {
     "TapNoteScore_Miss",
 }
 
--- list of judgments to display the bar/counts for
-local judgmentsChosen = {
-    "TapNoteScore_W1", -- marvelous
-    "TapNoteScore_W2", -- perfect
-    "TapNoteScore_W3", -- great
-    "TapNoteScore_W4", -- good
-    "TapNoteScore_W5", -- bad
-    "TapNoteScore_Miss", -- miss
-}
-
 -- list of tap/hold subtypes to display the counts for
 -- these are each a part of RadarCategory_x
 local subTypesChosen = {
@@ -262,11 +252,6 @@ local subTypesChosen = {
 }
 
 local modTextZoom = 0.6
-
-local judgmentTextZoom = 0.6
-local judgmentCountZoom = 0.6
-local judgmentPercentZoom = 0.3
-local judgmentCountPercentBump = 1 -- a bump in position added to the Count and Percent for spacing
 
 local subTypeTextZoom = 0.7
 local subTypeTextBump = 5 -- a bump in position added to the bottom left numbers for spacing
@@ -280,13 +265,6 @@ local songInfoTextSize = 0.55
 local scoreInfoTextSize = 0.8
 
 local textzoomFudge = 5
-
-
--- this number should stay the same as ApproachSeconds under metrics.ini [RollingNumbersJudgmentPercentage]
--- (or the associated RollingNumbers classes used in this file)
-local animationSeconds = 0.5
-
-local textEmbossColor = color("0,0,0,0")
 
 -- a helper to get the radar value for a score and fall back to playerstagestats if that fails
 -- it tends to fail a lot...
@@ -315,130 +293,6 @@ local function gatherRescoreTableFromScore(score)
         o["totalTaps"] = o["totalTaps"] + score:GetTapNoteScore(j)
     end
 	return o
-end
-
-local function judgmentBars()
-    local totalTaps = 0
-    local t = Def.ActorFrame {
-        Name = "JudgmentBarParentFrame",
-        SetCommand = function(self, params)
-            totalTaps = 0
-            for i, j in ipairs(judgmentsChosen) do
-                totalTaps = totalTaps + params.score:GetTapNoteScore(j)
-            end
-        end
-    }
-    local function makeJudgment(i)
-        local jdg = judgmentsChosen[i]
-        local count = 0
-
-        return Def.ActorFrame {
-            Name = "Judgment_"..i,
-            InitCommand = function(self)
-                -- finds the top of every bar given the requested spacing and the height of each bar within the allotted space
-                self:y((((i-1) * actuals.JudgmentBarHeight + (i-1) * actuals.JudgmentBarSpacing) / actuals.JudgmentBarAllottedSpace) * actuals.JudgmentBarAllottedSpace)
-            end,
-            SetCommand = function(self, params)
-                if params.score ~= nil then
-                    if params.judgeSetting ~= nil then
-                        count = getRescoredJudge(params.score:GetOffsetVector(), params.judgeSetting, i)
-                    else
-                        count = params.score:GetTapNoteScore(jdg)
-                    end
-                else
-                    count = 0
-                end
-            end,
-
-            Def.Quad {
-                Name = "BG",
-                InitCommand = function(self)
-                    self:halign(0):valign(0)
-                    self:zoomto(actuals.JudgmentBarLength, actuals.JudgmentBarHeight)
-                    self:diffuse(byJudgment(jdg))
-                    self:diffusealpha(0.5)
-                end
-            },
-            Def.Quad {
-                Name = "Fill",
-                InitCommand = function(self)
-                    self:halign(0):valign(0)
-                    self:zoomto(0, actuals.JudgmentBarHeight)
-                    self:diffuse(byJudgment(jdg))
-                    self:diffusealpha(0.5)
-                end,
-                SetCommand = function(self, params)
-                    self:finishtweening()
-                    self:smooth(animationSeconds)
-                    if params.score == nil then
-                        self:zoomx(0)
-                        return
-                    end
-                    local percent = count / totalTaps
-                    self:zoomx(actuals.JudgmentBarLength * percent)
-                end
-            },
-            LoadFont("Common Large") .. {
-                Name = "Name",
-                InitCommand = function(self)
-                    self:halign(0)
-                    self:xy(actuals.JudgmentNameLeftGap, actuals.JudgmentBarHeight / 2)
-                    self:zoom(judgmentTextZoom)
-                    self:strokecolor(textEmbossColor)
-                    -- allow 3/4 of the judgment area between the number alignment and the name alignment
-                    self:maxwidth((actuals.JudgmentBarLength - actuals.JudgmentNameLeftGap - actuals.JudgmentCountRightGap - judgmentCountPercentBump) / 4 * 3 / judgmentTextZoom)
-                    self:settext(getJudgeStrings(ms.JudgeCount[i]))
-                end
-            },
-            Def.RollingNumbers {
-                Name = "Count",
-                Font = "Common Large",
-                InitCommand = function(self)
-                    self:Load("RollingNumbersJudgmentNoLead")
-                    self:halign(1)
-                    self:xy(actuals.JudgmentBarLength - actuals.JudgmentCountRightGap - judgmentCountPercentBump, actuals.JudgmentBarHeight / 2)
-                    self:zoom(judgmentCountZoom)
-                    self:strokecolor(textEmbossColor)
-                    -- allow 1/4 of the judgment area between the number alignment and the name alignment
-                    self:maxwidth((actuals.JudgmentBarLength - actuals.JudgmentNameLeftGap - actuals.JudgmentCountRightGap - judgmentCountPercentBump) / 4 / judgmentTextZoom)
-                    self:targetnumber(0)
-                end,
-                SetCommand = function(self, params)
-                    if params.score == nil then
-                        self:targetnumber(0)
-                        return
-                    end
-                    self:targetnumber(count)
-                end
-            },
-            Def.RollingNumbers {
-                Name = "Percentage",
-                Font = "Common Large",
-                InitCommand = function(self)
-                    self:Load("RollingNumbersJudgmentPercentage")
-                    self:halign(0)
-                    self:xy(actuals.JudgmentBarLength - actuals.JudgmentCountRightGap + judgmentCountPercentBump, actuals.JudgmentBarHeight / 2)
-                    self:zoom(judgmentPercentZoom)
-                    self:maxwidth((actuals.JudgmentCountRightGap - judgmentCountPercentBump) / judgmentPercentZoom - textzoomFudge)
-                    self:strokecolor(textEmbossColor)
-                    self:targetnumber(0)
-                end,
-                SetCommand = function(self, params)
-                    if params.score == nil then
-                        self:targetnumber(0)
-                        return
-                    end
-                    local percent = count / totalTaps * 100
-                    self:targetnumber(percent)
-                end
-            }
-        }
-    end
-    for i = 1, #judgmentsChosen do
-        t[#t+1] = makeJudgment(i)
-    end
-
-    return t
 end
 
 local function subTypeStats()
@@ -1098,7 +952,15 @@ t[#t+1] = Def.ActorFrame {
             self:settext(mstr)
         end
     },
-    judgmentBars() .. {
+    LoadActorWithParams("../judgmentBars", {
+        sizing = {
+            JudgmentBarHeight = actuals.JudgmentBarHeight,
+            JudgmentBarLength = actuals.JudgmentBarLength,
+            JudgmentBarSpacing = actuals.JudgmentBarSpacing,
+            JudgmentBarAllottedSpace = actuals.JudgmentBarAllottedSpace,
+            JudgmentNameLeftGap = actuals.JudgmentNameLeftGap,
+            JudgmentCountRightGap = actuals.JudgmentCountRightGap,
+        }}) .. {
         InitCommand = function(self)
             self:xy(actuals.JudgmentBarLeftGap, actuals.JudgmentBarUpperGap)
         end
