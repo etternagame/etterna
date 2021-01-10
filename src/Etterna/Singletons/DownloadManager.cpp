bac4adeb448da4087fda4df6f2d1b5374c4a22a9
Barinade <poco0317@users.noreply.github.com>
2021-01-09 18:58:22 -0600

    Add lua access to get mirror link from downloads
    already has access to get main link

--- src/Etterna/Singletons/DownloadManager.cpp
+++ src/Etterna/Singletons/DownloadManager.cpp
@@ -2972,6 +2972,11 @@ class LunaDownloadablePack : public Luna<DownloadablePack>
 		lua_pushstring(L, p->url.c_str());
 		return 1;
 	}
+	static int GetMirror(T* p, lua_State* L)
+	{
+		lua_pushstring(L, p->mirror.c_str());
+		return 1;
+	}
 	LunaDownloadablePack()
 	{
 		ADD_METHOD(DownloadAndInstall);
@@ -2984,6 +2989,7 @@ class LunaDownloadablePack : public Luna<DownloadablePack>
 		ADD_METHOD(GetDownload);
 		ADD_METHOD(GetID);
 		ADD_METHOD(GetURL);
+		ADD_METHOD(GetMirror);
 	}
 };
 
