513dc66eea9b34ab8997aa6a215d664e58c4cf12
Barinade <poco0317@users.noreply.github.com>
2021-02-05 23:41:07 -0600

    Add Lua access to PlayerOptions FromString

--- src/Etterna/Models/Misc/PlayerOptions.cpp
+++ src/Etterna/Models/Misc/PlayerOptions.cpp
@@ -1711,6 +1711,12 @@ class LunaPlayerOptions : public Luna<PlayerOptions>
 		return 1;
 	}
 
+	static int FromString(T* p, lua_State* L)
+	{
+		p->FromString(SArg(1));
+		COMMON_RETURN_SELF;
+	}
+
 	LunaPlayerOptions()
 	{
 		ADD_METHOD(IsEasierForSongAndSteps);
@@ -1822,6 +1828,8 @@ class LunaPlayerOptions : public Luna<PlayerOptions>
 		ADD_METHOD(GetStepAttacks);
 		ADD_METHOD(ContainsTransformOrTurn);
 		ADD_METHOD(GetInvalidatingMods);
+
+		ADD_METHOD(FromString);
 	}
 };
 
