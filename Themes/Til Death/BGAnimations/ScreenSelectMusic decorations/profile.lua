41344c12f6f4ddc0478355db0006a6dc70c3fdc8
Barinade <poco0317@users.noreply.github.com>
2021-02-20 14:50:19 -0600

    Move "easy" input string function things to scripts
    this was scuffed and is now 1% less scuffed (caused crashing?)

--- Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/profile.lua
+++ Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/profile.lua
@@ -862,45 +862,6 @@ for i = 2, #ms.SkillSets do
 	r[#r + 1] = littlebits(i)
 end
 
--- these maybe should be generalized and placed into scripts -mina
-function easyInputStringWithParams(question, maxLength, isPassword, f, params)
-	SCREENMAN:AddNewScreenToTop("ScreenTextEntry")
-	local settings = {
-		Question = question,
-		MaxInputLength = maxLength,
-		Password = isPassword,
-		OnOK = function(answer)
-			f(answer, params)
-		end
-	}
-	SCREENMAN:GetTopScreen():Load(settings)
-end
-
-function easyInputStringWithFunction(question, maxLength, isPassword, f)
-	easyInputStringWithParams(
-		question,
-		maxLength,
-		isPassword,
-		function(answer, params)
-			f(answer)
-		end,
-		{}
-	)
-end
-
---Tables are passed by reference right? So the value is tablewithvalue to pass it by ref
-function easyInputString(question, maxLength, isPassword, tablewithvalue)
-	easyInputStringWithParams(
-		question,
-		maxLength,
-		isPassword,
-		function(answer, params)
-			tablewithvalue.inputString = answer
-		end,
-		{}
-	)
-end
-
 local user
 local pass
 local profilebuttons =
