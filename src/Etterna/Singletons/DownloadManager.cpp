57bd6954090679b38be1500701f778a5fcb85193
lvgx <l@vgx.fr>
2021-03-14 01:19:37 +0100

    Fix 404 error parsing of top/25 API answer
    In the JSON returned by the API "errors" is an array, not an object: this lead to a failed assert in the d["errors"].HasMember() call.

--- src/Etterna/Singletons/DownloadManager.cpp
+++ src/Etterna/Singletons/DownloadManager.cpp
@@ -1977,8 +1977,9 @@ DownloadManager::RefreshTop25(Skillset ss)
 	auto done = [ss](HTTPRequest& req, CURLMsg*) {
 		Document d;
 		if (d.Parse(req.result.c_str()).HasParseError() ||
-			(d.HasMember("errors") && d["errors"].HasMember("status") &&
-			 d["errors"]["status"].GetInt() == 404) ||
+			(d.HasMember("errors") && d["errors"].IsArray() &&
+			 d["errors"][0].HasMember("status") &&
+			 d["errors"][0]["status"].GetInt() == 404) ||
 			!d.HasMember("data") || !d["data"].IsArray()) {
 			LOG->Trace(
 			  ("Malformed top25 scores request response: " + req.result)
