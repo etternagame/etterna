05762950b4cc4786117279a0d2409e546040a59c
James <jameskr97@gmail.com>
2021-05-06 13:13:58 -0400

    Remove LowLevelWindow
    Replaced completely with IWindowBackend and GLFWWindowBackend

--- src/arch/LowLevelWindow/LowLevelWindow.cpp
+++
@@ -1,9 +0,0 @@
-#include "Etterna/Globals/global.h"
-#include "LowLevelWindow.h"
-#include "arch/arch_default.h"
-
-LowLevelWindow*
-LowLevelWindow::Create()
-{
-	return new ARCH_LOW_LEVEL_WINDOW;
-}
