38897410321d29bce275658b9167b8012578ed2c
James <jameskr97@gmail.com>
2021-05-11 00:50:45 -0400

    Update macOS loading window to use GLFW

--- src/arch/LoadingWindow/LoadingWindow_MacOSX.h
+++ src/arch/LoadingWindow/LoadingWindow_MacOSX.h
@@ -2,17 +2,23 @@
 #define LOADING_WINDOW_MACOSX_H
 
 #include "LoadingWindow.h"
+#include "Core/Platform/Window/GLFWWindowBackend.hpp"
+
+#include <memory>
+
 /** @brief Loading window for Mac OS X. */
-class LoadingWindow_MacOSX : public LoadingWindow
-{
-  public:
+class LoadingWindow_MacOSX : public LoadingWindow {
+public:
 	LoadingWindow_MacOSX();
-	~LoadingWindow_MacOSX();
-	void SetText(const std::string& str);
-	void SetSplash(const RageSurface* pSplash);
-	void SetProgress(const int progress);
-	void SetTotalWork(const int totalWork);
-	void SetIndeterminate(bool indeterminate);
+	~LoadingWindow_MacOSX() override;
+	void SetText(const std::string& str) override;
+	void SetSplash(const RageSurface* pSplash) override {}
+	void SetProgress(int progress) override;
+	void SetTotalWork(int totalWork) override;
+	void SetIndeterminate(bool indeterminate) override;
+
+private:
+    std::unique_ptr<Core::Platform::Window::GLFWWindowBackend> window;
 };
 #define USE_LOADING_WINDOW_MACOSX
 
