969b8dbf083f724dd945959f54b6026f1920d567
Barinade <poco0317@users.noreply.github.com>
2021-06-13 23:08:58 -0500

    add screen transitions

---
+++ BGAnimations/Screen in.lua
@@ -0,0 +1,10 @@
+-- defines the inward screen transition for every screen
+-- this is a full screen fade out of black
+return Def.Quad {
+	InitCommand = function(self)
+		self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y):zoomto(SCREEN_WIDTH, SCREEN_HEIGHT)
+	end,
+	OnCommand = function(self)
+		self:diffuse(color("0,0,0,1")):sleep(0.1):linear(0.1):diffusealpha(0)
+	end
+}
---
+++ BGAnimations/Screen out.lua
@@ -0,0 +1,10 @@
+-- defines the outward screen transition for every screen
+-- this is a full screen fade into black
+return Def.Quad {
+	InitCommand = function(self)
+		self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y):zoomto(SCREEN_WIDTH, SCREEN_HEIGHT)
+	end,
+	OnCommand = function(self)
+		self:diffuse(color("0,0,0,0")):sleep(0.1):linear(0.1):diffusealpha(1)
+	end
+}
