a95ac60b41fd06a6e0e4ccab1d75c25f1444e962
Barinade <poco0317@users.noreply.github.com>
2021-07-24 00:00:42 -0500

    reorganize and clean displaypercent
    yes i did it twice

--- BGAnimations/ScreenGameplay overlay/elements/displaypercent.lua
+++ BGAnimations/ScreenGameplay overlay/elements/displaypercent.lua
@@ -1,17 +1,39 @@
 -- the wifepercent display. it displays the wifepercent
 
+-- i dunno less copy paste whatever bro
+local formatstr = "%05.2f%%"
+local wifepercentTextSize = 0.3
+
 return Def.ActorFrame {
 	Name = "DisplayPercent",
 	InitCommand = function(self)
         self:xy(MovableValues.DisplayPercentX, MovableValues.DisplayPercentY)
 		self:zoom(MovableValues.DisplayPercentZoom)
 	end,
+    SpottedOffsetCommand = function(self, params)
+        local bg = self:GetChild("PercentBacking")
+        local perc = self:GetChild("DisplayPercent")
+
+        -- kind of putting all the logic in one place here
+        -- modify text then update bg for it
+        if params ~= nil and params.wifePercent ~= nil then
+            if perc then
+                perc:settextf(formatstr, params.wifePercent)
+            end
+        else
+            if perc then
+                perc:settextf(formatstr, 0)
+            end
+        end
+        if bg and perc then
+            bg:zoomto(perc:GetZoomedWidth() + 4, perc:GetZoomedHeight() + 4)
+        end
+    end,
 
 	Def.Quad {
         Name = "PercentBacking",
 		InitCommand = function(self)
             self:halign(1):valign(0)
-			self:zoomto(60, 13)
             self:diffuse(color("0,0,0,0.4"))
 		end
 	},
@@ -19,17 +41,8 @@ return Def.ActorFrame {
         Name = "DisplayPercent",
         InitCommand = function(self)
             self:halign(1):valign(0)
-            self:zoom(0.3)
+            self:zoom(wifepercentTextSize)
+            -- maybe we want to set the text color here
         end,
-        BeginCommand = function(self)
-            self:settextf("%05.2f%%", 0)
-        end,
-        SpottedOffsetCommand = function(self, params)
-            if params.wifePercent ~= nil then
-                self:settextf("%05.2f%%", wifey)
-            else
-                self:settextf("%05.2f%%", 0)
-            end
-        end
-    },
+    }
 }
