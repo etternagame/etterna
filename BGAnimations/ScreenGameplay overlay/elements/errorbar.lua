26643a741a1ef84192aad062ed258d8670e34af1
Barinade <poco0317@users.noreply.github.com>
2021-07-24 00:00:29 -0500

    reorganize and clean errorbar

--- BGAnimations/ScreenGameplay overlay/elements/errorbar.lua
+++ BGAnimations/ScreenGameplay overlay/elements/errorbar.lua
@@ -1,27 +1,48 @@
-local barcount = playerConfig:get_data().ErrorBarCount -- Number of bars. Older bars will refresh if judgments/barDuration exceeds this value.
-local barWidth = 2 -- Width of the ticks.
-local barDuration = 0.75 -- Time duration in seconds before the ticks fade out. Doesn't need to be higher than 1. Maybe if you have 300 bars I guess.
+-- Number of bars. Older bars will refresh if judgments/barDuration exceeds this value.
+local barcount = playerConfig:get_data().ErrorBarCount
+-- Width of the bars.
+local barWidth = 2
+-- Time duration in seconds before the ticks fade out. Doesn't need to be higher than 1. Maybe if you have 300 bars I guess.
+local barDuration = 0.75
+
 if barcount > 50 then barDuration = barcount / 50 end -- just procedurally set the duration if we pass 50 bars
---==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--
+
 local currentbar = 1 -- so we know which error bar we need to update
 local ingots = {} -- references to the error bars
-local alpha = 0.07 -- ewma alpha
+local ewmaAlpha = 0.07
 local avg
 local lastAvg
 
--- Makes the error bars. They position themselves relative to the center of the screen based on your dv and diffuse to your judgment value before disappating or refreshing
+-- to relate an offset to position within the bar
+-- max offset is 180ms
+local wscale = barWidth / 180
+
+local earlylateTextSize = 0.35
+
+-- the error bar can either load as Regular or Exponential Weighted Moving Average
+-- Regular loads a certain number of bars and places them continuously
+-- EWMA loads 1 bar and places it according to the EWMA of the previous n taps
+local errorbarType = playerConfig:get_data().ErrorBar == 1 and "Regular" or "EWMA"
+
+-- procedurally generated error bars
 local function smeltErrorBar(index)
 	return Def.Quad {
 		Name = index,
 		InitCommand = function(self)
-			self:xy(MovableValues.ErrorBarX, MovableValues.ErrorBarY):zoomto(barWidth, MovableValues.ErrorBarHeight):diffusealpha(0)
+			self:xy(MovableValues.ErrorBarX, MovableValues.ErrorBarY)
+			self:zoomto(barWidth, MovableValues.ErrorBarHeight)
+			self:diffusealpha(0)
 		end,
-		UpdateErrorBarCommand = function(self)
+		UpdateErrorBarCommand = function(self, params)
+			if not params or params.judgeCurrent == nil or params.judgeOffset == nil then return end
+			-- if this bar was somehow already in use, quickly move it along
 			self:hurrytweening(0.1)
 			self:diffusealpha(1)
-			self:diffuse(jcT[jdgCur])
+			-- now make it the color for this new judgment
+			self:diffuse(colorByJudgment(params.judgeCurrent))
+			-- and set up the position
 			if MovableValues and MovableValues.ErrorBarX then
-				self:x(MovableValues.ErrorBarX + dvCur * wscale)
+				self:x(MovableValues.ErrorBarX + params.judgeOffset * wscale)
 				self:y(MovableValues.ErrorBarY)
 				self:zoomtoheight(MovableValues.ErrorBarHeight)
 			end
@@ -34,10 +55,10 @@ local function smeltErrorBar(index)
 	}
 end
 
-local e = Def.ActorFrame {
+local t = Def.ActorFrame {
 	Name = "ErrorBar",
 	InitCommand = function(self)
-		if enabledErrorBar == 1 then
+		if errorbarType == "Regular" then
 			for i = 1, barcount do
 				ingots[#ingots + 1] = self:GetChild(i)
 			end
@@ -46,50 +67,34 @@ local e = Def.ActorFrame {
 			lastAvg = 0
 		end
 	end,
-	SpottedOffsetCommand = function(self)
-		if enabledErrorBar == 1 then
+	SpottedOffsetCommand = function(self, params)
+		if errorbarType == "Regular" then
 			if dvCur ~= nil then
 				currentbar = ((currentbar) % barcount) + 1
-				ingots[currentbar]:playcommand("UpdateErrorBar") -- Update the next bar in the queue
+				ingots[currentbar]:playcommand("UpdateErrorBar", params) -- Update the next bar in the queue
 			end
 		end
 	end,
 	DootCommand = function(self)
 		self:RemoveChild("DestroyMe")
 		self:RemoveChild("DestroyMe2")
-
-		if enabledErrorBar == 1 then
-			self:RemoveChild("WeightedBar")
-		end
 	end,
-	Def.Quad {
-		Name = "WeightedBar",
-		InitCommand = function(self)
-			if enabledErrorBar == 2 then
-				self:xy(MovableValues.ErrorBarX, MovableValues.ErrorBarY):zoomto(barWidth, MovableValues.ErrorBarHeight):diffusealpha(1):diffuse(getMainColor("enabled"))
-			else
-				self:visible(false)
-			end
-		end,
-		SpottedOffsetCommand = function(self)
-			if enabledErrorBar == 2 and dvCur ~= nil then
-				avg = alpha * dvCur + (1 - alpha) * lastAvg
-				lastAvg = avg
-				self:x(MovableValues.ErrorBarX + avg * wscale)
-			end
-		end
-	},
+
 	Def.Quad {
 		Name = "Center",
 		InitCommand = function(self)
-			self:diffuse(getMainColor("highlight")):xy(MovableValues.ErrorBarX, MovableValues.ErrorBarY):zoomto(2, MovableValues.ErrorBarHeight)
+			self:xy(MovableValues.ErrorBarX, MovableValues.ErrorBarY)
+			self:zoomto(2, MovableValues.ErrorBarHeight)
+			self:diffuse(COLORS:getGameplayColor("errorBarCenter"))
 		end
 	},
+
 	-- Indicates which side is which (early/late) These should be destroyed after the song starts.
 	LoadFont("Common Normal") .. {
         Name = "DestroyMe",
         InitCommand = function(self)
-            self:xy(MovableValues.ErrorBarX + errorBarFrameWidth / 4, MovableValues.ErrorBarY):zoom(0.35)
+            self:xy(MovableValues.ErrorBarX + errorBarFrameWidth / 4, MovableValues.ErrorBarY)
+			self:zoom(earlylateTextSize)
         end,
         BeginCommand = function(self)
             self:settext(translated_info["ErrorLate"])
@@ -99,7 +104,8 @@ local e = Def.ActorFrame {
 	LoadFont("Common Normal") .. {
         Name = "DestroyMe2",
         InitCommand = function(self)
-            self:xy(MovableValues.ErrorBarX - errorBarFrameWidth / 4, MovableValues.ErrorBarY):zoom(0.35)
+            self:xy(MovableValues.ErrorBarX - errorBarFrameWidth / 4, MovableValues.ErrorBarY)
+			self:zoom(earlylateTextSize)
         end,
         BeginCommand = function(self)
             self:settext(translated_info["ErrorEarly"])
@@ -110,6 +116,25 @@ local e = Def.ActorFrame {
         end
     },
 }
+
+if errorbarType == "EWMA" then
+	t[#t+1] = Def.Quad {
+		Name = "WeightedBar",
+		InitCommand = function(self)
+			self:xy(MovableValues.ErrorBarX, MovableValues.ErrorBarY)
+			self:zoomto(barWidth, MovableValues.ErrorBarHeight)
+			self:diffuse(COLORS:getGameplayColor("errorBarEWMABar"))
+			self:diffusealpha(1)
+		end,
+		SpottedOffsetCommand = function(self, params)
+			if enabledErrorBar == 2 and dvCur ~= nil then
+				avg = alpha * dvCur + (1 - alpha) * lastAvg
+				lastAvg = avg
+				self:x(MovableValues.ErrorBarX + avg * wscale)
+			end
+		end
+	}
+end
 
 if enabledErrorBar == 1 then
 	for i = 1, barcount do
