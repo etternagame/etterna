9f40e1c804179b0306d17ac5aa2f098df53f59b8
ulti_fd <Ulti-FD@users.noreply.github.com>
2021-07-26 01:34:59 -0400

    forgot to put RS back in whoops ecks dee

--- Themes/Til Death/Scripts/02 OptionsMenu.lua
+++ Themes/Til Death/Scripts/02 OptionsMenu.lua
@@ -1,6 +1,6 @@
 function getPlayerOptionsList(itemSet)
 	local Items = {
-		["Main"] = "Speed,RateList,NoteSk,PRAC,DownSc,Center,Persp,LC,BG,SF,Background,Judge,Life,Fail,Score",
+		["Main"] = "Speed,RateList,NoteSk,RS,PRAC,DownSc,Center,Persp,LC,BG,SF,Background,Judge,Life,Fail,Score",
 		["Theme"] = "CG,CBHL,JT,CT,DP,TT,TG,TTM,JC,EB,EBC,PI,FBP,FB,MB,LEADB,NPS",
 		["Effect"] = "Persp,App,GHO,SHO,Acc,Hide,Effect1,Effect2,Scroll,Turn,Insert,R1,R2,Holds,Mines"
 	}
