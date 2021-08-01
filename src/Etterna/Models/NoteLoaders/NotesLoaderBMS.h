4dfb137ce2e944d476312343244fb89dbd698690
staticssleever668 <64694940+staticssleever668@users.noreply.github.com>
2021-08-01 21:19:09 +0300

    Models NotesLoaderBMS: add missing includes

--- src/Etterna/Models/NoteLoaders/NotesLoaderBMS.h
+++ src/Etterna/Models/NoteLoaders/NotesLoaderBMS.h
@@ -1,12 +1,16 @@
 #ifndef NOTES_LOADER_BMS_H
 #define NOTES_LOADER_BMS_H
 
+#include <string>
+#include <vector>
+
 class Song;
 class Steps;
+
 /** @brief Reads a Song from a set of .BMS files. */
 namespace BMSLoader {
 void
-GetApplicableFiles(const std::string& sPath, vector<std::string>& out);
+GetApplicableFiles(const std::string& sPath, std::vector<std::string>& out);
 bool
 LoadFromDir(const std::string& sDir, Song& out);
 bool
