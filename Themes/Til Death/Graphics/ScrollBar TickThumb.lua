99fbff191b8b099c2b1bca32d6db31cef6a3e078
Barinade <poco0317@users.noreply.github.com>
2021-08-05 00:54:26 -0500

    somewhat fix til death musicwheel scroller bar y position

--- Themes/Til Death/Graphics/ScrollBar TickThumb.lua
+++ Themes/Til Death/Graphics/ScrollBar TickThumb.lua
@@ -13,11 +13,11 @@ t[#t + 1] = Def.ActorFrame {
 
 					local mx = SCREEN_WIDTH - INPUTFILTER:GetMouseX()
 					local my = SCREEN_HEIGHT - INPUTFILTER:GetMouseY()
-					if mx < 32 and mx > 0 and my < 440 and my > 48 then
+					if mx < 32 and mx > 0 and my < 442 and my > 48 then
 						self:SetUpdateFunction(function(self)
 							local mx = SCREEN_WIDTH - INPUTFILTER:GetMouseX()
 							local my = SCREEN_HEIGHT - INPUTFILTER:GetMouseY()
-							if moving or mx < 32 and mx > 0 and my < 440 and my > 48 then
+							if moving or mx < 32 and mx > 0 and my < 442 and my > 48 then
 								moving = true
 								self:playcommand("ClickingMusicWheelScroller")
 							end
