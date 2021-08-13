877d559f3c85df4a7d029bf57e4bf9ec9d989f2a
ulti_fd <Ulti-FD@users.noreply.github.com>
2021-08-13 01:15:14 -0400

    "downscroll" option --> "scroll direction"

--- Themes/Til Death/Scripts/02 OptionsMenu.lua
+++ Themes/Til Death/Scripts/02 OptionsMenu.lua
@@ -1,6 +1,6 @@
 function getPlayerOptionsList(itemSet)
 	local Items = {
-		["Main"] = "Speed,RateList,NoteSk,RS,PRAC,CG,DownSc,Center,Persp,LC,BG,SF,Background,Judge,Life,Fail,Score",
+		["Main"] = "Speed,RateList,NoteSk,RS,PRAC,CG,ScrollDir,Center,Persp,LC,BG,SF,Background,Judge,Life,Fail,Score",
 		["Theme"] = "CBHL,JT,CT,DP,TT,TG,TTM,JC,EB,EBC,PI,FBP,FB,MB,LEADB,NPS",
 		["Effect"] = "Persp,App,GHO,SHO,Acc,Hide,Effect1,Effect2,Scroll,Turn,Insert,R1,R2,Holds,Mines"
 	}
