5fd412072db8185bea5a2b86d1df70e412369d72
James <jameskr97@gmail.com>
2021-09-29 17:39:21 -0400

    Update CrashpadHandler to upload log file

--- src/Core/Crash/CrashpadHandler.cpp
+++ src/Core/Crash/CrashpadHandler.cpp
@@ -12,7 +12,7 @@
 #include <string>
 #include <tuple>
 
-std::tuple<base::FilePath, base::FilePath, bool> getCrashpadInfo(){
+std::tuple<base::FilePath, base::FilePath, base::FilePath, bool> getCrashpadInfo(){
     auto exeDir = Core::Platform::getExecutableDirectory();
     auto dataPath = exeDir / "CrashData";
 
@@ -23,6 +23,7 @@ std::tuple<base::FilePath, base::FilePath, bool> getCrashpadInfo(){
      * - If it is not an absolute path, it is assumed to be the executable name, for the exe file
      * place in the same directory as this binary.
      */
+	auto logFilePath = ghc::filesystem::path(Locator::getLogger()->getLogFile());
     auto handlerPath = ghc::filesystem::path{Core::AppInfo::CRASHPAD_HANDLER_EXE};
     if (!handlerPath.is_absolute())
         handlerPath = exeDir / handlerPath;
@@ -30,14 +31,18 @@ std::tuple<base::FilePath, base::FilePath, bool> getCrashpadInfo(){
 #ifdef _WIN32
     base::FilePath handler(handlerPath.generic_wstring());
     base::FilePath dataDir(dataPath.generic_wstring());
+	base::FilePath logFile(logFilePath.generic_wstring());
+
 	const bool startHandlerFromBGThread = true;
 #else
     base::FilePath handler(handlerPath);
     base::FilePath dataDir(dataPath);
+	base::FilePath logFile(logFilePath);
+
 	const bool startHandlerFromBGThread = false; // not available, assert crash if true
 #endif
 
-    return {handler, dataDir, startHandlerFromBGThread};
+    return {handler, dataDir, logFile, startHandlerFromBGThread};
 }
 
 /**
@@ -48,24 +53,22 @@ std::tuple<base::FilePath, base::FilePath, bool> getCrashpadInfo(){
  * @return True if successfully initialized, False if otherwise.
  */
 bool Core::Crash::initCrashpad() {
-    auto[handler, dataDir, startHandlerFromBGThread] = getCrashpadInfo();
+    auto[handler, dataDir, logFile, startHandlerFromBGThread] = getCrashpadInfo();
 
     auto database = crashpad::CrashReportDatabase::Initialize(dataDir);
     if (database == nullptr) return false;
 
     auto *client = new crashpad::CrashpadClient();
     bool status = client->StartHandler(
-        handler, // relative path to executable handler file
-        dataDir, // the crashpad database
-        dataDir, // an existing directory for metrics data
-
-        // an upload server. A url is required, even if nothing is uploaded.
-        "https://crash.etterna.dev/api/minidump/upload?api_key=0b03527eb91c4d4f8d7576cf4e4939c4",
-        {}, // crash report metadata
-        {"--no-upload-gzip"}, // any additional handler arguments
-        true, // auto-restart if handler dies
-        startHandlerFromBGThread // start handler from background thread (windows only)
-    );
+		handler, // relative path to executable handler file
+		dataDir, // the crashpad database
+		dataDir, // an existing directory for metrics data
+		"https://crash.etterna.dev/api/minidump/upload?api_key=0b03527eb91c4d4f8d7576cf4e4939c4", // an upload server
+		{}, // crash report metadata
+		{"--no-upload-gzip"}, // any additional handler arguments
+		true, // auto-restart if handler dies
+		startHandlerFromBGThread,
+		{logFile});
     return status;
 }
 
@@ -82,7 +85,7 @@ void Core::Crash::generateMinidump() {
  * @param shouldUpload True if game should upload minidump on crash, false otherwise.
  */
 void Core::Crash::setShouldUpload(bool shouldUpload) {
-    auto[handler, dataDir, BGthread] = getCrashpadInfo();
+    auto[handler, dataDir, logFile, BGthread] = getCrashpadInfo();
 
     auto database = crashpad::CrashReportDatabase::InitializeWithoutCreating(dataDir);
     if (database != nullptr && database->GetSettings() != nullptr) {
