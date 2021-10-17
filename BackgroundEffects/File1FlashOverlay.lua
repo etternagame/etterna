920554344e1be4271b142b99c39bee5e032c3e68
Barinade <poco0317@users.noreply.github.com>
2021-10-17 16:39:51 -0500

    tabs-to-spacening for global bgeffects and bganimations

--- BackgroundEffects/File1FlashOverlay.lua
+++ BackgroundEffects/File1FlashOverlay.lua
@@ -4,19 +4,19 @@ local Color2 = color(Var "Color2")
 local t = Def.ActorFrame {}
 
 t[#t + 1] =
-	LoadActor(Var "File1") ..
-	{
-		OnCommand = function(self)
-			self:diffuse(Color1):blend("BlendMode_Add"):x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y):scale_or_crop_background():linear(
-				1
-			):diffusealpha(0)
-		end,
-		GainFocusCommand = function(self)
-			self:play()
-		end,
-		LoseFocusCommand = function(self)
-			self:pause()
-		end
-	}
+    LoadActor(Var "File1") ..
+    {
+        OnCommand = function(self)
+            self:diffuse(Color1):blend("BlendMode_Add"):x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y):scale_or_crop_background():linear(
+                1
+            ):diffusealpha(0)
+        end,
+        GainFocusCommand = function(self)
+            self:play()
+        end,
+        LoseFocusCommand = function(self)
+            self:pause()
+        end
+    }
 
 return t
--- BackgroundEffects/OverlayAdd.lua
+++ BackgroundEffects/OverlayAdd.lua
@@ -3,17 +3,17 @@ local Color1 = color(Var "Color1")
 local t = Def.ActorFrame {}
 
 t[#t + 1] =
-	LoadActor(Var "File1") ..
-	{
-		OnCommand = function(self)
-			self:diffuse(Color1):blend("BlendMode_Add"):x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y):scale_or_crop_background()
-		end,
-		GainFocusCommand = function(self)
-			self:play()
-		end,
-		LoseFocusCommand = function(self)
-			self:pause()
-		end
-	}
+    LoadActor(Var "File1") ..
+    {
+        OnCommand = function(self)
+            self:diffuse(Color1):blend("BlendMode_Add"):x(SCREEN_CENTER_X):y(SCREEN_CENTER_Y):scale_or_crop_background()
+        end,
+        GainFocusCommand = function(self)
+            self:play()
+        end,
+        LoseFocusCommand = function(self)
+            self:pause()
+        end
+    }
 
 return t
