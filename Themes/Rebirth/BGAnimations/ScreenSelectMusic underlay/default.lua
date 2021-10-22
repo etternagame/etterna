c9fbe3c73eb7c0023ba2e90faeb093b813ce8091
Barinade <poco0317@users.noreply.github.com>
2021-10-21 23:04:22 -0500

    dynamic update bg with showbackgrounds pref

--- Themes/Rebirth/BGAnimations/ScreenSelectMusic underlay/default.lua
+++ Themes/Rebirth/BGAnimations/ScreenSelectMusic underlay/default.lua
@@ -1,10 +1,27 @@
+local showbg = function() return themeConfig:get_data().global.ShowBackgrounds end
 local t = Def.ActorFrame {
     Name = "UnderlayFile",
     WheelSettledMessageCommand = function(self, params)
         -- cascade visual update to everything
-        self:playcommand("Set", {song = params.song, group = params.group, hovered = params.hovered, steps = params.steps})
-    end
+        self:playcommand("Set", {
+            song = params.song,
+            group = params.group,
+            hovered = params.hovered,
+            steps = params.steps
+        })
+    end,
 }
+
+local function loadbg(self)
+    self:finishtweening()
+    self:visible(true)
+    if self.bgpath == self.loadedpath then return end
+    self:LoadBackground(self.bgpath)
+    self.loadedpath = self.bgpath
+    self:scale_or_crop_background()
+    self:smooth(0.5)
+    self:diffusealpha(0.3)
+end
 
 -- reset context manager as early as possible in the selectmusic init process
 -- this should be a safe place to do it, between all context manager registrations
@@ -17,16 +34,27 @@ t[#t+1] = Def.Sprite {
     end,
     SetCommand = function(self, params)
         self:finishtweening()
+        self.bgpath = nil
         if params.song and params.song:GetBackgroundPath() then
-            self:visible(true)
-            self:LoadBackground(params.song:GetBackgroundPath())
-            self:scale_or_crop_background()
-            self:smooth(0.5)
-            self:diffusealpha(0.3)
+            self.bgpath = params.song:GetBackgroundPath()
+            if not showbg() then self:visible(false) return end
+            loadbg(self)
         else
             self:visible(false)
         end
-    end
+    end,
+    OptionUpdatedMessageCommand = function(self, params)
+        if params and params.name == "Show Backgrounds" then
+            if self.bgpath then
+                if showbg() then
+                    self:visible(true)
+                    loadbg(self)
+                else
+                    self:visible(false)
+                end
+            end
+        end
+    end,
 }
 
 t[#t+1] = Def.Quad {
@@ -54,7 +82,7 @@ t[#t+1] = Def.Quad {
         else
             self:visible(false)
         end
-    end
+    end,
 }
 
 return t
