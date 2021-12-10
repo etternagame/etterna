5595b321f8dd42991631a7469896cc9feb8c1a92
Barinade <poco0317@users.noreply.github.com>
2021-12-09 18:15:35 -0600

    egg

--- Themes/Rebirth/BGAnimations/ScreenTitleMenu overlay/default.lua
+++ Themes/Rebirth/BGAnimations/ScreenTitleMenu overlay/default.lua
@@ -3,5 +3,6 @@ local t = Def.ActorFrame {Name = "OverlayFile"}
 t[#t+1] = LoadActor("profileSelect")
 t[#t+1] = LoadActor(THEME:GetPathG("", "_crashUploadOptIn"))
 t[#t+1] = LoadActor("../_mouse.lua")
+t[#t+1] = EGGMAN.snowyboy()
 
 return t
