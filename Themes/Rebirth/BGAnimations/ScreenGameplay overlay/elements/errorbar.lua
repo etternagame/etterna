f321f3d3e293d11d679303446ace0c038005c61c
Barinade <poco0317@users.noreply.github.com>
2021-12-12 15:37:19 -0600

    rebirth fix error bar resize increments

--- Themes/Rebirth/BGAnimations/ScreenGameplay overlay/elements/errorbar.lua
+++ Themes/Rebirth/BGAnimations/ScreenGameplay overlay/elements/errorbar.lua
@@ -75,7 +75,7 @@ local t = Def.ActorFrame {
         registerActorToCustomizeGameplayUI({
             actor = self,
             coordInc = {5,1},
-            zoomInc = {0.1,0.05},
+            zoomInc = {5,1},
         })
     end,
     SetUpMovableValuesMessageCommand = function(self)
