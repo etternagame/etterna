b4b105f729039c0be7da1815caccb89b85ad9c74
Barinade <poco0317@users.noreply.github.com>
2022-03-13 19:18:31 -0500

    rebirth version iteration

--- Themes/Rebirth/Scripts/00 ThemeInfo.lua
+++ Themes/Rebirth/Scripts/00 ThemeInfo.lua
@@ -1,7 +1,7 @@
 THEMEINFO = {
     Name = "Rebirth",
-    Version = "1",
-    Date = "20211204",
+    Version = "2",
+    Date = "20220313",
     Author = "Celebelian, poco0317, and the Etterna Team"
 }
 
