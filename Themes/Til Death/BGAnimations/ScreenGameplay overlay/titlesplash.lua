b57764807c03a61ef83be8fdb1f2f97ff2f63a7a
Barinade <poco0317@users.noreply.github.com>
2022-12-12 21:21:15 -0600

    til death add chart author credit to gameplay splash

--- Themes/Til Death/BGAnimations/ScreenGameplay overlay/titlesplash.lua
+++ Themes/Til Death/BGAnimations/ScreenGameplay overlay/titlesplash.lua
@@ -13,9 +13,11 @@ local t = Def.ActorFrame {
 	Def.Quad {
 		Name = "DestroyMe",
 		InitCommand = function(self)
-			self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y - 40):zoomto(400, 50):diffuse(getMainColor("highlight")):fadeleft(0.4):faderight(
-				0.4
-			):diffusealpha(0)
+			self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y - 37)
+			self:zoomto(400, 58)
+			self:diffuse(getMainColor("highlight"))
+			self:fadeleft(0.4):faderight(0.4)
+			self:diffusealpha(0)
 		end,
 		OnCommand = function(self)
 			self:smooth(0.5):diffusealpha(0.7):sleep(1):smooth(0.3):smooth(0.4):diffusealpha(0)
@@ -65,9 +67,22 @@ local t = Def.ActorFrame {
 		end
 	},
 	LoadFont("Common Normal") .. {
+		Name = "DestroyMe6",
+		InitCommand = function(self)
+			self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y - 15):zoom(0.3):diffusealpha(0)
+		end,
+		BeginCommand = function(self)
+			local auth = GAMESTATE:GetCurrentSong():GetOrTryAtLeastToGetSimfileAuthor()
+			self:settextf("By: %s", auth)
+		end,
+		OnCommand = function(self)
+			self:smooth(0.5):diffusealpha(1):sleep(1):smooth(0.3):smooth(0.4):diffusealpha(0)
+		end
+	},
+	LoadFont("Common Normal") .. {
 		Name = "DestroyMe5",
 		InitCommand = function(self)
-			self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y - 10):zoom(0.7):diffusealpha(0):valign(0)
+			self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y):zoom(0.7):diffusealpha(0):valign(0)
 		end,
 		BeginCommand = function(self)
 			mods = GAMESTATE:GetPlayerState():GetCurrentPlayerOptions():GetInvalidatingMods()
