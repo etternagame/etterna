983501345da1a44fdc6aec88df24f5d04fb4c6b7
Andrew Plotner <bluebandit21@gmail.com>
2023-01-06 18:58:02 -0500

    Install graphviz in Etterna Doc CI to properly build DOT files

--- .ci/generate_doc_configs.sh
+++ .ci/generate_doc_configs.sh
@@ -4,7 +4,7 @@
 sudo apt-get install build-essential libssl-dev libx11-dev libxrandr-dev libcurl4-openssl-dev libglu1-mesa-dev libpulse-dev libogg-dev libasound-dev libjack-dev
 
 #Install doxygen
-sudo apt-get install doxygen 
+sudo apt-get install doxygen graphviz
 
 eval "$(luarocks path --bin)"
 
