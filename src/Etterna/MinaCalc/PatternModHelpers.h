9ec41de054fb0db7960b3df4f9d219ca08f68074
Andrew Plotner <bluebandit21@gmail.com>
2024-03-11 01:27:09 -0400

    Add SSE2Neon external dependency
    The calc code uses x86_64 SIMD instrinsics for speed.
    However, those are unavailable on ARM.
    
    SSE2Neon provides ARM equivalents of each missing instruction
     using ARM's Neon SIMD instructions, providing equivalent
     speed for the majority of x86_64 SIMD instructions :)

--- src/Etterna/MinaCalc/PatternModHelpers.h
+++ src/Etterna/MinaCalc/PatternModHelpers.h
@@ -3,7 +3,12 @@
 #include <array>
 #include <vector>
 #include <algorithm>
-#include <xmmintrin.h>
+#ifndef __aarch64__
+	#include <xmmintrin.h>
+#else
+	//Use sse2neon to transparently provide ARM Neon equivalents of x86_64 SIMD intrinsics
+	#include "sse2neon.h"
+#endif
 #include <numeric>
 #include <cstring>
 
