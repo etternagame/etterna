a278b9ca752b0cf4ce39059832b2ccc90600744a
Vezxe <ayyitscreo@gmail.com>
2024-09-16 06:27:24 +0000

    Add scroll debounce setting + use it in themes.

--- Themes/Til Death/BGAnimations/_mousewheelscroll.lua
+++ Themes/Til Death/BGAnimations/_mousewheelscroll.lua
@@ -3,11 +3,12 @@ local whee
 local pressingtab = false
 local top
 
+local last_scroll_input = GetTimeSinceStart()
 local function scrollInput(event)
 	if top:GetName() == "ScreenSelectmusic" then
 		if top:GetSelectionState() == 2 then
 			return
-		end	
+		end
 	elseif event.DeviceInput.button == "DeviceButton_tab" then
 		if event.type == "InputEventType_FirstPress" then
 			pressingtab = true
@@ -15,14 +16,20 @@ local function scrollInput(event)
 			pressingtab = false
 		end
 	elseif event.type == "InputEventType_FirstPress" then
-		if event.DeviceInput.button == "DeviceButton_mousewheel up" then
+	    local DI_Button = event.DeviceInput.button
+		local Current_Time = GetTimeSinceStart()
+		local scroll_debounce_time = PREFSMAN:GetPreference("ScrollDebounceTime")
+		if string.find(DI_Button,"DeviceButton_mousewheel") and (Current_Time - last_scroll_input) > scroll_debounce_time then
+		last_scroll_input = Current_Time
+
+		if DI_Button == "DeviceButton_mousewheel up" then
 			moving = true
 			if pressingtab == true and not whee:IsSettled() then
 				whee:Move(-2)
 			else
 				whee:Move(-1)
 			end
-		elseif event.DeviceInput.button == "DeviceButton_mousewheel down" then
+		elseif DI_Button == "DeviceButton_mousewheel down" then
 			moving = true
 			if pressingtab == true and not whee:IsSettled() then
 				whee:Move(2)
@@ -30,11 +37,12 @@ local function scrollInput(event)
 				whee:Move(1)
 			end
 		end
+		end
 	elseif moving == true then
 		whee:Move(0)
 		moving = false
 	end
-	
+
 	return false
 end
 
