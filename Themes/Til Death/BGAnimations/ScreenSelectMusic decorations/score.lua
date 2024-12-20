46dd0acfbff9707162a43d84cda96bc33d041a08
Barinade <poco0317@users.noreply.github.com>
2024-12-19 20:12:56 -0600

    til death add message to score tab when no chart is hovered

--- Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/score.lua
+++ Themes/Til Death/BGAnimations/ScreenSelectMusic decorations/score.lua
@@ -51,6 +51,7 @@ local translated_info = {
 	ChordCohesion = THEME:GetString("TabScore", "ChordCohesion"),
 	Judge = THEME:GetString("TabScore", "ScoreJudge"),
 	NoScores = THEME:GetString("TabScore", "NoScores"),
+	NoChart = THEME:GetString("TabScore", "NoChart"),
 	Yes = THEME:GetString("OptionNames", "Yes"),
 	No = THEME:GetString("OptionNames", "No"),
 	ShowOffset = THEME:GetString("TabScore", "ShowOffsetPlot"),
@@ -519,7 +520,11 @@ local l = Def.ActorFrame {
 	LoadFont("Common Normal") .. {
 		InitCommand = function(self)
 			self:xy(frameWidth - offsetX - frameX, frameHeight - headeroffY - 15 - offsetY):zoom(0.5):halign(1)
-			self:settext(translated_info["NoScores"])
+			if GAMESTATE:GetCurrentSteps() == nil then
+				self:settext(translated_info["NoChart"])
+			else
+				self:settext(translated_info["NoScores"])
+			end
 		end,
 		DisplayCommand = function(self)
 			self:settextf("%s %s - %s %d/%d", translated_info["Rate"], rates[rateIndex], translated_info["Showing"], scoreIndex, #rtTable[rates[rateIndex]])
