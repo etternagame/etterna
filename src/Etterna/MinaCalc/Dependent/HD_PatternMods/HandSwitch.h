7b5aaf24d1357502355fc63317c5df1dfeb47fef
StarbotArc <StarbotArc@users.noreply.github.com>
2025-03-22 00:31:22 -0400

    Back to work I go.

--- src/Etterna/MinaCalc/Dependent/HD_PatternMods/HandSwitch.h
+++ src/Etterna/MinaCalc/Dependent/HD_PatternMods/HandSwitch.h
@@ -3,18 +3,29 @@
 
 // 241 should trigger
 
+static float bell_curve(float a)
+{
+	return 1.F / fastpow(100, fastpow(a, 2));
+}
+
 struct HandSwitchMod
 {
 	const CalcPatternMod _pmod = HandSwitch;
 	const std::string name = "HandSwitchMod";
 
 #pragma region params
-	float base = 0.0F;
+	float base = 0.F;
 	float min_mod = 0.F;
 	float max_mod = 2.F;
 
-	float prop_multi = 1.5F;
+	float prop_multi = 2.15F;
 	float threshold = 0.625F;
+
+	float ms_bias = 30.F;
+	float ms_range = 10.F;
+	float ms_max_threshold = 500.F;
+	float ms_min_threshold = 23.3F;
+	float ms_multi = 2.53F;
 
 	const std::vector<std::pair<std::string, float*>> _params {
 		{ "min_mod", &min_mod },
@@ -22,6 +33,11 @@ struct HandSwitchMod
 		{ "base", &base },
 		{ "prop_multi", &prop_multi },
 		{ "threshold", &threshold },
+		{ "ms_bias", &ms_bias },
+		{ "ms_range", &ms_range },
+		{ "ms_max_threshold", &ms_max_threshold },
+		{ "ms_min_threshold", &ms_min_threshold },
+		{ "ms_multi", &ms_multi },
 	};
 
 #pragma endregion params and param map
@@ -35,6 +51,9 @@ struct HandSwitchMod
 	int _mh, _oh, _ml = 0;
 	int _hmi = 0;
 
+	float _ms_last = 0.F;
+	float _ms_reward = 0.F;
+
 	void advance_sequencing(const float& ms_now, const unsigned& notes)
 	{
 		_hmi--;
@@ -43,12 +62,19 @@ struct HandSwitchMod
 		if (!(notes ^ 0b00100))
 		{
 			_hmi = 2;
+			_ms_last = 0;
 		}
 
+		float delta = ms_now - _ms_last;
 		if (_hmi > 0)
 		{
 			if (notes & 0b001) _ml++;
-			if (notes & 0b110) _mh++;
+			if (notes & 0b110)
+			{
+				_mh++;
+
+				_ms_reward += bell_curve(2 * (delta + ms_bias) / ms_max_threshold - 1) * 0.04;
+			}
 			if (notes & 0b00011) _oh++;
 		}
 		else if (_ml >= 1)
@@ -71,6 +97,7 @@ struct HandSwitchMod
 						yep = fastsqrt(yep);
 					}
 					yep *= prop_multi;
+					yep *= ms_multi;
 
 					_yep += yep;
 					_spike_average += _yep;
@@ -78,7 +105,10 @@ struct HandSwitchMod
 			}
 
 			_mh = _oh = _ml = 0;
+			_ms_reward = 0.F;
 		}
+
+		_ms_last = ms_now;
 	}
 
 	void full_reset()
@@ -90,6 +120,9 @@ struct HandSwitchMod
 
 		_mh = _oh = _ml = 0;
 		_hmi = 0;
+
+		_ms_last = 0.F;
+		_ms_reward = 0.F;
 	}
 
 	auto operator()(const metaItvGenericHandInfo& mitvghi)
