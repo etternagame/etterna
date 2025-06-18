6a5738bcfa1061e8b0e83604082406e115456317
convextriangle <87372947+convextriangle@users.noreply.github.com>
2025-06-19 02:26:29 +0300

    shader handler boilerplate

---
+++ src/RageUtil/Graphics/Shaders/RageShaderHandler.cpp
@@ -0,0 +1,1 @@
+#include "RageShaderHandler.h"
---
+++ src/RageUtil/Graphics/Shaders/RageShaderHandler.h
@@ -0,0 +1,10 @@
+#ifndef RAGE_SHADER_HANDLER_H
+#define RAGE_SHADER_HANDLER_H
+
+class RageShaderHandler
+{
+	// todo: caching stuffs (via RageShaderReference) and active shader
+	// tracking?
+};
+
+#endif
