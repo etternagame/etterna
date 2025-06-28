eb3c649a0558b5ef7a91409cdf342dc300743f35
convextriangle <87372947+convextriangle@users.noreply.github.com>
2025-06-28 13:57:10 +0300

    stub impl for RageShaderHandler

--- src/RageUtil/Graphics/Shaders/RageShaderHandler.cpp
+++ src/RageUtil/Graphics/Shaders/RageShaderHandler.cpp
@@ -1,1 +1,79 @@
 #include "RageShaderHandler.h"
+
+bool
+RageShaderHandler::IsShaderInCache(size_t shaderLookupKey) const
+{
+	return m_ShaderCache.contains(shaderLookupKey);
+}
+
+bool
+RageShaderHandler::IsDefaultShaderInCache(size_t defaultShaderIndex) const
+{
+	return defaultShaderIndex < m_DefaultShaderCache.size();
+}
+
+std::optional<RageShaderHandler::ShaderId>
+RageShaderHandler::CacheShaderFromPath(const std::string& path,
+										bool useAsDefaultShader)
+{
+	std::optional<RageShader> shader = m_CompileShader(path);
+	if (!shader.has_value()) {
+		return std::nullopt;
+	}
+
+	if (useAsDefaultShader) {
+		m_DefaultShaderCache.emplace_back(*shader);
+		return RageShaderHandler::ShaderId{ m_DefaultShaderCache.size() - 1,
+											true };
+	}
+
+	size_t lookupKey = GetShaderLookupKey(path);
+	m_ShaderCache.emplace(lookupKey, *shader);
+	return RageShaderHandler::ShaderId{ lookupKey, true };
+}
+
+bool
+RageShaderHandler::TryRemoveShaderFromCache(size_t shaderLookupKey)
+{
+	auto it = m_ShaderCache.find(shaderLookupKey);
+	if (it == m_ShaderCache.end()) {
+		return false;
+	}
+
+	m_ShaderCache.erase(it);
+	return true;
+}
+
+bool
+RageShaderHandler::TrySetActiveShader(ShaderId shaderId)
+{
+	auto& [shaderLookupKey, setDefaultShader] = shaderId;
+
+	if ((setDefaultShader && !IsDefaultShaderInCache(shaderLookupKey)) ||
+		(!setDefaultShader && !IsShaderInCache(shaderLookupKey))) {
+		return false;
+	}
+
+	if (m_CurrentShader.has_value() && *m_CurrentShader == shaderId) {
+		return true;
+	}
+
+	RageShader* neededShader = setDefaultShader
+								 ? &m_DefaultShaderCache[shaderLookupKey]
+								 : &m_ShaderCache[shaderLookupKey];
+
+	// TODO: rollback to previous shader in case of fucky wucky?
+	size_t setShaderResult = m_TrySetShaderForDevice(neededShader);
+	if (setShaderResult == 0) {
+		m_CurrentShader = shaderId;
+		return true;
+	}
+
+	return false;
+}
+
+size_t
+RageShaderHandler::GetShaderLookupKey(const std::string& path)
+{
+	return m_StringHasher(path);
+}
--- src/RageUtil/Graphics/Shaders/RageShaderHandler.h
+++ src/RageUtil/Graphics/Shaders/RageShaderHandler.h
@@ -1,10 +1,46 @@
 #ifndef RAGE_SHADER_HANDLER_H
 #define RAGE_SHADER_HANDLER_H
 
+#include "RageShader.h"
+#include "RageShaderWeakRef.h"
+#include <unordered_map>
+#include <memory>
+#include <functional>
+#include <optional>
+
+// CONVENTION: ownership of the return value must be passed to the caller
+using CompileShaderFromPathCallback = std::function<std::optional<RageShader>(const std::string&)>;
+
+// CONVENTION: the shader ownership is NOT passed to the callback; returns true if everything went OK
+using TrySetShaderForDeviceCallback = std::function<bool(RageShader*)>;
+
 class RageShaderHandler
 {
-	// todo: caching stuffs (via RageShaderReference) and active shader
-	// tracking?
+	// (lookupKey, isDefaultShader)
+	using ShaderId = std::pair<size_t, bool>;
+
+	RageShaderHandler(CompileShaderFromPathCallback compileShaderCallback,
+					  TrySetShaderForDeviceCallback setShaderCallback);
+
+	bool IsShaderInCache(size_t shaderLookupKey) const;
+	bool IsDefaultShaderInCache(size_t defaultShaderIndex) const;
+
+	std::optional<ShaderId> CacheShaderFromPath(const std::string& path,
+											   bool useAsDefaultShader);
+	bool TryRemoveShaderFromCache(size_t shaderLookupKey);
+	bool TrySetActiveShader(ShaderId shaderId);
+
+  private:
+	std::unordered_map<size_t, RageShader> m_ShaderCache;
+	std::vector<RageShader> m_DefaultShaderCache;
+
+	std::optional<ShaderId> m_CurrentShader;
+
+	const CompileShaderFromPathCallback m_CompileShader;
+	const TrySetShaderForDeviceCallback m_TrySetShaderForDevice;
+
+	size_t GetShaderLookupKey(const std::string& path);
+	std::hash<std::string> m_StringHasher;
 };
 
 #endif
