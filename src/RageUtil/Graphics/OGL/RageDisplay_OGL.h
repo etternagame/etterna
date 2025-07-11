8049cc41bce54e2e5c3441881b206f8d8cb7ab9f
convextriangle <87372947+convextriangle@users.noreply.github.com>
2025-07-11 22:54:47 +0300

    move ogl helper macros to ogl helpers

--- src/RageUtil/Graphics/OGL/RageDisplay_OGL.h
+++ src/RageUtil/Graphics/OGL/RageDisplay_OGL.h
@@ -8,28 +8,6 @@
 #include "Etterna/Actor/Base/Sprite.h"
 #include "RageUtil/Graphics/RageTextureRenderTarget.h"
 
-/* Making an OpenGL call doesn't also flush the error state; if we happen
- * to have an error from a previous call, then the assert below will fail.
- * Flush it. */
-#define FlushGLErrors()                                                        \
-	do {                                                                       \
-	} while (glGetError() != GL_NO_ERROR)
-#define AssertNoGLError()                                                      \
-                                                                               \
-	{                                                                          \
-		GLenum error = glGetError();                                           \
-		ASSERT_M(error == GL_NO_ERROR,                                         \
-				 RageDisplay_Legacy_Helpers::GLToString(error));               \
-	}
-
-#if defined(DEBUG) || !defined(GL_GET_ERROR_IS_SLOW)
-#define DebugFlushGLErrors() FlushGLErrors()
-#define DebugAssertNoGLError() AssertNoGLError()
-#else
-#define DebugFlushGLErrors()
-#define DebugAssertNoGLError()
-#endif
-
 class RageDisplay_Legacy : public RageDisplay
 {
   public:
--- src/RageUtil/Graphics/OGL/RageDisplay_OGL_Helpers.h
+++ src/RageUtil/Graphics/OGL/RageDisplay_OGL_Helpers.h
@@ -15,6 +15,28 @@
 #define GL_TEXTURE_INDEX_SIZE_EXT 0x80ED
 #endif
 
+/* Making an OpenGL call doesn't also flush the error state; if we happen
+ * to have an error from a previous call, then the assert below will fail.
+ * Flush it. */
+#define FlushGLErrors()                                                        \
+	do {                                                                       \
+	} while (glGetError() != GL_NO_ERROR)
+#define AssertNoGLError()                                                      \
+                                                                               \
+	{                                                                          \
+		GLenum error = glGetError();                                           \
+		ASSERT_M(error == GL_NO_ERROR,                                         \
+				 RageDisplay_Legacy_Helpers::GLToString(error));               \
+	}
+
+#if defined(DEBUG) || !defined(GL_GET_ERROR_IS_SLOW)
+#define DebugFlushGLErrors() FlushGLErrors()
+#define DebugAssertNoGLError() AssertNoGLError()
+#else
+#define DebugFlushGLErrors()
+#define DebugAssertNoGLError()
+#endif
+
 /** @brief Utilities for working with the RageDisplay. */
 namespace RageDisplay_Legacy_Helpers {
 void
