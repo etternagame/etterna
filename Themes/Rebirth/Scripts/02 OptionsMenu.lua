3efa708ab9e2fc0dd60ef8f580a8160c3e90b310
Barinade <poco0317@users.noreply.github.com>
2026-03-11 00:29:11 -0500

    move theme specific lane cover into theme specific settings

--- Themes/Rebirth/Scripts/02 OptionsMenu.lua
+++ Themes/Rebirth/Scripts/02 OptionsMenu.lua
@@ -1,7 +1,7 @@
 function getPlayerOptionsList(itemSet)
 	local Items = {
-		["Main"] = "Speed,RateList,NoteSk,RS,PRAC,CG,ScrollDir,Center,Persp,LC,BG,SF,Background,Judge,Life,Fail,Assist,Score",
-		["Theme"] = "CBHL,JT,JA,CT,CL,DP,TT,TG,TTM,JC,EB,EBC,PI,FB,MB,LEADB,NPS",
+		["Main"] = "Speed,RateList,NoteSk,RS,PRAC,CG,ScrollDir,Center,Persp,BG,SF,Background,Judge,Life,Fail,Assist,Score",
+		["Theme"] = "LC,CBHL,JT,JA,CT,CL,DP,TT,TG,TTM,JC,EB,EBC,PI,FB,MB,LEADB,NPS",
 		["Effect"] = "Persp,App,GHO,SHO,Acc,Hide,Effect1,Effect2,Scroll,Turn,Insert,R1,R2,Holds,Mines,HoldReleases"
 	}
 	return Items[itemSet] .. ",NextScr"
