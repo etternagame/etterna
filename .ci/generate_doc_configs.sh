e7004e56bc6e2a5d1058c606a73caac2a42a6e49
convextriangle <87372947+convextriangle@users.noreply.github.com>
2026-08-15 14:24:00 +0300

    disable vk in macos & doc generation CI

--- .ci/generate_doc_configs.sh
+++ .ci/generate_doc_configs.sh
@@ -10,4 +10,4 @@ eval "$(luarocks path --bin)"
 
 # Generate Etterna CMake
 mkdir build && cd build
-cmake -G "Unix Makefiles" -DWITH_CRASHPAD=OFF -DOPENSSL_ROOT_DIR=~/toolchain/ -DBUILD_TESTS=FALSE -DBUILD_EXAMPLES=FALSE ..
+cmake -G "Unix Makefiles" -DWITH_CRASHPAD=OFF -DWITH_VULKAN=OFF -DOPENSSL_ROOT_DIR=~/toolchain/ -DBUILD_TESTS=FALSE -DBUILD_EXAMPLES=FALSE ..
